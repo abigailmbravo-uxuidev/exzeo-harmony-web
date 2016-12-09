2b42e3e99e681e3764d4cdb51a40fd63ecd4958d
jcarrillo1 <xjcarrillox@gmail.com>
2016-12-09 15:06:48 -0500

    Added mask for handle change on radio group

--- src/components/common/form/RadioGroup.jsx
+++ src/components/common/form/RadioGroup.jsx
@@ -1,24 +1,36 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 import React, { PropTypes } from 'react';
 
-const RadioGroup = ({ question, answers, value, id, handleChange, segmented }) => (
-  <div className={segmented ? 'form-group segmented' : 'form-group'} role="group">
-    {question || null}
-    {answers && answers.length > 0 ? answers.map((answer, index) =>
-      <label htmlFor={index} key={index}>
-        <input
-          type="radio"
-          value={answer || null}
-          key={index}
-          name={id || null}
-          checked={value === answer}
-          onChange={handleChange || null}
-        />
-        <span onChange={handleChange}>{answer || null}</span>
-      </label>,
-    ) : null}
-  </div>
-);
+const RadioGroup = ({ question, answers, value, id, handleChange, segmented }) => {
+  const onClick = (answer) => {
+    const newEvent = {
+      target: {
+        name: id,
+        value: answer,
+      },
+    };
+    handleChange(newEvent);
+  };
+
+  return (
+    <div className={segmented ? 'form-group segmented' : 'form-group'} role="group">
+      {question || null}
+      {answers && answers.length > 0 ? answers.map((answer, index) =>
+        <label htmlFor={index} key={index}>
+          <input
+            type="radio"
+            value={answer || null}
+            key={index}
+            name={id || null}
+            checked={value === answer}
+            onChange={handleChange || null}
+          />
+          <span onClick={() => onClick(answer)}>{answer || null}</span>
+        </label>,
+      ) : null}
+    </div>
+  );
+};
 
 RadioGroup.propTypes = {
   question: PropTypes.string,
