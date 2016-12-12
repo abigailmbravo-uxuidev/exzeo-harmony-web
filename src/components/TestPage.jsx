b9a6a8841970a2da9129f92c8fc2dc6102313e59
jcarrillo1 <xjcarrillox@gmail.com>
2016-12-12 10:07:20 -0500

    More work to handle submit component

--- src/components/TestPage.jsx
+++ src/components/TestPage.jsx
@@ -1,55 +1,6 @@
 // Test page for components
 import React, { Component } from 'react';
-import Question from './common/question/Question';
 import Survey from './common/question/Survey';
-
-const boolQuestion = {
-  question: {
-    id: 'bool-test',
-    question: 'Test for bool?',
-    description: 'Testing for bool',
-    answerType: 'bool',
-  },
-};
-
-const textQuestion = {
-  question: {
-    id: 'text-test',
-    question: 'Test for text?',
-    description: 'Testing for text',
-    answerType: 'text',
-  },
-};
-
-const passwordQuestion = {
-  question: {
-    id: 'password-test',
-    question: 'Test for password?',
-    description: 'Testing for password',
-    answerType: 'password',
-  },
-};
-
-// Needs 6 or more answers to activate
-const dropdownQuestion = {
-  question: {
-    id: 'dropdown-test',
-    question: 'Test for dropdown?',
-    description: 'Testing for dropdown',
-    answerType: 'radio',
-    answers: ['answer1', 'answer2', 'answer3', 'answer4', 'answer5', 'answer6'],
-  },
-};
-
-const radioQuestion = {
-  question: {
-    id: 'radio-test',
-    question: 'Test for radio?',
-    description: 'Testing for radio',
-    answerType: 'radio',
-    answers: ['answer1', 'answer2', 'answer3'],
-  },
-};
 
 const surveyQuestions = [{
   id: 'bool-test',
@@ -93,19 +44,13 @@ class TestPage extends Component {
     state[event.target.name] = event.target.value;
     this.setState(state);
   }
-  handleSubmit = (event) => {
-    event.preventDefault();
-    console.log("wew"); // eslint-disable-line
+  handleSubmit = (form) => {
+    console.log(form); // eslint-disable-line
   }
   render() {
     return (
       <div>
         <br />
-        <Question {...boolQuestion} handleChange={this.handleChange} value={this.state['bool-test']} />
-        <Question {...textQuestion} handleChange={this.handleChange} value={this.state['text-test']} />
-        <Question {...passwordQuestion} handleChange={this.handleChange} value={this.state['password-test']} />
-        <Question {...dropdownQuestion} handleChange={this.handleChange} value={this.state['dropdown-test']} />
-        <Question {...radioQuestion} handleChange={this.handleChange} value={this.state['radio-test']} />
         <Survey questions={surveyQuestions} handleSubmit={this.handleSubmit} />
       </div>
     );
