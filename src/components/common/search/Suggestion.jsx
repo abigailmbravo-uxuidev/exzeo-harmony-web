267e8321d1b913f4aeb2e194bd8692d10b0f112a
Marco James <mjames@exzeo.com>
2016-12-13 15:41:43 -0500

    redirect to 'quote' from search submit and typeahead select.

--- src/components/common/search/Suggestion.jsx
+++ src/components/common/search/Suggestion.jsx
@@ -1,6 +1,6 @@
 import React, { PropTypes } from 'react';
 
-const Suggestion = ({ data }) => {
+const Suggestion = ({ data, handleSelect }) => {
   const { heading, mapping, results, count } = data;
   return (
     <div>
@@ -9,7 +9,11 @@ const Suggestion = ({ data }) => {
       <ul>
         {
           results && results.length > 0 ? results.map((result, index) => (
-            <li key={index}><a><span>{result[mapping.title]} {result[mapping.details]}</span><i className="fa fa-chevron-circle-right"></i></a></li>
+            <li key={index}>
+                <a onClick={handleSelect}>
+                    <span>{result[mapping.title]} {result[mapping.details]}</span><i className="fa fa-chevron-circle-right"></i>
+                </a>
+            </li>
           )) : null
         }
       </ul>
