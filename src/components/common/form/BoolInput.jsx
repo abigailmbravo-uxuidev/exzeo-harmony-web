c2a5dc36e46527297befce3f9c4211d838d99240
jcarrillo1 <xjcarrillox@gmail.com>
2016-12-21 14:52:05 -0500

    Initial population for bool

--- src/components/common/form/BoolInput.jsx
+++ src/components/common/form/BoolInput.jsx
@@ -1,32 +1,69 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 
-const BoolInput = ({ question, name, value, handleChange, isSwitch, styleName = '' }) => {
-  const onChange = () => {
-    const newEvent = {
+class BoolInput extends Component {
+  componentWillMount = () => {
+    this.props.handleChange({
       target: {
-        name: name,
-        value: !value,
+        name: this.props.name,
+        value: false,
       },
-    };
-    handleChange(newEvent);
-  };
-  const classnames = `form-group ${isSwitch ? 'switch' : ''} ${styleName} ${name}`;
-  return (
-    <div className={classnames} >
-      <label htmlFor={name || null} onClick={onChange}>
-        {question || null}
-        <input
-          type="checkbox"
-          name={name || null}
-          checked={value || false}
-          onChange={onChange}
-        />
-        {isSwitch && <div />}
-      </label>
-    </div>
-  );
-};
+    });
+  }
+  onChange = () => {
+    this.props.handleChange({
+      target: {
+        name: this.props.name,
+        value: !this.props.value,
+      },
+    });
+  }
+  render() {
+    const { isSwitch, styleName, name, value, question } = this.props;
+    const classnames = `form-group ${isSwitch ? 'switch' : ''} ${styleName} ${name}`;
+    return (
+      <div className={classnames} >
+        <label htmlFor={name || null} onClick={this.onChange}>
+          {question || null}
+          <input
+            type="checkbox"
+            name={name || null}
+            checked={value || false}
+            onChange={this.onChange}
+          />
+          {isSwitch && <div />}
+        </label>
+      </div>
+    );
+  }
+}
+
+// const BoolInput = ({ question, name, value, handleChange, isSwitch, styleName = '' }) => {
+//   const onChange = () => {
+//     const newEvent = {
+//       target: {
+//         name,
+//         value: !value,
+//       },
+//     };
+//     handleChange(newEvent);
+//   };
+//   const classnames = `form-group ${isSwitch ? 'switch' : ''} ${styleName} ${name}`;
+//   return (
+//     <div className={classnames} >
+//       <label htmlFor={name || null} onClick={onChange}>
+//         {question || null}
+//         <input
+//           type="checkbox"
+//           name={name || null}
+//           checked={value || false}
+//           onChange={onChange}
+//         />
+//         {isSwitch && <div />}
+//       </label>
+//     </div>
+//   );
+// };
 
 BoolInput.propTypes = {
   question: PropTypes.string,
