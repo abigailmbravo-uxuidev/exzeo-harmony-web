5554b4596a31a0abbbf989c76b3b251e7a35e3b4
Joe Sutphin <jsutphin@Helps-MacBook-Pro-2.local>
2016-12-23 14:24:46 -0500

    range styling

--- src/components/common/form/SliderInput.jsx
+++ src/components/common/form/SliderInput.jsx
@@ -1,29 +1,33 @@
 import React, { PropTypes } from 'react';
 
 const SliderInput = ({ question, name, description, value, minValue, maxValue, leftLabel, rightLabel, answerType, handleChange, styleName = '' }) => (
-  <div className={`form-group ${styleName} ${name}`}>
+  <div className={`form-group range-component ${styleName} ${name}`}>
     <label htmlFor={name || null}>
       {question || null}
       &nbsp;
       {description && <span className="tooltip"><i className="fa fa-info-circle" aria-hidden="true" /><span className="tooltiptext">{description}</span></span>}
     </label>
-    <span>{leftLabel || Math.ceil(minValue) || null}</span>
-    <input
-      type={answerType || 'range'}
-      name={name || null}
-      min={Math.ceil(minValue)}
-      max={Math.floor(maxValue)}
-      step={1000}
-      value={value || minValue}
-      onChange={handleChange || null}
-    />
-    <span>{rightLabel || Math.floor(maxValue) || null}</span>
-    <span><input
-      type="text"
-      value={value}
-      onChange={handleChange}
-      name={name}
-    /></span>
+    <div className="range-wrapper">
+            <div className="range-control-wrapper">
+                    <span className="range-limit">{leftLabel || Math.ceil(minValue) || null}</span>
+                    <input
+                      type={answerType || 'range'}
+                      name={name || null}
+                      min={Math.ceil(minValue)}
+                      max={Math.floor(maxValue)}
+                      step={1000}
+                      value={value || minValue}
+                      onChange={handleChange || null}
+                    />
+            <span className="range-limit">{rightLabel || Math.floor(maxValue) || null}</span>
+            </div>
+            <span className="range-value"><input
+              type="text"
+              value={value}
+              onChange={handleChange}
+              name={name}
+            /></span>
+        </div>
   </div>
 );
 
