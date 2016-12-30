82768530e5b9ecf60f8f38407cb1eba3ce442859
Seth Lowie <slowie@exzeo.com>
2016-12-30 09:28:34 -0500

    hide links through search config

--- src/components/common/search/Suggestion.jsx
+++ src/components/common/search/Suggestion.jsx
@@ -1,11 +1,12 @@
 import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
 
 let links = [
-  <a key="ho3">Home</a>,
-  <a key="af3">Flood</a>,
+  <span key="ho3">Home</span>,
+  <span key="af3">Flood</span>,
 ];
 
-const Suggestion = ({ data, handleSelect }) => {
+const Suggestion = ({ data, handleSelect, showLinks }) => {
   const { heading, mapping, results, count } = data;
   links = links.filter(a => (mapping.links ? mapping.links.includes(a.key) : null));
   return (
@@ -16,7 +17,7 @@ const Suggestion = ({ data, handleSelect }) => {
           results && results.length > 0 ? results.map((result, index) => (
             <li key={index}>
               <a onClick={handleSelect} >
-                <span>{result[mapping.title]} {result[mapping.details]}</span>{links}<i className="fa fa-chevron-circle-right" />
+                <span>{result[mapping.title]} {result[mapping.details]}</span>{showLinks ? links : null}<i className="fa fa-chevron-circle-right" />
               </a>
             </li>
           )) : null
@@ -32,4 +33,8 @@ Suggestion.propTypes = {
   }),
 };
 
-export default Suggestion;
+const mapStateToProps = state => ({
+  showLinks: state.search.get('config') ? state.search.get('config').showLinks : true,
+});
+
+export default connect(mapStateToProps)(Suggestion);
