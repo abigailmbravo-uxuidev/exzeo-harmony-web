bbf2709e36be775b3a1bb1d3c09f8527ee81bd79
HCI01\meads <meads@hcpci.com>
2017-01-26 11:28:39 -0500

    fixed eslint errors

--- src/components/Rules.jsx
+++ src/components/Rules.jsx
@@ -2,17 +2,17 @@ import validator from 'validator';
 
 
 const rules = {
-  required: value => value ? undefined : 'Field Required',
-  email: value => validator.isEmail(value) ? undefined : 'Not a valid email address',
-  phone: value => value.match(/^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/g) ? undefined : 'is not a valid Phone Number.',
-  date: value => validator.isDate(value) ? value : 'is not a valid Date.',
+  required: (value) => { value ? undefined : 'Field Required'; },
+  email: (value) => { validator.isEmail(value) ? undefined : 'Not a valid email address'; },
+  phone: (value) => { value.match(/^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/g) ? undefined : 'is not a valid Phone Number.'; },
+  date: (value) => { validator.isDate(value) ? value : 'is not a valid Date.'; },
 };
 
 export function combineRules(validations) {
   const ruleArray = [];
 
   if (validations) {
-    for (let i = 0; i < validations.length; i++) {
+    for (let i = 0; i < validations.length; i += 1) {
       ruleArray.push(rules[`${validations[i]}`]);
     }
   }
