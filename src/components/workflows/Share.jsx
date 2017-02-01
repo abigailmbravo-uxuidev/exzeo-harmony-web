0fcac3dbf7ca7e606fe54fbc4edd7df723100e7c
Giridhar Raveendran <graveendran@outlook.com>
2017-02-01 16:48:10 -0500

    Updated the Share with content

--- src/components/workflows/Share.jsx
+++ src/components/workflows/Share.jsx
@@ -5,11 +5,33 @@ import React, { Component } from 'react';
 class Share extends Component {
   state = {
     // Some state will go here
-  }
+  };
   render() {
     return (
       <div>
-        SHARE
+        <div>
+          <p>Coverages</p>
+          <p>Dwelling: $100,000</p>
+          <p>Other Structures: $100,000</p>
+          <p>Personal Property: $50000</p>
+          <p>Personal Property Replacement Cost: Yes</p>
+          <p>Loss Of Use: $10,000</p>
+          <p>Personal Liability: $100,000</p>
+          <p>Medical Payments: $2,000</p>
+          <p>Mold Property: $10,000</p>
+          <p>Mold Liability: $50,000</p>
+          <p>Ordinance or Law: 25,000</p>
+          <p>Property Incidental Occupancies: Yes</p>
+          <p>Sinkhole Coverage: Yes</p>
+          <p>All Other Perils: $500</p>
+          <p>Hurricane: 2,000</p>
+          <p>Sinkhole Deductible: Yes</p>
+        </div>
+        <div className="workflow-steps">
+          <button className="btn btn-primary" type="submit" form="survey">Customize</button>
+          <button className="btn btn-primary" type="submit" form="survey">share</button>
+          <button className="btn btn-primary" type="submit" form="survey">next</button>
+        </div>
       </div>
     );
   }
