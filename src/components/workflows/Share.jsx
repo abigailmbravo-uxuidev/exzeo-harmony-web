883bd9eb92e9c988e23b1f2a0bc546491e1cd934
Joe Sutphin <jsutphin@Helps-MacBook-Pro-2.local>
2017-02-02 15:41:32 -0500

    share step html & style

--- src/components/workflows/Share.jsx
+++ src/components/workflows/Share.jsx
@@ -1,7 +1,87 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import Footer from '../common/Footer';
 
 // import Stepper from './workflows/Stepper';
-
+const CoverageDetails = () => (
+  <div className="CoverageDetails detail-group">
+    <h4>Coverages</h4>
+    <button className="btn btn-link edit-btn" type="" form=""><i className="fa fa-pencil"></i>Edit</button>
+    <section className="summary-section">
+      <dl>
+        <dt>
+          <span>A</span>Dwelling</dt>
+        <dd>$100,000</dd>
+      </dl>
+      <dl>
+        <dt>
+          <span>B</span>
+          Other Structures</dt>
+        <dd>$100,000</dd>
+      </dl>
+      <dl>
+        <dt>
+          <span>C</span>Personal Property</dt>
+        <dd>$50000</dd>
+      </dl>
+      <dl>
+        <dt>Personal Property Replacement Cost</dt>
+        <dd>Yes</dd>
+      </dl>
+      <dl>
+        <dt>Loss Of Use</dt>
+        <dd>$10,000</dd>
+      </dl>
+      <dl>
+        <dt>Personal Liability</dt>
+        <dd>$100,000</dd>
+      </dl>
+      <dl>
+        <dt>Medical Payments</dt>
+        <dd>$2,000</dd>
+      </dl>
+    </section>
+  </div>
+)
+const RatingDetails = () => (
+  <div className="RatingDetails detail-group">
+    <h4>Rating</h4>
+    <button className="btn btn-link edit-btn" type="" form=""><i className="fa fa-pencil"></i>Edit</button>
+    <section className="summary-section">
+      <dl>
+        <dt>Mold Property</dt>
+        <dd>$10,000</dd>
+      </dl>
+      <dl>
+        <dt>Mold Liability</dt>
+        <dd>$50,000</dd>
+      </dl>
+      <dl>
+        <dt>Ordinance or Law</dt>
+        <dd>$25,000</dd>
+      </dl>
+      <dl>
+        <dt>Property Incidental Occupancies</dt>
+        <dd>Yes</dd>
+      </dl>
+      <dl>
+        <dt>Hurricane</dt>
+        <dd>$2,000</dd>
+      </dl>
+      <dl>
+        <dt>Sinkhole Coverage</dt>
+        <dd>Yes</dd>
+      </dl>
+      <dl>
+        <dt>Sinkhole Deductible</dt>
+        <dd>Yes</dd>
+      </dl>
+      <dl>
+        <dt>All Other Perils</dt>
+        <dd>$500</dd>
+      </dl>
+    </section>
+  </div>
+)
 class Share extends Component {
   state = {
     // Some state will go here
@@ -9,61 +89,36 @@ class Share extends Component {
   render() {
     return (
       <div className="fade-in">
-        <div className="form-group">
-          <h4>Coverages</h4>
-          <dl>
-                  <dt>Dwelling</dt>
-                  <dd>$100,000</dd></dl>
-          <dl>
-                  <dt>Other Structures</dt>
-                  <dd>$100,000</dd></dl>
-          <dl>
-                  <dt>Personal Property</dt>
-                  <dd>$50000</dd></dl>
-          <dl>
-                  <dt>Personal Property Replacement Cost</dt>
-                  <dd>Yes</dd></dl>
-          <dl>
-                  <dt>Loss Of Use</dt>
-                  <dd>$10,000</dd></dl>
-          <dl>
-                  <dt>Personal Liability</dt>
-                  <dd>$100,000</dd></dl>
-          <dl>
-                  <dt>Medical Payments</dt>
-                  <dd>$2,000</dd></dl>
-          <dl>
-                  <dt>Mold Property</dt>
-                  <dd>$10,000</dd></dl>
-          <dl>
-                  <dt>Mold Liability</dt>
-                  <dd>$50,000</dd></dl>
-          <dl>
-                  <dt>Ordinance or Law</dt>
-                  <dd>$25,000</dd></dl>
-          <dl>
-                  <dt>Property Incidental Occupancies</dt>
-                  <dd>Yes</dd></dl>
-          <dl>
-                  <dt>Sinkhole Coverage</dt>
-                  <dd>Yes</dd></dl>
-          <dl>
-                  <dt>All Other Perils</dt>
-                  <dd>$500</dd></dl>
-          <dl>
-                  <dt>Hurricane</dt><dd>$2,000</dd></dl>
-          <dl>
-                  <dt>Sinkhole Deductible</dt><dd>Yes</dd></dl>
+        <div className="detail-content-wrapper ">
+          <aside>
+            <dl>
+              <dt>Quote number</dt>
+              <dd>TTIC-HO3-12345</dd>
+            </dl>
+            <dl>
+              <dt>Address</dt>
+              <dd>123 Main Street</dd>
+              <dd>Fort lauderdale, FL 12345</dd>
+            </dl>
+            <dl>
+              <dt>Year Built</dt>
+              <dd>2000</dd>
+            </dl>
+          </aside>
+          <div className="detail-wrapper">
+            <CoverageDetails/>
+            <RatingDetails/>
+          </div>
         </div>
         <div className="workflow-steps">
-          <button className="btn btn-primary" type="submit " form="survey">Customize</button>
+
           <button className="btn btn-primary" type="submit" form="survey">share</button>
           <button className="btn btn-primary" type="submit" form="survey">next</button>
         </div>
+        <Footer />
       </div>
     );
   }
 }
-
 
 export default Share;
--- src/components/workflows/Workflow.jsx
+++ src/components/workflows/Workflow.jsx
@@ -2,6 +2,64 @@ import React, {Component, PropTypes} from 'react';
 //import WorkflowStep from './WorkflowStep';
 import WorkflowHeader from '../workflows/WorkflowHeader';
 
+const WorkflowDetails = () => (
+        <div className="detailHeader">
+          <section id="quoteDetails" className="quoteDetails">
+            <dl>
+              <div>
+                <dt>Quote number</dt>
+                <dd>TTIC-HO3-<span>12345</span>
+                </dd>
+              </div>
+            </dl>
+          </section>
+          <section id="propertyDetails" className="propertyDetails">
+            <dl>
+              <div>
+                <dt>Address</dt>
+                <dd>123 Main Street</dd>
+                <dd>Fort Lauderdale, FL 12345</dd>
+              </div>
+            </dl>
+          </section>
+          <section id="propertyDetails" className="propertyDetails">
+            <dl>
+              <div>
+                <dt>Year built</dt>
+                <dd>2000</dd>
+              </div>
+            </dl>
+          </section>
+          <section id="coverageDetails" className="coverageDetails">
+            <dl>
+              <div>
+                <dt>Coverage A</dt>
+                <dd>$100,000</dd>
+              </div>
+            </dl>
+            <dl>
+              <div>
+                <dt>Coverage B</dt>
+                <dd>$100,000</dd>
+              </div>
+            </dl>
+            <dl>
+              <div>
+                <dt>Coverage C</dt>
+                <dd>$50,000</dd>
+              </div>
+            </dl>
+          </section>
+          <section id="premium" className="premium">
+            <dl>
+              <div>
+                <dt>Annual premium</dt>
+                <dd>$10,000</dd>
+              </div>
+            </dl>
+          </section>
+        </div>
+)
 class Workflow extends Component {
 
   static propTypes = {
@@ -65,62 +123,7 @@ class Workflow extends Component {
     return (
       <div className="workflow" role="article">
         <div className="fade-in">
-          <div className="detailHeader">
-            <section id="quoteDetails" className="quoteDetails">
-              <dl>
-                <div>
-                  <dt>Quote number</dt>
-                  <dd>TTIC-HO3-<span>12345</span>
-                  </dd>
-                </div>
-              </dl>
-            </section>
-            <section id="propertyDetails" className="propertyDetails">
-              <dl>
-                <div>
-                  <dt>Address</dt>
-                  <dd>123 Main Street</dd>
-                  <dd>Fort Lauderdale, FL, 12345</dd>
-                </div>
-              </dl>
-            </section>
-            <section id="propertyDetails" className="propertyDetails">
-              <dl>
-                <div>
-                  <dt>Year built</dt>
-                  <dd>2000</dd>
-                </div>
-              </dl>
-            </section>
-            <section id="coverageDetails" className="coverageDetails">
-              <dl>
-                <div>
-                  <dt>Coverage A</dt>
-                  <dd>$100,000</dd>
-                </div>
-              </dl>
-              <dl>
-                <div>
-                  <dt>Coverage B</dt>
-                  <dd>$100,000</dd>
-                </div>
-              </dl>
-              <dl>
-                <div>
-                  <dt>Coverage C</dt>
-                  <dd>$50,000</dd>
-                </div>
-              </dl>
-            </section>
-            <section id="premium" className="premium">
-              <dl>
-                <div>
-                  <dt>Annual premium</dt>
-                  <dd>$10,000</dd>
-                </div>
-              </dl>
-            </section>
-          </div>
+          <WorkflowDetails />
           <WorkflowHeader steps={workflow.steps} completedSteps={this.state.completedSteps}/>
         </div>
       </div>
