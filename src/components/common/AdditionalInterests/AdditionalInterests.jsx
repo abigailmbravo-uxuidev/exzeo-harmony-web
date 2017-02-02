08cde1659b6b62a1e1b10fe672f2ce0627eecce1
HCI01\meads <meads@hcpci.com>
2017-02-02 13:29:13 -0500

    Additional interests updates

--- src/components/common/AdditionalInterests/AdditionalInterests.jsx
+++ src/components/common/AdditionalInterests/AdditionalInterests.jsx
@@ -4,18 +4,18 @@ import TextInput from '../form/TextInput';
 import MailingAddress from '../MailingAddress/MailingAddress';
 import PolicyHolder from '../policyHolder/PolicyHolder';
 
-const rendermortgagees = ({ fields, InterestType, handleChange, meta: { touched, error } }) => (
+const rendermortgagees = ({ fields, InterestType, InterestTypeName, handleChange, meta: { touched, error } }) => (
   <div>
-    <button type="button" onClick={() => fields.push({})}>+ Add a {InterestType}</button>
+    <button type="button" onClick={() => fields.push({})}>+ Add {InterestTypeName}</button>
     {touched && error && <span>{error}</span>}
     {fields.map((ai, index) =>
       <div key={index}>
         <button
           type="button"
           onClick={() => fields.remove(index)}
-        >Remove {InterestType}</button>
+        >Remove {InterestTypeName}</button>
         <br /> <br />
-        <h4>{InterestType} #{index + 1}</h4>
+        <h4>{InterestTypeName} #{index + 1}</h4>
         <TextInput
           answerType="text"
           handleChange={function () {}}
@@ -50,18 +50,18 @@ const rendermortgagees = ({ fields, InterestType, handleChange, meta: { touched,
   </div>
 );
 
-const renderPolicyHolder = ({ fields, InterestType, handleChange, meta: { touched, error } }) => (
+const renderPolicyHolder = ({ fields, InterestType, InterestTypeName, handleChange, meta: { touched, error } }) => (
   <div>
-    <button type="button" onClick={() => fields.push({})}>+ Add a {InterestType}</button>
+    {fields.length < 1 && <button type="button" onClick={() => fields.push({})}>+ Add {InterestTypeName}</button>}
     {touched && error && <span>{error}</span>}
     {fields.map((ai, index) =>
       <div key={index}>
         <button
           type="button"
           onClick={() => fields.remove(index)}
-        >Remove {InterestType}</button>
+        >Remove {InterestTypeName}</button>
         <br /> <br />
-        <h4>{InterestType} #{index + 2}</h4>
+        <h4>{InterestTypeName}</h4>
         <PolicyHolder handleChange={function () {}} name={`${InterestType + index + 2}.`} />
       </div>,
     )}
@@ -75,17 +75,17 @@ const AdditionalInterests = (props) => {
     <div>
       <h3>Mailing Address</h3>
       <MailingAddress {...props} />
-      <FieldArray name="mortgagees.Mortgagee" component={rendermortgagees} InterestType={'Mortgagee'} />
+      <FieldArray name="mortgagees.Mortgagee" component={rendermortgagees} InterestType={'Mortgagee'} InterestTypeName={'Mortgagee'} />
       <br /><br />
-      <FieldArray name="mortgagees.leinholder" component={rendermortgagees} InterestType={'Lienholder'} />
+      <FieldArray name="mortgagees.leinholder" component={rendermortgagees} InterestType={'Lienholder'} InterestTypeName={'Lienholder'} />
       <br /><br />
-      <FieldArray name="mortgagees.AdditionalInterest" component={rendermortgagees} InterestType={'AdditionalInterest'} />
+      <FieldArray name="mortgagees.AdditionalInterest" component={rendermortgagees} InterestType={'AdditionalInterest'} InterestTypeName={'Additional Interest'} />
       <br /><br />
-      <FieldArray name="mortgagees.AdditionalInsured" component={rendermortgagees} InterestType={'AdditionalInsured'} />
+      <FieldArray name="mortgagees.AdditionalInsured" component={rendermortgagees} InterestType={'AdditionalInsured'} InterestTypeName={'Additional Insured'} />
       <br /><br />
-      <FieldArray name="mortgagees.BillPayer" component={rendermortgagees} InterestType={'BillPayer'} />
+      <FieldArray name="mortgagees.BillPayer" component={rendermortgagees} InterestType={'BillPayer'} InterestTypeName={'Bill Payer'} />
       <br /><br />
-      <FieldArray name="additionalPolicyHolder" component={renderPolicyHolder} InterestType={'Policy Holder'} />
+      <FieldArray name="additionalPolicyHolder" component={renderPolicyHolder} InterestType={'AdditionalPolicyHolder'} InterestTypeName={'Additional Policy Holder'} />
     </div>
   );
 };
