be818d9c04ffb8a454904a344cc61d90139627b4
Marco James <mjames@exzeo.com>
2017-02-20 14:33:56 -0500

    Update quote with customized info added.

--- src/components/forms/Customize/Customize.jsx
+++ src/components/forms/Customize/Customize.jsx
@@ -33,6 +33,8 @@ class Customize extends Component {
   componentWillMount() {
   }
 
+
+
   componentWillReceiveProps(newProps) {
     if ((!this.props.data.steps && newProps.data.steps) ||
     (!newProps.data.loading &&
@@ -54,13 +56,13 @@ class Customize extends Component {
           state[question.name] = question.readOnlyValue;
         } else if (question.defaultValueLocation) {
           const val = _.get(realQuote, question.defaultValueLocation);
-          state[question.name] = _.isString(val) ? String(val) : val;
+          state[question.name] = val;
         } else {
           state[question.name] = '';
         }
       });
 
-      //console.log('STATE AFTER CONVERIONS: ', this.state);
+      console.log('QUOTE OBJECT: ', realQuote);
 
       state.quoteInfo = realQuote;
 
@@ -73,8 +75,7 @@ class Customize extends Component {
             const { dependency } = question.conditional;
             const parentValue = _.get(state, dependency.parent);
 
-            const stateValue = Number(state[question.name]) ?
-               Number(state[question.name]) : state[question.name];
+            const stateValue = state[question.name];
             // const calculatedValue = parentValue / 100;
             const newValue = question.answers.find(a =>
               (dependency.type === 'percent' ? (parentValue * a.answer) / 100 : parentValue * a.answer) === stateValue);
@@ -135,10 +136,23 @@ class Customize extends Component {
     }
   });
 
+  convertQuoteStringsToNumber(quote){
+    for(let obj in quote){
+      if(_.isString(quote[obj])) {
+        quote[obj] = (Number(quote[obj]) ? Number(quote[obj]) : quote[obj])
+      }
+    }
+    return quote;
+  }
+
   recalculateQuote = async () => {
 
     const { completeStep } = this.props;
     console.log('STATE BEFORE SENDING RECALC', this.state);
+
+    const updateQuote = this.convertQuoteStringsToNumber(this.state);
+    console.log('STATE AFTER CONVERION SENDING RECALC', updateQuote);
+
     try {
       let data = await completeStep(this.buildSubmission('askToCustomizeDefaultQuote', {shouldCustomizeQuote: 'Yes'}));
       console.log('THIS IS shouldCustomizeQuote', data);
