f0b2abc95fc79e8941c4951468a2426ed3860988
HCI01\meads <meads@hcpci.com>
2017-03-02 15:20:31 -0500

    updated tests

---
+++ src/containers/Splash.spec.js
@@ -0,0 +1,26 @@
+// import React from 'react';
+// import { shallow, mount } from 'enzyme';
+// import { expect } from 'chai';
+// import configureStore from 'redux-mock-store';
+// import { Provider } from 'react-redux';
+// import Splash from './Splash';
+//
+// let wrapper;
+// const mockStore = configureStore([]);
+// const store = mockStore({
+//   features: { get() { } }
+// });
+//
+// describe('Splash', () => {
+//   beforeEach(() => {
+//     wrapper = mount(
+//       <Provider store={store}>
+//         <Splash />
+//       </Provider>
+//       );
+//   });
+//
+//   it('should render', () => {
+//     expect(wrapper).to.exist();
+//   });
+// });
