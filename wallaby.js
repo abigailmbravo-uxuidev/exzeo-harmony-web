b847853a161ea66b558fdee73c454aa19384f75c
dblbee <db.bee.me@gmail.com>
2017-04-12 10:39:54 -0400

    added wallaby config

--- wallaby.js
+++ wallaby.js
@@ -1,18 +1,28 @@
+
 module.exports = function (wallaby) {
+  var path = require('path');
+
+  // Babel, jest-cli and some other modules are located under
+  // react-scripts/node_modules, so need to let node.js know about it
+  process.env.NODE_PATH +=
+    path.delimiter +
+    path.join(__dirname, 'node_modules') +
+    path.delimiter +
+    path.join(__dirname, 'node_modules/react-scripts/node_modules');
+
+  require('module').Module._initPaths();
 
   // Babel needs this
   process.env.NODE_ENV = 'development';
 
   return {
     files: [
-      'src/**/*.js',
-      'src/**/*.jsx',
-      '!src/**/*.spec.js',
-      '!src/**/*.spec.jsx'
+      'src/**/*.js*',
+      '!src/**/*.spec.js'
     ],
-
-    tests: ['src/**/*.spec.js', 'src/**/*.spec.jsx'],
-
+    tests: [
+      'src/**/*.spec.js'
+    ],
     env: {
       type: 'node',
       runner: 'node'
@@ -22,12 +32,17 @@ module.exports = function (wallaby) {
       '**/*.js': wallaby.compilers.babel({
         babel: require('babel-core'),
         presets: ['react-app']
+      }),
+      '**/*.jsx': wallaby.compilers.babel({
+        babel: require('babel-core'),
+        presets: ['react-app']
       })
     },
 
-    setup: (wallaby) => {
+    setup: () => {
       wallaby.testFramework.configure({
         // as in node_modules/react-scripts/utils/createJestConfig.js
+        setupFiles: [require('path').join(wallaby.localProjectDir, 'node_modules/react-scripts/config/polyfills.js')],
         moduleNameMapper: {
           '^.+\\.(jpg|jpeg|png|gif|svg)$': require.resolve('react-scripts/config/jest/fileTransform.js'),
           '^.+\\.css$': require.resolve('react-scripts/config/jest/cssTransform.js')
