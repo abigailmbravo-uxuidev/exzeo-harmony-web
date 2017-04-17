5becd9fc9b5706169487a607a9ed085297392cdc
Joe Sutphin <jsutphin@exzeo.com>
2017-04-17 14:58:13 -0400

    updates to error html

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -15,41 +15,17 @@ const handleGetQuoteData = (state) => {
 };
 
 const Error = ({ quote, exceptions }) => {
-  
+
     return (
-        <div className="app-wrapper">
+        <div className="route-content">
             <div className="error-content" role="article">
                 {/* HARD STOP WORKFLOW ERROR*/}
-                <div className="quote-details-error">
-                    {quote && <section className="display-element">
-                        <dl className="quote-number">
-                            <div>
-                                <dt>Quote Number</dt>
-                                <dd>{quote.quoteNumber}</dd>
-                            </div>
-                        </dl>
-                    </section>
-}
-                    {quote.property && <section className="display-element">
-                        <dl className="property-information">
-                            <div>
-                                <dt>Property Address</dt>
-                                <dd>{quote.property.physicalAddress.address1}</dd>
-                                <dd>{quote.property.physicalAddress.address2}</dd>
-                                <dd>{`${quote.property.physicalAddress.city}, ${quote.property.physicalAddress.state} ${
-                                    quote.property.physicalAddress.zip}`}</dd>
-                            </div>
-                        </dl>
-                    </section>
-}
-                </div>
                 <div className="error-wrapper">
                     <section>
                         <div id="Error">
                             <div className="detail-wrapper">
-                                <h3 className="section-group-header error"><i className="fa fa-exclamation-triangle"/>
-                                    Please contact us</h3>
-                                <h4 className="error-message">Property does not qualify for automated quote</h4>
+                                <h3 className="section-group-header error"><i className="fa fa-exclamation-triangle"/> Please contact us</h3>
+                                <h4 className="error-message">This property does not qualify for automated quote</h4>
                                 <ul>
                                     {exceptions && exceptions.map((exception, key) => {
                                         if (exception.action === 'Fatal Error') {
@@ -65,14 +41,9 @@ const Error = ({ quote, exceptions }) => {
                     <aside>
                         <div className="image"/>
                         <div className="contact-info">
-                            <a className="link-email" href="mailto:customerservice@typtap.com"><i className="fa fa-envelope"/>
-                                <span>email us</span>
-                            </a>
-                            <a className="link-phone" href="tel:8442897968"><i className="fa fa-phone"/>
-                                <span>(844) 289-7968</span>
-                            </a>
+                            <a className="link-email" href="mailto:customerservice@typtap.com"><i className="fa fa-envelope"/> <span>email us</span></a>
+                            <a className="link-phone" href="tel:8442897968"><i className="fa fa-phone"/> <span>(844) 289-7968</span></a>
                         </div>
-                        <div className="waves"/>
                     </aside>
                 </div>
             </div>
