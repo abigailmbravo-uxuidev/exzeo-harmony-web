6adedff0ab7e33a4c6aa0fd2f61080f1a2c50e29
Joe Sutphin <jsutphin@exzeo.com>
2017-04-19 13:01:48 -0400

    updates to error html/css

--- src/containers/AppError.jsx
+++ src/containers/AppError.jsx
@@ -1,8 +1,8 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router';
-import { Link } from 'react-router-dom';
 
+import Header from '../components/Common/Header';
 import Footer from '../components/Common/Footer';
 
 export const AppError = (props, context) => {
@@ -10,33 +10,37 @@ export const AppError = (props, context) => {
     return <Redirect to={{ pathname: context.router.route.location.state.redirectUrl }} />;
   }
   return (
-    <div className="error-wrapper" role="article">
-      <div className="fade-in">
-        <div className="route">
-          <div className="error-content" role="article">
-            <div className="fade-in">
-              <section>
-                <div className="fade-in" id="Error">
-                  <div className="detail-wrapper">
-                    <div className="waves" />
-                    <div className="duckie" />
-                    <h3 className="section-group-header error"><i className="fa fa-exclamation-triangle" /> App Error</h3>
-                    <div className="header-wrapper">
-                      <div>
-                        <h4 className="error-message">OH NO! You&lsquo;ve flooded the website!</h4>
-                        <p>Just kidding, something has gone wrong. Please go the <Link to="/">Dashboard</Link> and start over.</p>
-                      </div>
-                    </div>
-                    <Link className="btn btn-secondary" to="/">Return to Dashboard</Link>
+<div className="app-wrapper error">
+        <Header />
+        <main role="document">
+                <div className="route">
+          <div className="route-content">
+              <div className="error-content" role="article">
+                  <div className="error-wrapper">
+                      <section>
+                          <div id="Error">
+                              <div className="detail-wrapper">
+                                  <h3 className="section-group-header error"><i className="fa fa-exclamation-triangle"/> Application Error</h3>
+                                  <h4>We're sorry, something seems to have went wrong.</h4>
+                                  <p>Please start your quote over or contact one of our representatives so they may further assist you in obtaining a HO3 insurance quote for this property.</p>
+                              </div>
+                          </div>
+                      </section>
+                      <aside>
+                          <div className="image"/>
+                          <div className="contact-info">
+                              <a className="link-email" href="mailto:customerservice@typtap.com"><i className="fa fa-envelope"/> <span>email us</span></a>
+                              <a className="link-phone" href="tel:8442897968"><i className="fa fa-phone"/> <span>(844) 289-7968</span></a>
+                          </div>
+                      </aside>
                   </div>
-                </div>
-              </section>
-            </div>
-            <Footer />
+              </div>
+              <Footer />
+
           </div>
-        </div>
-      </div>
-    </div>
+          </div>
+          </main>
+          </div>
   );
 };
 
