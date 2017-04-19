4c9ae621de3ea648811bdbfde4cbc671bea14d34
dblbee <db.bee.me@gmail.com>
2017-04-19 08:58:28 -0400

    added axios default header to the request for all authenticated calls

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import * as persistTypes from 'redux-persist/constants';
 import * as types from './../actions/actionTypes';
 import initialState from './initialState';
@@ -10,11 +11,15 @@ export default function userReducer(state = initialState.user, action) {
     case types.AUTHENTICATED:
       newState = { ...state, ...action.user };
       newState.error = undefined;
+      // add the auth header to every request
+      axios.defaults.headers.common['Authentication'] = `bearer ${action.user.token}`; // eslint-disable-line
       return newState;
     case types.AUTHENTICATE_ERROR:
       return { ...state, ...action.user };
     case persistTypes.REHYDRATE:
       newState = (action.payload && action.payload.user) ? action.payload.user : newState;
+      // add the auth header to every request
+      axios.defaults.headers.common['Authentication'] = `bearer ${action.payload.user.token}`; // eslint-disable-line
       return newState;
     default:
       return state;
