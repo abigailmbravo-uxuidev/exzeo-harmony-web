b85bf3eed3494ed81a370c49b4912edda1b0740b
jsutphin@exzeo.com <jsutphin@exzeo.com>
2017-05-15 20:53:00 +0000

    Merged in HAR-728 (pull request #135)
    HAR-728
    
    * Updated Retrieve Quote to handle Quote States
    
    * update spacing
    
    * update error screen
    
    * update flex properties of new div & add flex utility scss
    
    Approved-by: Bryan Boudreau <bboudreau@exzeo.com>
    Approved-by: Giri <graveendran@hcpci.com>
    Approved-by: Eric Lamison <elamison@exzeo.com>
    Approved-by: Lee Hull <lhull@hcpci.com>

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { propTypes } from 'redux-form';
 
 import Footer from '../Common/Footer';
 import * as cgActions from '../../actions/cgActions';
@@ -8,6 +9,7 @@ import * as appStateActions from '../../actions/appStateActions';
 import SearchBar from './SearchBar';
 import SearchResults from './SearchResults';
 import NoResultsConnect from './NoResults';
+import QuoteError from '../Common/QuoteError';
 
 const userTasks = {
   handleSelectAddress: 'chooseAddress',
@@ -27,15 +29,25 @@ const handleSelectAddress = (address, props) => {
 
 const handleSelectQuote = (quote, props) => {
   const workflowId = props.appState.instanceId;
-  const taskName = userTasks.handleSelectQuote;
-  const data = {
-    quoteId: quote._id
-  };
-  props.actions.appStateActions.setAppState(props.appState.modelName, workflowId, { submitting: true });
-  props.actions.cgActions.completeTask(props.appState.modelName, workflowId, taskName, data);
+
+  if (quote.quoteState === 'Quote Started' || quote.quoteState === 'Application Started' || quote.quoteState === 'Quote Stopped') {
+    const taskName = userTasks.handleSelectQuote;
+    const data = {
+      quoteId: quote._id
+    };
+    props.actions.appStateActions.setAppState(props.appState.modelName, workflowId, { submitting: true });
+    props.actions.cgActions.completeTask(props.appState.modelName, workflowId, taskName, data);
+  } else {
+    props.actions.appStateActions.setAppState(props.appState.modelName, workflowId, { showQuoteErrors: true, selectedQuote: quote });
+  }
 };
 
-const Search = () => (
+const closeQuoteError = (props) => {
+  props.actions.appStateActions.setAppState(props.appState.modelName, props.appState.instanceId, { showEmailPopup: false });
+};
+
+const Search = props => (
+<div className="flex grow">
   <div className="search route-content">
     <SearchBar />
     <div className="survey-wrapper scroll">
@@ -46,14 +58,22 @@ const Search = () => (
       <Footer />
     </div>
   </div>
+  {props.appState.data.showQuoteErrors &&
+    <QuoteError
+      quote={props.appState.data.selectedQuote || {}}
+      closeButtonHandler={() => closeQuoteError(props)}
+    />}
+</div>
 );
 
 Search.propTypes = {
+  ...propTypes,
   appState: PropTypes.shape({
     modelName: PropTypes.string,
     instanceId: PropTypes.string,
     workflowData: PropTypes.func
-  })
+  }),
+  showQuoteErrors: PropTypes.bool
 };
 
 const mapStateToProps = state => ({
