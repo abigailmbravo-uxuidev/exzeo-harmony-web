1aeff419df10019c3c00f710d80d523ae4cb6860
jsutphin@exzeo.com <jsutphin@exzeo.com>
2017-05-15 20:52:14 +0000

    Merged in HAR-1055 (pull request #131)
    HAR-1055
    
    * Displays menus on touch devices
    
    * Fixed some bugs.
    
    * add click outside functionality
    
    * update css to make click outside animate, fix phone link on small devices
    
    * adding blur class to blur background
    
    Approved-by: Giri <graveendran@hcpci.com>
    Approved-by: Robert Egnacheski <regnacheski@exzeo.com>
    Approved-by: Bryan Boudreau <bboudreau@exzeo.com>
    Approved-by: Lee Hull <lhull@hcpci.com>

--- src/containers/Base.jsx
+++ src/containers/Base.jsx
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
@@ -10,32 +10,57 @@ const handleLogout = (props) => {
   props.actions.user.logout();
 };
 
-export const Base = props => (
-  <div className="app-wrapper">
-    <Header />
-    <main role="document">
-      <aside className="content-panel-left">
-        <div className="user">
-          <label htmlFor="user">User</label>
-          <h5 className="user-name">
-            <span>TTIC20000@typtap.com</span>
-            <i className="fa fa-gear" />
-          </h5>
-        </div>
-        <SideNav />
-        <button className="btn logout btn-action" type="button" onClick={() => handleLogout(props)}>
-          <div>
-            <i className="fa fa-sign-out" />
-            <span>Logout</span>
+class Base extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      active: false,
+      headerActive: false
+    };
+    this.toggleClass = this.toggleClass.bind(this);
+    this.toggleClassHeader = this.toggleClassHeader.bind(this);
+  }
+  toggleClass() {
+    const currentState = this.state.active;
+    this.setState({ headerActive: false, active: !currentState });
+  }
+
+  toggleClassHeader() {
+    const currentStateHeader = this.state.headerActive;
+    this.setState({ headerActive: !currentStateHeader, active: false });
+  }
+
+  render() {
+    return (
+      <div className={this.state.headerActive ? 'app-wrapper blur' : 'app-wrapper'} >
+        <Header toggleHeader={this.toggleClassHeader} toggle={this.toggleClass} active={this.state.active} />
+        <main role="document">
+          <aside activeClassName="active" className={this.state.headerActive ? 'content-panel-left active' : 'content-panel-left'}>
+            <div className="user">
+              <label htmlFor="user">User</label>
+              <h5 className="user-name">
+                <span>TTIC20000@typtap.com</span>
+                <i className="fa fa-gear" />
+              </h5>
+            </div>
+            <SideNav />
+            <button className="btn logout btn-action" type="button" onClick={() => handleLogout(this.props)}>
+              <div>
+                <i className="fa fa-sign-out" />
+                <span>Logout</span>
+              </div>
+            </button>
+          </aside>
+          <div className={this.state.headerActive ? 'aside-modal active' : 'aside-modal'} onClick={this.toggleClassHeader}></div>
+          <div className="content-wrapper">
+            {this.props.children}
           </div>
-        </button>
-      </aside>
-      <div className="content-wrapper">
-        {props.children}
+        </main>
       </div>
-    </main>
-  </div>
-);
+    );
+  }
+}
 
 
 Base.propTypes = {
