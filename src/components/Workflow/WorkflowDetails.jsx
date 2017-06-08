0408c73fc00bb93a383360fc880d9e0795f13479
Justin Mitchell <jmitchell@exzeo.com>
2017-06-08 16:05:49 -0400

    code cleanup

--- src/components/Workflow/WorkflowDetails.jsx
+++ src/components/Workflow/WorkflowDetails.jsx
@@ -1,19 +1,10 @@
-import React, {
-  Component,
-  PropTypes
-} from 'react';
-import {
-  bindActionCreators
-} from 'redux';
-import {
-  connect
-} from 'react-redux';
+import React, { Component, PropTypes } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import _ from 'lodash';
 import * as cgActions from '../../actions/cgActions';
 import * as appStateActions from '../../actions/appStateActions';
 import * as completedTasksActions from '../../actions/completedTasksActions';
-import TaskRunnerConnect from '../Workflow/TaskRunner';
-// import Loader from '../Common/Loader';
 
 const workflowDetailsModelName = 'quoteModelGetQuote';
 
@@ -35,7 +26,6 @@ const goToStep = (props, taskName) => {
   // don't allow submission until the other step is completed
   if (props.appState.data.submitting) return;
 
-  // props.actions.appStateActions.setAppState(props.appState.modelName, props.appState.instanceId, { ...props.appState.data, submitting: true });
   const currentData = props.tasks && props.tasks[props.workflowModelName].data ? props.tasks[props.workflowModelName].data : {};
 
   if ((currentData && currentData.activeTask && currentData.activeTask.name !== taskName) &&
