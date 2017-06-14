01c0dff9b9aeb8dcc66c189cc9baaada206a81f5
dblbee <db.bee.me@gmail.com>
2017-06-14 17:11:56 -0400

    updated the login to use the Ashton auth0 solution

--- src/containers/Quote.jsx
+++ src/containers/Quote.jsx
@@ -3,6 +3,6 @@ import React from 'react';
 import WorkflowConnect from '../components/Workflow/Workflow';
 import BaseConnect from './Base';
 
-const Quote = () => <BaseConnect><div className="workflow" role="article"><WorkflowConnect /></div></BaseConnect>;
+const Quote = props => <BaseConnect {...props} ><div className="workflow" role="article"><WorkflowConnect /></div></BaseConnect>;
 
 export default Quote;
