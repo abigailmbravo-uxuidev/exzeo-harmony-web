d12812dc3b6535d98c0b71d4a7d3d00554f9dc81
dblbee <db.bee.me@gmail.com>
2017-06-20 18:04:58 -0400

    updated the axios header for the id token

--- src/Auth.js
+++ src/Auth.js
@@ -1,4 +1,5 @@
 import auth0 from 'auth0-js';
+import axios from 'axios';
 
 import history from './history';
 
@@ -45,6 +46,7 @@ export default class Auth {
     localStorage.setItem('access_token', authResult.accessToken);
     localStorage.setItem('id_token', authResult.idToken);
     localStorage.setItem('expires_at', expiresAt);
+    axios.defaults.headers.common['authorization'] = `bearer ${authResult.idToken}`; // eslint-disable-line
     // navigate to the home route
     history.replace('/');
   }
@@ -87,6 +89,7 @@ export default class Auth {
     localStorage.removeItem('id_token');
     localStorage.removeItem('expires_at');
     this.userProfile = null;
+    axios.defaults.headers.common['authorization'] = undefined; // eslint-disable-line
     // navigate to the home route
     history.replace('/');
   }
