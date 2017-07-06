7d76884091a71597ab4cb83ed54692ee04a47a2b
elamison <eric@lamison.com>
2017-07-06 10:35:49 -0400

    Removed an action call causing a recursive loop

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -2,7 +2,6 @@ import * as types from './../actions/actionTypes';
 import initialState from './initialState';
 
 export default function authReducer(state = initialState.authState, action) {
-  let newState = {};
   switch (action.type) {
     case types.AUTH:
       const newState = { ...state, ...action.authState };
