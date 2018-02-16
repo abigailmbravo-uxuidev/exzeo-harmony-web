ed51861857e468eee91815601daef09a39a5ad22
Mark Eads <meads@hcpci.com>
2018-02-16 10:19:58 -0500

    removed lodash

--- src/components/Form/inputs/SelectFieldBilling.jsx
+++ src/components/Form/inputs/SelectFieldBilling.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import _ from 'lodash';
 import FieldHint from './FieldHint';
 import reduxFormField from './reduxFormField';
 
