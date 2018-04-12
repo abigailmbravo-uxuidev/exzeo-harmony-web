77d9fb367ebe89e47f2ee77eb741156283e2fa47
Joe Sutphin <jsutphin@exzeo.com>
2018-04-12 10:43:05 -0400

    add smooth scroll

--- src/components/Common/reduxFormFailSubmit.js
+++ src/components/Common/reduxFormFailSubmit.js
@@ -3,11 +3,10 @@ const failedSubmission = (errors, dispatch, submitError, props) => {
   props.actions.appStateActions.setAppState(props.appState.modelName, workflowId, { ...props.appState.data, showSnackBar: true });
   const errorKeys = Object.keys(errors);
   const elem = document.querySelector(`#${errorKeys[0]}`);
-  if (elem) elem.scrollIntoView();
+  if (elem) elem.scrollIntoView({ block: 'end',  behavior: 'smooth' });
   setTimeout(() => {
     props.actions.appStateActions.setAppState(props.appState.modelName, workflowId, { ...props.appState.data, showSnackBar: false });
   }, 3000);
 };
 
 export default failedSubmission;
-
