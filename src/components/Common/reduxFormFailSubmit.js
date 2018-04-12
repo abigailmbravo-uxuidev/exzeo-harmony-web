710bfacfe3f8f2f3688ef6e465f86fe9ab8e3693
Mark Eads <meads@hcpci.com>
2018-04-12 10:21:28 -0400

    elem.scrollIntoView

--- src/components/Common/reduxFormFailSubmit.js
+++ src/components/Common/reduxFormFailSubmit.js
@@ -1,6 +1,9 @@
 const failedSubmission = (errors, dispatch, submitError, props) => {
   const workflowId = props.appState.instanceId;
   props.actions.appStateActions.setAppState(props.appState.modelName, workflowId, { ...props.appState.data, showSnackBar: true });
+  const errorKeys = Object.keys(errors);
+  const elem = document.querySelector(`#${errorKeys[0]}`);
+  if (elem) elem.scrollIntoView();
   setTimeout(() => {
     props.actions.appStateActions.setAppState(props.appState.modelName, workflowId, { ...props.appState.data, showSnackBar: false });
   }, 3000);
