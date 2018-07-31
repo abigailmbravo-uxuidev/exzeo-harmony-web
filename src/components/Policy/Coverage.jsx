7d39b2cf01c399fa6da9447faa5e8386ba739948
Mark Eads <meads@hcpci.com>
2018-07-31 15:28:00 -0400

    updated property and coverage

--- src/components/Policy/Coverage.jsx
+++ src/components/Policy/Coverage.jsx
@@ -1,33 +1,114 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import _ from 'lodash';
 import moment from 'moment';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
-import * as policyStateActions from '../../actions/policyStateActions';
-import * as serviceActions from '../../actions/serviceActions';
-import * as errorActions from '../../actions/errorActions';
-import Downloader from '../Common/Downloader';
+import normalizeNumbers from '../Form/normalizeNumbers';
 import Loader from '../Common/Loader';
 import PolicyTabs from '../Common/PolicyTabs';
 
 export const dateFormatter = cell => `${moment.unix(cell).format('MM/DD/YYYY')}`;
 export const nameFormatter = cell => `${String(cell.match(/^(.+?)-/g)).replace('-', '')}`;
 
-export class Coverage extends Component {
-  render() {
-    const { policy, policyNumber } = this.props;
-    if (!policy || !policy.policyID) {
-      return (<Loader />);
-    }
-
-    return (
-      <React.Fragment>
-        <PolicyTabs activeTab="coverage" policyNumber={policyNumber} />
-      </React.Fragment>);
+export const Coverage = ({ policy, policyNumber }) => {
+  if (!policy || !policy.policyID) {
+    return (<Loader />);
   }
-}
+
+  const { property, underwritingAnswers, rating, coverageLimits, coverageOptions, deductibles } = policy;
+  const monthsOccupied = underwritingAnswers ? underwritingAnswers.monthsOccupied.answer : null;
+  const everRented = underwritingAnswers ? underwritingAnswers.rented.answer : 'No';
+  const noPriorIns = underwritingAnswers ? underwritingAnswers.noPriorInsuranceSurcharge.answer : 'No';
+  const windMitFactor = rating ? rating.worksheet.elements.windMitigationFactors.windMitigationDiscount : '0';
+
+  return (
+    <React.Fragment>
+      <PolicyTabs activeTab="coverage" policyNumber={policyNumber} />
+      <section>
+        <h3>Coverage Limits</h3>
+        <div className="property-info">
+          <dl>
+            <div>
+              <dt className="yearHomeBuilt">A. Dwelling</dt>
+              <dd className="yearHomeBuilt">{`$ ${normalizeNumbers(coverageLimits.dwelling.amount)}`}</dd>
+              <dt className="construction">B. Other Structures</dt>
+              <dd className="construction">{`$ ${normalizeNumbers(coverageLimits.otherStructures.amount)}`}</dd>
+              <dt className="yearRoofBuilt">C. Personal Property</dt>
+              <dd className="yearRoofBuilt">{`$ ${normalizeNumbers(coverageLimits.personalProperty.amount)}`}</dd>
+              <dt className="yearRoofBuilt">D. Loss of Use</dt>
+              <dd className="yearRoofBuilt">{`$ ${normalizeNumbers(coverageLimits.lossOfUse.amount)}`}</dd>
+              <dt className="yearRoofBuilt">E. Personal Liability</dt>
+              <dd className="yearRoofBuilt">{`$ ${normalizeNumbers(coverageLimits.personalLiability.amount)}`}</dd>
+              <dt className="yearRoofBuilt">F. Medical Payments</dt>
+              <dd className="yearRoofBuilt">{ `$ ${normalizeNumbers(coverageLimits.medicalPayments.amount)}`}</dd>
+              <dt className="yearRoofBuilt">Personal Property Replacement Cost</dt>
+              <dd className="yearRoofBuilt">{coverageOptions.personalPropertyReplacementCost.answer ? 'Yes' : 'No'}</dd>
+            </div>
+          </dl>
+          <dl>
+            <div>
+              <dt className="moldProperty">Mold Property</dt>
+              <dd className="moldProperty">{`$ ${normalizeNumbers(coverageLimits.moldProperty.amount)}`}</dd>
+              <dt className="moldLiability">Mold Liability</dt>
+              <dd className="moldLiability">{`$ ${normalizeNumbers(coverageLimits.moldLiability.amount)}`}</dd>
+              <dt className="ordinanceOrLaw">Ordinance or Law</dt>
+              <dd className="ordinanceOrLaw">{`${coverageLimits.ordinanceOrLaw.amount}%`}</dd>
+              <dt className="allOtherPerils">All other Perils Deductible</dt>
+              <dd className="allOtherPerils">{ `$ ${normalizeNumbers(deductibles.allOtherPerils.amount)}`}</dd>
+              <dt className="hurricane">Hurricane Deductible</dt>
+              <dd className="hurricane">{`$ ${normalizeNumbers(deductibles.hurricane.calculatedAmount)}`}</dd>
+              <dt className="sinkhole">Sinkhole Deductible</dt>
+              <dd className="sinkhole">{deductibles.sinkhole.calculatedAmount ? `$ ${normalizeNumbers(deductibles.sinkhole.calculatedAmount)}` : '$ 0'}</dd>
+            </div>
+          </dl>
+        </div>
+      </section>
+      <section>
+        <h3>Discount / Surcharge</h3>
+        <div className="property-info">
+          <dl>
+            <div>
+              <dt className="townhouseRowhouse">Townhouse/Rowhouse</dt>
+              <dd className="townhouseRowhouse">{property.townhouseRowhouse === false ? 'No' : 'Yes'}</dd>
+              <dt className="everRented">Property Ever Rented</dt>
+              <dd className="everRented">{everRented}</dd>
+              <dt className="monthsOccupied">Seasonally Occupied</dt>
+              <dd className="monthsOccupied">{monthsOccupied === '10+' || monthsOccupied === '7-9' ? 'No' : 'Yes'}</dd>
+              <dt className="noPriorIns">No Prior Insurance</dt>
+              <dd className="noPriorIns">{noPriorIns}</dd>
+            </div>
+          </dl>
+          <dl>
+            <div>
+              <dt className="burglarAlarm">Burglar Alarm</dt>
+              <dd className="burglarAlarm">{property.burglarAlarm ? 'Yes' : 'No'}</dd>
+              <dt className="fireAlarm">Fire Alarm</dt>
+              <dd className="fireAlarm">{property.fireAlarm ? 'Yes' : 'No'}</dd>
+              <dt className="sprinkler">Sprinkler</dt>
+              <dd className="sprinkler">{property.sprinkler === 'N' ? 'No' : property.sprinkler}</dd>
+              <dt className="windMitFactor">Wind Mit Facor</dt>
+              <dd className="windMitFactor">{windMitFactor}</dd>
+            </div>
+          </dl>
+        </div>
+      </section>
+      <section>
+        <h3>Coverage Limits</h3>
+        <div className="property-info">
+          <dl>
+            <div>
+              <dt className="allOtherPerils">All other Perils</dt>
+              <dd className="allOtherPerils">{ `$ ${normalizeNumbers(deductibles.allOtherPerils.amount)}`}</dd>
+              <dt className="hurricane">Hurricane Deductible</dt>
+              <dd className="hurricane">{`${normalizeNumbers(deductibles.hurricane.amount)}%`}</dd>
+              <dt className="sinkhole">Sinkhole Deductible</dt>
+              <dd className="sinkhole">{deductibles.sinkhole.amount ? `${normalizeNumbers(deductibles.sinkhole.amount)}%` : 'No'}</dd>
+            </div>
+          </dl>
+        </div>
+      </section>
+    </React.Fragment>);
+};
 
 Coverage.contextTypes = {
   router: PropTypes.object
--- src/components/Policy/Property.jsx
+++ src/components/Policy/Property.jsx
@@ -1,32 +1,116 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import _ from 'lodash';
+import get from 'lodash/get';
 import moment from 'moment';
-import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
-import * as policyStateActions from '../../actions/policyStateActions';
-import * as serviceActions from '../../actions/serviceActions';
-import * as errorActions from '../../actions/errorActions';
 import Loader from '../Common/Loader';
 import PolicyTabs from '../Common/PolicyTabs';
+import normalizeNumbers from '../Form/normalizeNumbers';
 
 export const dateFormatter = cell => `${moment.unix(cell).format('MM/DD/YYYY')}`;
 export const nameFormatter = cell => `${String(cell.match(/^(.+?)-/g)).replace('-', '')}`;
 
-export class Property extends Component {
-  render() {
-    const { policy, policyNumber } = this.props;
-    if (!policy || !policy.policyID) {
-      return (<Loader />);
-    }
-
-    return (
-      <React.Fragment>
-        <PolicyTabs activeTab="property" policyNumber={policyNumber} />
-      </React.Fragment>);
+export const Property = ({ policy, policyNumber }) => {
+  if (!policy || !policy.policyID) {
+    return (<Loader />);
   }
-}
+  const { property, rating } = policy;
+
+  return (
+    <React.Fragment>
+      <PolicyTabs activeTab="property" policyNumber={policyNumber} />
+      <section>
+        <h3>Home and Location</h3>
+        <div className="property-info">
+          <dl>
+            <div>
+              <dt className="yearHomeBuilt">Year Home Built</dt>
+              <dd className="yearHomeBuilt">{property.yearBuilt}</dd>
+              <dt className="construction">Construction</dt>
+              <dd className="construction">{property.constructionType}</dd>
+              <dt className="yearRoofBuilt">Year Roof Built</dt>
+              <dd className="yearRoofBuilt">{property.yearOfRoof}</dd>
+            </div>
+          </dl>
+          <dl>
+            <div>
+              <dt className="protectionClass">Protection Class</dt>
+              <dd className="protectionClass">{property.protectionClass}</dd>
+              <dt className="BCEG">BCEG</dt>
+              <dd className="BCEG">{property.buildingCodeEffectivenessGrading}</dd>
+              <dt className="familyUnits">Family Units</dt>
+              <dd className="familyUnits">{property.familyUnits}</dd>
+            </div>
+          </dl>
+          <dl>
+            <div>
+              <dt className="distToTidalWaters">Dist. to Tidal Waters</dt>
+              <dd className="distToTidalWaters">{normalizeNumbers(property.distanceToTidalWater)} ft.</dd>
+              <dt className="distToFireHydrant">Dist. to Fire Hydrant</dt>
+              <dd className="distToFireHydrant">{property.distanceToFireHydrant ? `${normalizeNumbers(property.distanceToFireHydrant)} ft.` : '-'}</dd>
+              <dt className="distToFireStation">Dist. to Fire Station</dt>
+              <dd className="distToFireStation">{property.distanceToFireStation} mi.</dd>
+            </div>
+          </dl>
+          <dl>
+            <div>
+              <dt className="residenceType">Residence Type</dt>
+              <dd className="residenceType">{property.residenceType}</dd>
+              <dt className="squareFootage">Square Footage</dt>
+              <dd className="squareFootage">{property.squareFeet}</dd>
+              <dt className="floodZone">Flood Zone</dt>
+              <dd className="floodZone">{property.floodZone}</dd>
+            </div>
+          </dl>
+        </div>
+      </section>
+      <section>
+        <h3>Wind Mitigation</h3>
+        <div className="wind-mitigation">
+          <dl>
+            <div>
+              <dt className="roofCovering">Roof Covering</dt>
+              <dd className="roofCovering">{property.windMitigation.roofCovering}</dd>
+              <dt className="roofDeckAttachment">Roof Deck Attachment</dt>
+              <dd className="roofDeckAttachment">{property.windMitigation.roofDeckAttachment}</dd>
+              <dt className="roofToWallAttachment">Roof to Wall Attachment</dt>
+              <dd className="roofToWallAttachment">{property.windMitigation.roofToWallConnection}</dd>
+            </div>
+          </dl>
+          <dl>
+            <div>
+              <dt className="roofGeometry">Roof Geometry</dt>
+              <dd className="roofGeometry">{property.windMitigation.roofGeometry}</dd>
+              <dt className="SWR">Secondary Water Resistance (SWR)</dt>
+              <dd className="SWR">{property.windMitigation.secondaryWaterResistance}</dd>
+              <dt className="openingProtection">Opening Protection</dt>
+              <dd className="openingProtection">{property.windMitigation.openingProtection}</dd>
+            </div>
+          </dl>
+          <dl>
+            <div>
+              <dt className="windSpeed">FBC Wind Speed</dt>
+              <dd className="windSpeed">{property.windMitigation.floridaBuildingCodeWindSpeed}</dd>
+              <dt className="windSpeedDesign">FBC Wind Speed Design</dt>
+              <dd className="windSpeedDesign">{property.windMitigation.floridaBuildingCodeWindSpeedDesign}</dd>
+              <dt className="terrain">Terrain</dt>
+              <dd className="terrain">{property.windMitigation.terrain}</dd>
+            </div>
+          </dl>
+          <dl>
+            <div>
+              <dt className="internalPressureDesign">Internal Pressure Design</dt>
+              <dd className="internalPressureDesign">{property.windMitigation.internalPressureDesign}</dd>
+              <dt className="WBDR">Wind Borne Debris Region (WBDR)</dt>
+              <dd className="WBDR">{property.windMitigation.windBorneDebrisRegion}</dd>
+              <dt className="windMitFactor">Wind Mit Factor</dt>
+              <dd className="windMitFactor">{get(rating, 'worksheet.elements.windMitigationFactors.windMitigationDiscount') || 0}</dd>
+            </div>
+          </dl>
+        </div>
+      </section>
+    </React.Fragment>);
+};
 
 Property.contextTypes = {
   router: PropTypes.object
