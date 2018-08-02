9421f30dcd324450d31e85a18e7cb5501418cfba
Mark Eads <meads@hcpci.com>
2018-08-02 08:40:48 -0400

    updated style of sections

--- src/components/Policy/Coverage.jsx
+++ src/components/Policy/Coverage.jsx
@@ -16,89 +16,95 @@ export const Coverage = ({ policy, policyNumber }) => {
   return (
     <React.Fragment>
       <PolicyTabs activeTab="coverage" policyNumber={policyNumber} />
-      <section>
-        <h3>Coverage Limits</h3>
-        <div className="property-info">
-          <dl>
-            <div>
-              <dt className="yearHomeBuilt">A. Dwelling</dt>
-              <dd className="yearHomeBuilt">{`$ ${numbers(coverageLimits.dwelling.amount)}`}</dd>
-              <dt className="construction">B. Other Structures</dt>
-              <dd className="construction">{`$ ${numbers(coverageLimits.otherStructures.amount)}`}</dd>
-              <dt className="yearRoofBuilt">C. Personal Property</dt>
-              <dd className="yearRoofBuilt">{`$ ${numbers(coverageLimits.personalProperty.amount)}`}</dd>
-              <dt className="yearRoofBuilt">D. Loss of Use</dt>
-              <dd className="yearRoofBuilt">{`$ ${numbers(coverageLimits.lossOfUse.amount)}`}</dd>
-              <dt className="yearRoofBuilt">E. Personal Liability</dt>
-              <dd className="yearRoofBuilt">{`$ ${numbers(coverageLimits.personalLiability.amount)}`}</dd>
-              <dt className="yearRoofBuilt">F. Medical Payments</dt>
-              <dd className="yearRoofBuilt">{ `$ ${numbers(coverageLimits.medicalPayments.amount)}`}</dd>
-              <dt className="yearRoofBuilt">Personal Property Replacement Cost</dt>
-              <dd className="yearRoofBuilt">{coverageOptions.personalPropertyReplacementCost.answer ? 'Yes' : 'No'}</dd>
+      <div className="route-content verify">
+        <div className="scroll">
+          <div className="detail-wrapper">
+            <div className="detail-group property-details">
+              <section className="display-element">
+                <h3 className="section-group-header"><i className="fa fa-line-chart" /> Coverage Limits</h3>
+                <dl>
+                  <div>
+                    <dt className="yearHomeBuilt">A. Dwelling</dt>
+                    <dd className="yearHomeBuilt">{`$ ${numbers(coverageLimits.dwelling.amount)}`}</dd>
+                    <dt className="construction">B. Other Structures</dt>
+                    <dd className="construction">{`$ ${numbers(coverageLimits.otherStructures.amount)}`}</dd>
+                    <dt className="yearRoofBuilt">C. Personal Property</dt>
+                    <dd className="yearRoofBuilt">{`$ ${numbers(coverageLimits.personalProperty.amount)}`}</dd>
+                    <dt className="yearRoofBuilt">D. Loss of Use</dt>
+                    <dd className="yearRoofBuilt">{`$ ${numbers(coverageLimits.lossOfUse.amount)}`}</dd>
+                    <dt className="yearRoofBuilt">E. Personal Liability</dt>
+                    <dd className="yearRoofBuilt">{`$ ${numbers(coverageLimits.personalLiability.amount)}`}</dd>
+                    <dt className="yearRoofBuilt">F. Medical Payments</dt>
+                    <dd className="yearRoofBuilt">{ `$ ${numbers(coverageLimits.medicalPayments.amount)}`}</dd>
+                    <dt className="yearRoofBuilt">Personal Property Replacement Cost</dt>
+                    <dd className="yearRoofBuilt">{coverageOptions.personalPropertyReplacementCost.answer ? 'Yes' : 'No'}</dd>
+                  </div>
+                </dl>
+                <dl>
+                  <div>
+                    <dt className="moldProperty">Mold Property</dt>
+                    <dd className="moldProperty">{`$ ${numbers(coverageLimits.moldProperty.amount)}`}</dd>
+                    <dt className="moldLiability">Mold Liability</dt>
+                    <dd className="moldLiability">{`$ ${numbers(coverageLimits.moldLiability.amount)}`}</dd>
+                    <dt className="ordinanceOrLaw">Ordinance or Law</dt>
+                    <dd className="ordinanceOrLaw">{`${coverageLimits.ordinanceOrLaw.amount}%`}</dd>
+                    <dt className="allOtherPerils">All other Perils Deductible</dt>
+                    <dd className="allOtherPerils">{ `$ ${numbers(deductibles.allOtherPerils.amount)}`}</dd>
+                    <dt className="hurricane">Hurricane Deductible</dt>
+                    <dd className="hurricane">{`$ ${numbers(deductibles.hurricane.calculatedAmount)}`}</dd>
+                    <dt className="sinkhole">Sinkhole Deductible</dt>
+                    <dd className="sinkhole">{deductibles.sinkhole.calculatedAmount ? `$ ${numbers(deductibles.sinkhole.calculatedAmount)}` : '$ 0'}</dd>
+                  </div>
+                </dl>
+              </section>
             </div>
-          </dl>
-          <dl>
-            <div>
-              <dt className="moldProperty">Mold Property</dt>
-              <dd className="moldProperty">{`$ ${numbers(coverageLimits.moldProperty.amount)}`}</dd>
-              <dt className="moldLiability">Mold Liability</dt>
-              <dd className="moldLiability">{`$ ${numbers(coverageLimits.moldLiability.amount)}`}</dd>
-              <dt className="ordinanceOrLaw">Ordinance or Law</dt>
-              <dd className="ordinanceOrLaw">{`${coverageLimits.ordinanceOrLaw.amount}%`}</dd>
-              <dt className="allOtherPerils">All other Perils Deductible</dt>
-              <dd className="allOtherPerils">{ `$ ${numbers(deductibles.allOtherPerils.amount)}`}</dd>
-              <dt className="hurricane">Hurricane Deductible</dt>
-              <dd className="hurricane">{`$ ${numbers(deductibles.hurricane.calculatedAmount)}`}</dd>
-              <dt className="sinkhole">Sinkhole Deductible</dt>
-              <dd className="sinkhole">{deductibles.sinkhole.calculatedAmount ? `$ ${numbers(deductibles.sinkhole.calculatedAmount)}` : '$ 0'}</dd>
+            <div className="detail-group property-details">
+              <section className="display-element">
+                <h3 className="section-group-header"><i className="fa fa-shopping-cart" /> Discount / Surcharge</h3>
+                <dl>
+                  <div>
+                    <dt className="townhouseRowhouse">Townhouse/Rowhouse</dt>
+                    <dd className="townhouseRowhouse">{property.townhouseRowhouse === false ? 'No' : 'Yes'}</dd>
+                    <dt className="everRented">Property Ever Rented</dt>
+                    <dd className="everRented">{everRented}</dd>
+                    <dt className="monthsOccupied">Seasonally Occupied</dt>
+                    <dd className="monthsOccupied">{monthsOcc === '10+' || monthsOcc === '7-9' ? 'No' : 'Yes'}</dd>
+                    <dt className="noPriorIns">No Prior Insurance</dt>
+                    <dd className="noPriorIns">{noPriorIns}</dd>
+                  </div>
+                </dl>
+                <dl>
+                  <div>
+                    <dt className="burglarAlarm">Burglar Alarm</dt>
+                    <dd className="burglarAlarm">{property.burglarAlarm ? 'Yes' : 'No'}</dd>
+                    <dt className="fireAlarm">Fire Alarm</dt>
+                    <dd className="fireAlarm">{property.fireAlarm ? 'Yes' : 'No'}</dd>
+                    <dt className="sprinkler">Sprinkler</dt>
+                    <dd className="sprinkler">{property.sprinkler === 'N' ? 'No' : property.sprinkler}</dd>
+                    <dt className="windMitFactor">Wind Mit Facor</dt>
+                    <dd className="windMitFactor">{windMitFactor}</dd>
+                  </div>
+                </dl>
+              </section>
             </div>
-          </dl>
+            <div className="detail-group property-details">
+              <section className="display-element">
+                <h3 className="section-group-header"><i className="fa fa-long-arrow-down" /> Deductible</h3>
+                <dl>
+                  <div>
+                    <dt className="allOtherPerils">All other Perils</dt>
+                    <dd className="allOtherPerils">{ `$ ${numbers(deductibles.allOtherPerils.amount)}`}</dd>
+                    <dt className="hurricane">Hurricane Deductible</dt>
+                    <dd className="hurricane">{`${numbers(deductibles.hurricane.amount)}%`}</dd>
+                    <dt className="sinkhole">Sinkhole Deductible</dt>
+                    <dd className="sinkhole">{deductibles.sinkhole.amount ? `${numbers(deductibles.sinkhole.amount)}%` : 'No'}</dd>
+                  </div>
+                </dl>
+              </section>
+            </div>
+          </div>
         </div>
-      </section>
-      <section>
-        <h3>Discount / Surcharge</h3>
-        <div className="property-info">
-          <dl>
-            <div>
-              <dt className="townhouseRowhouse">Townhouse/Rowhouse</dt>
-              <dd className="townhouseRowhouse">{property.townhouseRowhouse === false ? 'No' : 'Yes'}</dd>
-              <dt className="everRented">Property Ever Rented</dt>
-              <dd className="everRented">{everRented}</dd>
-              <dt className="monthsOccupied">Seasonally Occupied</dt>
-              <dd className="monthsOccupied">{monthsOcc === '10+' || monthsOcc === '7-9' ? 'No' : 'Yes'}</dd>
-              <dt className="noPriorIns">No Prior Insurance</dt>
-              <dd className="noPriorIns">{noPriorIns}</dd>
-            </div>
-          </dl>
-          <dl>
-            <div>
-              <dt className="burglarAlarm">Burglar Alarm</dt>
-              <dd className="burglarAlarm">{property.burglarAlarm ? 'Yes' : 'No'}</dd>
-              <dt className="fireAlarm">Fire Alarm</dt>
-              <dd className="fireAlarm">{property.fireAlarm ? 'Yes' : 'No'}</dd>
-              <dt className="sprinkler">Sprinkler</dt>
-              <dd className="sprinkler">{property.sprinkler === 'N' ? 'No' : property.sprinkler}</dd>
-              <dt className="windMitFactor">Wind Mit Facor</dt>
-              <dd className="windMitFactor">{windMitFactor}</dd>
-            </div>
-          </dl>
-        </div>
-      </section>
-      <section>
-        <h3>Coverage Limits</h3>
-        <div className="property-info">
-          <dl>
-            <div>
-              <dt className="allOtherPerils">All other Perils</dt>
-              <dd className="allOtherPerils">{ `$ ${numbers(deductibles.allOtherPerils.amount)}`}</dd>
-              <dt className="hurricane">Hurricane Deductible</dt>
-              <dd className="hurricane">{`${numbers(deductibles.hurricane.amount)}%`}</dd>
-              <dt className="sinkhole">Sinkhole Deductible</dt>
-              <dd className="sinkhole">{deductibles.sinkhole.amount ? `${numbers(deductibles.sinkhole.amount)}%` : 'No'}</dd>
-            </div>
-          </dl>
-        </div>
-      </section>
+      </div>
     </React.Fragment>);
 };
 
--- src/components/Policy/Property.jsx
+++ src/components/Policy/Property.jsx
@@ -1,103 +1,111 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { normalize } from '@exzeo/core-ui/lib/InputLifecycle/index';
 import PolicyTabs from '../Common/PolicyTabs';
-import normalizeNumbers from '../Form/normalizeNumbers';
+
+const { numbers } = normalize;
 
 export const Property = ({ policy, policyNumber }) => {
   const { property, rating } = policy;
   return (
     <React.Fragment>
       <PolicyTabs activeTab="property" policyNumber={policyNumber} />
-      <section>
-        <h3>Home and Location</h3>
-        <div className="property-info">
-          <dl>
-            <div>
-              <dt className="yearHomeBuilt">Year Home Built</dt>
-              <dd className="yearHomeBuilt">{property.yearBuilt}</dd>
-              <dt className="construction">Construction</dt>
-              <dd className="construction">{property.constructionType}</dd>
-              <dt className="yearRoofBuilt">Year Roof Built</dt>
-              <dd className="yearRoofBuilt">{property.yearOfRoof}</dd>
+      <div className="route-content verify">
+        <div className="scroll">
+          <div className="detail-wrapper">
+            <div className="detail-group property-details">
+              <section className="display-element">
+                <h3 className="section-group-header"><i className="fa fa-map-marker" /> Home and Location</h3>
+                <dl>
+                  <div>
+                    <dt className="yearHomeBuilt">Year Home Built</dt>
+                    <dd className="yearHomeBuilt">{property.yearBuilt}</dd>
+                    <dt className="construction">Construction</dt>
+                    <dd className="construction">{property.constructionType}</dd>
+                    <dt className="yearRoofBuilt">Year Roof Built</dt>
+                    <dd className="yearRoofBuilt">{property.yearOfRoof}</dd>
+                  </div>
+                </dl>
+                <dl>
+                  <div>
+                    <dt className="protectionClass">Protection Class</dt>
+                    <dd className="protectionClass">{property.protectionClass}</dd>
+                    <dt className="BCEG">BCEG</dt>
+                    <dd className="BCEG">{property.buildingCodeEffectivenessGrading}</dd>
+                    <dt className="familyUnits">Family Units</dt>
+                    <dd className="familyUnits">{property.familyUnits}</dd>
+                  </div>
+                </dl>
+                <dl>
+                  <div>
+                    <dt className="distToTidalWaters">Dist. to Tidal Waters</dt>
+                    <dd className="distToTidalWaters">{numbers(property.distanceToTidalWater)} ft.</dd>
+                    <dt className="distToFireHydrant">Dist. to Fire Hydrant</dt>
+                    <dd className="distToFireHydrant">{property.distanceToFireHydrant ? `${numbers(property.distanceToFireHydrant)} ft.` : '-'}</dd>
+                    <dt className="distToFireStation">Dist. to Fire Station</dt>
+                    <dd className="distToFireStation">{property.distanceToFireStation} mi.</dd>
+                  </div>
+                </dl>
+                <dl>
+                  <div>
+                    <dt className="residenceType">Residence Type</dt>
+                    <dd className="residenceType">{property.residenceType}</dd>
+                    <dt className="squareFootage">Square Footage</dt>
+                    <dd className="squareFootage">{property.squareFeet}</dd>
+                    <dt className="floodZone">Flood Zone</dt>
+                    <dd className="floodZone">{property.floodZone}</dd>
+                  </div>
+                </dl>
+              </section>
+              <div className="detail-group property-details">
+                <section className="display-element">
+                  <h3 className="section-group-header"><i className="fa fa-flag" /> Wind Mitigation</h3>
+                  <dl>
+                    <div>
+                      <dt className="roofCovering">Roof Covering</dt>
+                      <dd className="roofCovering">{property.windMitigation.roofCovering}</dd>
+                      <dt className="roofDeckAttachment">Roof Deck Attachment</dt>
+                      <dd className="roofDeckAttachment">{property.windMitigation.roofDeckAttachment}</dd>
+                      <dt className="roofToWallAttachment">Roof to Wall Attachment</dt>
+                      <dd className="roofToWallAttachment">{property.windMitigation.roofToWallConnection}</dd>
+                    </div>
+                  </dl>
+                  <dl>
+                    <div>
+                      <dt className="roofGeometry">Roof Geometry</dt>
+                      <dd className="roofGeometry">{property.windMitigation.roofGeometry}</dd>
+                      <dt className="SWR">Secondary Water Resistance (SWR)</dt>
+                      <dd className="SWR">{property.windMitigation.secondaryWaterResistance}</dd>
+                      <dt className="openingProtection">Opening Protection</dt>
+                      <dd className="openingProtection">{property.windMitigation.openingProtection}</dd>
+                    </div>
+                  </dl>
+                  <dl>
+                    <div>
+                      <dt className="windSpeed">FBC Wind Speed</dt>
+                      <dd className="windSpeed">{property.windMitigation.floridaBuildingCodeWindSpeed}</dd>
+                      <dt className="windSpeedDesign">FBC Wind Speed Design</dt>
+                      <dd className="windSpeedDesign">{property.windMitigation.floridaBuildingCodeWindSpeedDesign}</dd>
+                      <dt className="terrain">Terrain</dt>
+                      <dd className="terrain">{property.windMitigation.terrain}</dd>
+                    </div>
+                  </dl>
+                  <dl>
+                    <div>
+                      <dt className="internalPressureDesign">Internal Pressure Design</dt>
+                      <dd className="internalPressureDesign">{property.windMitigation.internalPressureDesign}</dd>
+                      <dt className="WBDR">Wind Borne Debris Region (WBDR)</dt>
+                      <dd className="WBDR">{property.windMitigation.windBorneDebrisRegion}</dd>
+                      <dt className="windMitFactor">Wind Mit Factor</dt>
+                      <dd className="windMitFactor">{rating.worksheet.elements.windMitigationFactors.windMitigationDiscount}</dd>
+                    </div>
+                  </dl>
+                </section>
+              </div>
             </div>
-          </dl>
-          <dl>
-            <div>
-              <dt className="protectionClass">Protection Class</dt>
-              <dd className="protectionClass">{property.protectionClass}</dd>
-              <dt className="BCEG">BCEG</dt>
-              <dd className="BCEG">{property.buildingCodeEffectivenessGrading}</dd>
-              <dt className="familyUnits">Family Units</dt>
-              <dd className="familyUnits">{property.familyUnits}</dd>
-            </div>
-          </dl>
-          <dl>
-            <div>
-              <dt className="distToTidalWaters">Dist. to Tidal Waters</dt>
-              <dd className="distToTidalWaters">{normalizeNumbers(property.distanceToTidalWater)} ft.</dd>
-              <dt className="distToFireHydrant">Dist. to Fire Hydrant</dt>
-              <dd className="distToFireHydrant">{property.distanceToFireHydrant ? `${normalizeNumbers(property.distanceToFireHydrant)} ft.` : '-'}</dd>
-              <dt className="distToFireStation">Dist. to Fire Station</dt>
-              <dd className="distToFireStation">{property.distanceToFireStation} mi.</dd>
-            </div>
-          </dl>
-          <dl>
-            <div>
-              <dt className="residenceType">Residence Type</dt>
-              <dd className="residenceType">{property.residenceType}</dd>
-              <dt className="squareFootage">Square Footage</dt>
-              <dd className="squareFootage">{property.squareFeet}</dd>
-              <dt className="floodZone">Flood Zone</dt>
-              <dd className="floodZone">{property.floodZone}</dd>
-            </div>
-          </dl>
+          </div>
         </div>
-      </section>
-      <section>
-        <h3>Wind Mitigation</h3>
-        <div className="wind-mitigation">
-          <dl>
-            <div>
-              <dt className="roofCovering">Roof Covering</dt>
-              <dd className="roofCovering">{property.windMitigation.roofCovering}</dd>
-              <dt className="roofDeckAttachment">Roof Deck Attachment</dt>
-              <dd className="roofDeckAttachment">{property.windMitigation.roofDeckAttachment}</dd>
-              <dt className="roofToWallAttachment">Roof to Wall Attachment</dt>
-              <dd className="roofToWallAttachment">{property.windMitigation.roofToWallConnection}</dd>
-            </div>
-          </dl>
-          <dl>
-            <div>
-              <dt className="roofGeometry">Roof Geometry</dt>
-              <dd className="roofGeometry">{property.windMitigation.roofGeometry}</dd>
-              <dt className="SWR">Secondary Water Resistance (SWR)</dt>
-              <dd className="SWR">{property.windMitigation.secondaryWaterResistance}</dd>
-              <dt className="openingProtection">Opening Protection</dt>
-              <dd className="openingProtection">{property.windMitigation.openingProtection}</dd>
-            </div>
-          </dl>
-          <dl>
-            <div>
-              <dt className="windSpeed">FBC Wind Speed</dt>
-              <dd className="windSpeed">{property.windMitigation.floridaBuildingCodeWindSpeed}</dd>
-              <dt className="windSpeedDesign">FBC Wind Speed Design</dt>
-              <dd className="windSpeedDesign">{property.windMitigation.floridaBuildingCodeWindSpeedDesign}</dd>
-              <dt className="terrain">Terrain</dt>
-              <dd className="terrain">{property.windMitigation.terrain}</dd>
-            </div>
-          </dl>
-          <dl>
-            <div>
-              <dt className="internalPressureDesign">Internal Pressure Design</dt>
-              <dd className="internalPressureDesign">{property.windMitigation.internalPressureDesign}</dd>
-              <dt className="WBDR">Wind Borne Debris Region (WBDR)</dt>
-              <dd className="WBDR">{property.windMitigation.windBorneDebrisRegion}</dd>
-              <dt className="windMitFactor">Wind Mit Factor</dt>
-              <dd className="windMitFactor">{rating.worksheet.elements.windMitigationFactors.windMitigationDiscount}</dd>
-            </div>
-          </dl>
-        </div>
-      </section>
+      </div>
     </React.Fragment>);
 };
 
