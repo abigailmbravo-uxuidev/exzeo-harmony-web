08c80ba644b1d27af444a365d033c4bc6a3092b0
Mark Eads <meads@hcpci.com>
2018-08-15 14:31:35 -0400

    format

--- src/components/Common/AgentCard.jsx
+++ src/components/Common/AgentCard.jsx
@@ -5,32 +5,32 @@ import normalizePhone from '../Form/normalizePhone';
 const AgentCard = ({ agent }) => (
   <section className="display-element agent">
     <dl>
-        <div>
-          <dt className="agentName">Agent Name</dt>
-          <dd className="agentName">{agent.firstName} {agent.lastName}</dd>
-        </div>
-        <div>
-          <dt className="agentPhone">Phone</dt>
-          <dd className="agentPhone">{normalizePhone(agent.primaryPhoneNumber)}</dd>
-        </div>
-        { agent.secondaryPhoneNumber &&
+      <div>
+        <dt className="agentName">Agent Name</dt>
+        <dd className="agentName">{agent.firstName} {agent.lastName}</dd>
+      </div>
+      <div>
+        <dt className="agentPhone">Phone</dt>
+        <dd className="agentPhone">{normalizePhone(agent.primaryPhoneNumber)}</dd>
+      </div>
+      { agent.secondaryPhoneNumber &&
         <div>
           <dt className="agentPhone">Phone</dt>
           <dd className="agentPhone">{normalizePhone(agent.secondaryPhoneNumber)}</dd>
         </div>
    }
-        <div>
-          <dt className="agentEmail">Email</dt>
-          <dd className="agentEmail">{agent.emailAddress}</dd>
-        </div>
-      </dl>
+      <div>
+        <dt className="agentEmail">Email</dt>
+        <dd className="agentEmail">{agent.emailAddress}</dd>
+      </div>
+    </dl>
     <dl>
-        <div>
-          <dt className="agentMailingAddress">Mailing Address</dt>
-          <dd className="agentMailingAddress">{`${agent.mailingAddress.address1} ${agent.mailingAddress.address2 ? agent.mailingAddress.address2 : ''}
-${agent.mailingAddress.city}, ${agent.mailingAddress.state} ${agent.mailingAddress.zip}`}</dd>
-        </div>
-      </dl>
+      <div>
+        <dt className="agentMailingAddress">Mailing Address</dt>
+        <dd className="agentMailingAddress">{`${agent.mailingAddress.address1} ${agent.mailingAddress.address2 ? agent.mailingAddress.address2 : ''}
+          ${agent.mailingAddress.city}, ${agent.mailingAddress.state} ${agent.mailingAddress.zip}`}</dd>
+      </div>
+    </dl>
   </section>
 );
 
