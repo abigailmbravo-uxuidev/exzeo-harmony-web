532356206c555aac8e2aa2d651e3819bcc398372
Mark Eads <meads@hcpci.com>
2018-08-15 14:29:51 -0400

    fixed classes

--- src/components/Common/AgentCard.jsx
+++ src/components/Common/AgentCard.jsx
@@ -4,31 +4,31 @@ import normalizePhone from '../Form/normalizePhone';
 
 const AgentCard = ({ agent }) => (
   <React.Fragment>
-    <section className="display-element policy-holder">
+    <section className="display-element agent">
       <dl>
         <div>
-          <dt className="policyholderName">Agent Name</dt>
-          <dd className="policyholderName">{agent.firstName} {agent.lastName}</dd>
+          <dt className="agentName">Agent Name</dt>
+          <dd className="agentName">{agent.firstName} {agent.lastName}</dd>
         </div>
         <div>
-          <dt className="policyholderPhone">Phone</dt>
-          <dd className="policyholderPhone">{normalizePhone(agent.primaryPhoneNumber)}</dd>
+          <dt className="agentPhone">Phone</dt>
+          <dd className="agentPhone">{normalizePhone(agent.primaryPhoneNumber)}</dd>
         </div>
         { agent.secondaryPhoneNumber &&
         <div>
-          <dt className="policyholderPhone">Phone</dt>
-          <dd className="policyholderPhone">{normalizePhone(agent.secondaryPhoneNumber)}</dd>
+          <dt className="agentPhone">Phone</dt>
+          <dd className="agentPhone">{normalizePhone(agent.secondaryPhoneNumber)}</dd>
         </div>
    }
         <div>
-          <dt className="policyholderEmail">Email</dt>
-          <dd className="policyholderEmail">{agent.emailAddress}</dd>
+          <dt className="agentEmail">Email</dt>
+          <dd className="agentEmail">{agent.emailAddress}</dd>
         </div>
       </dl>
       <dl>
         <div>
-          <dt className="agent.mailingAddress">Mailing Address</dt>
-          <dd className="agent.mailingAddress">{`${agent.mailingAddress.address1} ${agent.mailingAddress.address2 ? agent.mailingAddress.address2 : ''}
+          <dt className="agentMailingAddress">Mailing Address</dt>
+          <dd className="agentMailingAddress">{`${agent.mailingAddress.address1} ${agent.mailingAddress.address2 ? agent.mailingAddress.address2 : ''}
 ${agent.mailingAddress.city}, ${agent.mailingAddress.state} ${agent.mailingAddress.zip}`}</dd>
         </div>
       </dl>
