6633726e84cc84d994c59abc5c3df2f1b9ccd26b
Mark Eads <meads@hcpci.com>
2018-08-16 11:04:32 -0400

    removed classes

--- src/components/Common/PolicyHolderCard.jsx
+++ src/components/Common/PolicyHolderCard.jsx
@@ -5,33 +5,33 @@ import normalizePhone from '../Form/normalizePhone';
 const PolicyHolderCard = ({ policyHolder, policyHolderMailingAddress, phIndex
 }) => (
   <React.Fragment>
-    <h3 data-test-="policyholderHeader" className="section-group-header"><i className="fa fa-vcard-o" /> Policyholder {phIndex + 1}</h3>
+    <h3 data-test="policyholderHeader" className="section-group-header"><i className="fa fa-vcard-o" /> Policyholder {phIndex + 1}</h3>
     <section className="display-element policy-holder">
       <dl>
-        <div data-test-="policyholderName">
-          <dt className="policyholderName">Policyholder Name</dt>
-          <dd className="policyholderName">{policyHolder.firstName} {policyHolder.lastName}</dd>
+        <div data-test="policyholderName">
+          <dt>Policyholder Name</dt>
+          <dd>{policyHolder.firstName} {policyHolder.lastName}</dd>
         </div>
-        <div data-test-="policyholderPhone" >
-          <dt className="policyholderPhone">Phone</dt>
-          <dd className="policyholderPhone">{normalizePhone(policyHolder.primaryPhoneNumber)}</dd>
+        <div data-test="policyholderPhone" >
+          <dt>Phone</dt>
+          <dd>{normalizePhone(policyHolder.primaryPhoneNumber)}</dd>
         </div>
         { policyHolder.secondaryPhoneNumber &&
-        <div data-test-="policyholderPhone2">
-          <dt className="policyholderPhone">Phone</dt>
-          <dd className="policyholderPhone">{normalizePhone(policyHolder.secondaryPhoneNumber)}</dd>
+        <div data-test="policyholderPhone2">
+          <dt>Phone</dt>
+          <dd>{normalizePhone(policyHolder.secondaryPhoneNumber)}</dd>
         </div>
    }
-        <div data-test-="policyholderEmail">
-          <dt className="policyholderEmail">Email</dt>
-          <dd className="policyholderEmail">{policyHolder.emailAddress}</dd>
+        <div data-test="policyholderEmail">
+          <dt>Email</dt>
+          <dd>{policyHolder.emailAddress}</dd>
         </div>
       </dl>
       {phIndex === 0 &&
       <dl>
-        <div data-test-="policyHolderMailingAddress">
-          <dt className="policyHolderMailingAddress">Mailing Address</dt>
-          <dd className="policyHolderMailingAddress">{`${policyHolderMailingAddress.address1} ${policyHolderMailingAddress.address2 ? policyHolderMailingAddress.address2 : ''}
+        <div data-test="policyHolderMailingAddress">
+          <dt>Mailing Address</dt>
+          <dd>{`${policyHolderMailingAddress.address1} ${policyHolderMailingAddress.address2 ? policyHolderMailingAddress.address2 : ''}
             ${policyHolderMailingAddress.city}, ${policyHolderMailingAddress.state} ${policyHolderMailingAddress.zip}`}</dd>
         </div>
       </dl>
