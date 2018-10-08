74e80bd52a77f0849e51345df4a5db5c206f4d1f
elamison <eric@lamison.com>
2018-10-08 15:17:24 -0400

    Added billing page for portal.

--- src/components/Common/PolicyTabs.jsx
+++ src/components/Common/PolicyTabs.jsx
@@ -7,6 +7,7 @@ const PolicyTabs = ({ activeTab, policyNumber }) =>
     <Link to={{ pathname: `/policy/${policyNumber}/policyHolder` }} className={`btn btn-tab ${activeTab === 'policyHolder' ? 'active' : ''}`}><i className="fa fa-user" /><span>Policyholder</span></Link>
     <Link to={{ pathname: `/policy/${policyNumber}/property` }} className={`btn btn-tab ${activeTab === 'property' ? 'active' : ''}`}><i className="fa fa-map-marker" /><span>Property</span></Link>
     <Link to={{ pathname: `/policy/${policyNumber}/coverage` }} className={`btn btn-tab ${activeTab === 'coverage' ? 'active' : ''}`}><i className="fa fa-sliders" /><span>Coverage</span></Link>
+    <Link to={{ pathname: `/policy/${policyNumber}/billing` }} className={`btn btn-tab ${activeTab === 'billing' ? 'active' : ''}`}><i className="fa fa-money" /><span>Billing</span></Link>
     <Link to={{ pathname: `/policy/${policyNumber}/documents` }} className={`btn btn-tab ${activeTab === 'documents' ? 'active' : ''}`}><i className="fa fa-file-text-o" /><span>Documents</span></Link>
   </nav>;
 
