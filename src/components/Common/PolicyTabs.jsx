8c93b6fa2a7cf1a2e8fbb1af59b2b35fb30c261c
Joe Sutphin <jsutphin@exzeo.com>
2018-10-10 10:46:43 -0400

    update billing section

--- src/components/Common/PolicyTabs.jsx
+++ src/components/Common/PolicyTabs.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 
 const PolicyTabs = ({ activeTab, policyNumber }) =>
   <nav className="nav-tabs">
-    <Link to={{ pathname: `/policy/${policyNumber}/policyHolder` }} className={`btn btn-tab ${activeTab === 'policyHolder' ? 'active' : ''}`}><i className="fa fa-user" /><span>Policyholder</span></Link>
+    <Link to={{ pathname: `/policy/${policyNumber}/policyHolder` }} className={`btn btn-tab ${activeTab === 'policyHolder' ? 'active' : ''}`}><i className="fa fa-vcard" /><span>Policyholder</span></Link>
     <Link to={{ pathname: `/policy/${policyNumber}/property` }} className={`btn btn-tab ${activeTab === 'property' ? 'active' : ''}`}><i className="fa fa-map-marker" /><span>Property</span></Link>
     <Link to={{ pathname: `/policy/${policyNumber}/coverage` }} className={`btn btn-tab ${activeTab === 'coverage' ? 'active' : ''}`}><i className="fa fa-sliders" /><span>Coverage</span></Link>
     <Link to={{ pathname: `/policy/${policyNumber}/billing` }} className={`btn btn-tab ${activeTab === 'billing' ? 'active' : ''}`}><i className="fa fa-money" /><span>Billing</span></Link>
