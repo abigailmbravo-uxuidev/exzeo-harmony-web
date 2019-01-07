ace8124903e6bc1988498d228709be2e73c6f5d1
Joe Sutphin <jsutphin@exzeo.com>
2019-01-07 14:05:26 -0500

    update phone format to use core

--- src/components/Contacts/ContactCard.jsx
+++ src/components/Contacts/ContactCard.jsx
@@ -1,14 +1,7 @@
 import React from 'react';
+import { normalize } from '@exzeo/core-ui/';
 // import PropTypes from 'prop-types';
 
-function formatPhone(phoneNumberString) {
-  const cleaned = `${phoneNumberString.replace(/\D/g)}`;
-  const match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
-  if (match) {
-    return `(${match[1]}) ${match[2]}-${match[3]}`;
-  }
-  return '';
-}
 
 const ContactCard = ({ image, name, title, phone, cell, extension, email, address1, address2, city, state, zip, message }) => {
   return (
@@ -20,8 +13,8 @@ const ContactCard = ({ image, name, title, phone, cell, extension, email, addres
         <div className="contact-info">
           {name && <h4 className="contact-name">{name}</h4>}
           {title && <p className="contact-title">{title}</p>}
-          {phone && <p className="contact-phone"><i className="fa fa-phone-square" /><a href={`tel:${phone}`}>{formatPhone(phone)} {extension && `${extension}`}</a></p>}
-          {cell && <p className="contact-phone mobile"><a href={`tel:${cell}`}>{formatPhone(cell)}</a><span>MOBILE</span></p>}
+          {phone && <p className="contact-phone"><i className="fa fa-phone-square" /><a href={`tel:${phone}`}>{normalize.Phone(phone)} {extension && `${extension}`}</a></p>}
+          {cell && <p className="contact-phone mobile"><a href={`tel:${cell}`}>{normalize.Phone(cell)}</a><span>MOBILE</span></p>}
           {email && <p className="contact-email"><i className="fa fa-envelope" /><a href={`mailto:${email}`}>{email}</a></p>}
           {address1 && <p className="contact-address"><i className="fa fa-map-marker" />
             <span className="contact-address-wrapper">
