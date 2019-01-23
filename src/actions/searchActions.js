8f7ac2e4e9aa71beecd903b522b2ad0a7f2992f2
Jordan Rhodes <jsrhodes15@gmail.com>
2019-01-23 14:22:20 -0500

    Remove commented code that was inserted for debugging

--- src/actions/searchActions.js
+++ src/actions/searchActions.js
@@ -147,7 +147,6 @@ export function searchQuotes(quoteSearchData) {
       dispatch(toggleLoading(true));
       const results = await fetchQuotes(quoteSearchData);
       dispatch(setSearchResults(formatQuoteResults(results)));
-      // dispatch(errorActions.setAppError({ message: 'Some Error!!!'}));
     } catch (error) {
       dispatch(errorActions.setAppError(error));
     } finally {
