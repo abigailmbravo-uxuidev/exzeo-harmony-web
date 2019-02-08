03f5b26341e864a3a0a58232aa8c259ee029b430
Mark Eads <meads@hcpci.com>
2019-02-08 15:41:03 -0500

    WIP for initial values

--- src/selectors/quoteState.selectors.js
+++ src/selectors/quoteState.selectors.js
@@ -8,6 +8,8 @@ export const getQuoteSelector = createSelector(
     (quoteData) => {
       if (!quoteData || !quoteData.quoteNumber) return {};
       quoteData.effectiveDate = formatDate(quoteData.effectiveDate, FORMATS.SECONDARY);
+      // do some kind of transformation then it all works form here. Just a thought
+      quoteData.coverageLimits.otherStructures.amount = (quoteData.coverageLimits.otherStructures.amount * 100) / quoteData.coverageLimits.dwelling.amount
       return quoteData;
     }
   );
