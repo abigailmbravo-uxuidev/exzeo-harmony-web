271e5fbf481cd016f28cfa78926d6bef6cb55fc3
Mark Eads <meads@hcpci.com>
2019-02-13 15:50:45 -0500

    added error redirect

--- src/modules/Quote/QuoteWorkflow.jsx
+++ src/modules/Quote/QuoteWorkflow.jsx
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import { submit } from 'redux-form';
-
 import { updateQuote } from '../../actions/quoteState.actions';
 
 import App from '../../components/AppWrapper';
@@ -42,7 +41,7 @@ export class QuoteWorkflow extends Component {
     isRecalc: false,
   };
 
-  componentDidMount() {
+  componentDidMount(){
     const { quote } = this.props;
 
     if (quote && quote.property) {
@@ -57,11 +56,11 @@ export class QuoteWorkflow extends Component {
 
   handlePremiumRecalc = () => {
     document
-      .getElementById(FORM_ID)
-      .dispatchEvent(new Event("submit", { cancelable: true }))
+    .getElementById(FORM_ID)
+    .dispatchEvent(new Event("submit", { cancelable: true }))
   };
 
-  handleUpdateQuote = async ({ data, quoteNumber }) => {
+  handleUpdateQuote = async ({data, quoteNumber}) => {
     const { updateQuote } = this.props;
     const quote = await updateQuote({ data, quoteNumber });
 
@@ -78,11 +77,11 @@ export class QuoteWorkflow extends Component {
   };
 
   handleGandalfSubmit = async (values) => {
-    const { zipCodeSettings, quote, history, updateQuote, location } = this.props;
+    const { zipCodeSettings, quote, history, updateQuote, location, workflowState } = this.props;
     const { isRecalc } = this.state;
-    await updateQuote({ data: { ...values, recalc: isRecalc }, quoteNumber: quote.quoteNumber, options: { timezone: (zipCodeSettings || {}).timezone || 'America/New_York' } });
-    // TODO: Figure out a routing solution
-    if (!isRecalc) history.replace(NEXT_PAGE_ROUTING[location.pathname.split('/')[3]]);
+    await updateQuote({  data: { ...values, recalc: isRecalc }, quoteNumber: quote.quoteNumber, options: { timezone: (zipCodeSettings|| {}).timezone || 'America/New_York' } });
+        // TODO: Figure out a routing solution
+    if(!(isRecalc || workflowState.isHardStop)) history.replace(NEXT_PAGE_ROUTING[location.pathname.split('/')[3]]);
   };
 
   getConfigForJsonTransform = () => {
@@ -103,7 +102,7 @@ export class QuoteWorkflow extends Component {
   };
 
   render() {
-    const { auth, history, isLoading, match, location, options, quote } = this.props;
+    const { auth, history, isLoading, match, location, options, quote, workflowState } = this.props;
     const { isRecalc } = this.state;
     const currentStep = location.pathname.split('/')[3];
     const shouldUseGandalf = ROUTES_NOT_HANDLED_BY_GANDALF.indexOf(currentStep) === -1;
@@ -117,53 +116,54 @@ export class QuoteWorkflow extends Component {
         render={() => (
           <div className="route">
             {isLoading && <Loader />}
+            {workflowState.isHardStop && <Redirect to={'error'} />}
 
-            <WorkflowNavigation handleRecalc={this.handlePremiumRecalc} isRecalc={isRecalc} history={history} goToStep={this.goToStep} isLoading={isLoading} isThankYou={currentStep === 'thankYou'} />
+            <WorkflowNavigation handleRecalc={this.handlePremiumRecalc} isRecalc={isRecalc} history={history} goToStep={this.goToStep} isLoading={isLoading}/>
             {/*{ Gandalf will be replacing most/all of these routes }*/}
-            {shouldUseGandalf &&
-              <Route
-                path={`${match.url}`}
-                render={props => (
-                  <React.Fragment>
-                    <Gandalf
-                      formId={FORM_ID}
-                      currentPage={PAGE_ROUTING[currentStep]}
-                      /* passing needed data as options all the way to the Input component, I don't really like that but we can prob do something with state */
-                      options={options}
-                      className="survey-wrapper"
-                      path={location.pathname}
-                      initialValues={quote}
-                      handleSubmit={this.handleGandalfSubmit}
-                      template={MOCK_TEMPLATE}
-                      transformConfig={this.getConfigForJsonTransform()}
-                      onDirtyCallback={shouldPassCallback ? this.setRecalc : undefined}
-                      renderFooter={({ submitting }) => (
-                        <React.Fragment>
-                          <div className="btn-group">
-                            <button type="submit" className="btn btn-primary" disabled={submitting}>{this.state.isRecalc ? 'recalculate' : 'next'}</button>
-                          </div>
-                        </React.Fragment>
-                      )}
-                    />
+            {shouldUseGandalf&&
+            <Route
+              path={`${match.url}`}
+              render={props => (
+                <React.Fragment>
+                <Gandalf
+                  formId={FORM_ID}
+                  currentPage={PAGE_ROUTING[currentStep]}
+                  /* passing needed data as options all the way to the Input component, I don't really like that but we can prob do something with state */
+                  options={options}
+                  className="survey-wrapper"
+                  path={location.pathname}
+                  initialValues={quote}
+                  handleSubmit={this.handleGandalfSubmit}
+                  template={MOCK_TEMPLATE}
+                  transformConfig={this.getConfigForJsonTransform()}
+                  onDirtyCallback={shouldPassCallback ? this.setRecalc : undefined}
+                  renderFooter={({ submitting }) => (
+                    <React.Fragment>
+                      <div className="btn-group">
+                        <button type="submit" className="btn btn-primary" disabled={submitting}>{this.state.isRecalc ? 'recalculate' : 'next'}</button>
+                      </div>
+                    </React.Fragment>
+                  )}
+                />
 
-                    <Footer />
-                  </React.Fragment>
-                )} />
+                <Footer />
+                </React.Fragment>
+              )} />
             }
             {/*<Route exact path={`${match.url}/underwriting`}          render={props => <Underwriting {...props} updateQuote={this.handleUpdateQuote} />} />*/}
             {/*<Route exact path={`${match.url}/customize`}             render={props => <Customize {...props} updateQuote={this.handleUpdateQuote} isRecalc={isRecalc} setRecalc={this.setRecalc} />} />*/}
-            <Route exact path={`${match.url}/share`} render={props => <Share {...props} updateQuote={this.handleUpdateQuote} />} />
-            <Route exact path={`${match.url}/assumptions`} render={props => <Assumptions {...props} updateQuote={this.handleUpdateQuote} />} />
-            <Route exact path={`${match.url}/additionalInterests`} render={props => <AddAdditionalInterest {...props} updateQuote={this.handleUpdateQuote} />} />
-            <Route exact path={`${match.url}/askMortgagee`} render={props => <Mortgagee {...props} updateQuote={this.handleUpdateQuote} />} />
+            <Route exact path={`${match.url}/share`}                 render={props => <Share {...props} updateQuote={this.handleUpdateQuote} />} />
+            <Route exact path={`${match.url}/assumptions`}           render={props => <Assumptions {...props} updateQuote={this.handleUpdateQuote} />} />
+            <Route exact path={`${match.url}/additionalInterests`}   render={props => <AddAdditionalInterest {...props} updateQuote={this.handleUpdateQuote} />} />
+            <Route exact path={`${match.url}/askMortgagee`}          render={props => <Mortgagee {...props} updateQuote={this.handleUpdateQuote} />} />
             <Route exact path={`${match.url}/askAdditionalInterest`} render={props => <AdditionalInterest {...props} updateQuote={this.handleUpdateQuote} />} />
-            <Route exact path={`${match.url}/askAdditionalInsured`} render={props => <AdditionalInsured {...props} updateQuote={this.handleUpdateQuote} />} />
-            <Route exact path={`${match.url}/askPremiumFinance`} render={props => <PremiumFinance {...props} updateQuote={this.handleUpdateQuote} />} />
-            <Route exact path={`${match.url}/askBillPayer`} render={props => <BillPayer {...props} updateQuote={this.handleUpdateQuote} />} />
-            <Route exact path={`${match.url}/mailingBilling`} render={props => <Billing {...props} updateQuote={this.handleUpdateQuote} />} />
-            <Route exact path={`${match.url}/verify`} render={props => <Verify {...props} updateQuote={this.handleUpdateQuote} goToStep={this.goToStep} />} />
-            <Route exact path={`${match.url}/thankYou`} render={props => <ThankYou {...props} updateQuote={this.handleUpdateQuote} />} />
-            <Route exact path={`${match.url}/error`} render={props => <Error {...props} updateQuote={this.handleUpdateQuote} />} />
+            <Route exact path={`${match.url}/askAdditionalInsured`}  render={props => <AdditionalInsured {...props} updateQuote={this.handleUpdateQuote} />} />
+            <Route exact path={`${match.url}/askPremiumFinance`}     render={props => <PremiumFinance {...props} updateQuote={this.handleUpdateQuote} />} />
+            <Route exact path={`${match.url}/askBillPayer`}          render={props => <BillPayer {...props} updateQuote={this.handleUpdateQuote} />} />
+            <Route exact path={`${match.url}/mailingBilling`}        render={props => <Billing {...props} updateQuote={this.handleUpdateQuote} />} />
+            <Route exact path={`${match.url}/verify`}                render={props => <Verify {...props} updateQuote={this.handleUpdateQuote} goToStep={this.goToStep} />} />
+            <Route exact path={`${match.url}/thankYou`}              render={props => <ThankYou {...props} updateQuote={this.handleUpdateQuote} />} />
+            <Route exact path={`${match.url}/error`}                 render={props => <Error {...props} updateQuote={this.handleUpdateQuote} />} />
             {/*{ ^^^ Gandalf will be replacing most/all of these routes ^^^ }*/}
           </div>
         )}
