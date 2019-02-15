89c4801b841047314f2a31360b8b53a347578d7c
Joe Sutphin <jsutphin@exzeo.com>
2019-02-15 09:00:04 -0500

    update share page to work with view grid

--- src/components/Share/Share.jsx
+++ src/components/Share/Share.jsx
@@ -48,38 +48,33 @@ export class Share extends React.Component {
 
     const { showEmailPopup } = getState();
     return (
-      <div className="route-content">
+      <React.Fragment>
         {isHardStop &&
           <Redirect to="error" />
         }
-        <div id="SharePage" >
-          <div className="scroll">
-            <div className="form-group detail-wrapper">
-              <section className="section-instructions" data-test="share-section-one">
-                <h3 className="section-group-header"><i className="fa fa-share-alt" /> Share</h3>
-                <p>To SHARE this quote as a PDF via email, click the <strong>SHARE</strong> button</p>
-              </section>
-              <section className="section-instructions" data-test="share-section-two">
-                <h3 className="section-group-header"><i className="fa fa-arrow-circle-right" /> Continue</h3>
-                <p>To CONTINUE the quote process, you will need the following</p>
-                <ul>
-                  <li>Mortgage information</li>
-                  <li>Name and email address of additional owners</li>
-                  <li>Name and address of any other additional insured to add to this policy</li>
-                </ul>
-                <p>When you are prepared to move forward, click the <strong>NEXT</strong> button</p>
-              </section>
-              <section className="section-instructions" data-test="share-section-three">
-                <h3 className="section-group-header"><i className="fa fa-quote-left" /> New Quote</h3>
-                <p>Your current quote is saved and can be retrieved at any time. To begin a NEW QUOTE, click the <i className="fa fa-dashboard" /> <strong>DASHBOARD</strong> tab</p>
-              </section>
-            </div>
-            <div className="workflow-steps">
-              <button className="btn btn-secondary" type="button" data-test="share" onClick={() => setEmailPopup(true)}>share</button>
-              <button className="btn btn-primary" type="button" onClick={this.noShareSubmit} data-test="submit" disabled={isLoading}>next</button>
-            </div>
-          </div>
+        <section className="section-instructions">
+          <div className="title"><i className="fa fa-share-alt" /> Share</div>
+          <p>To SHARE this quote as a PDF via email, click the <strong>SHARE</strong> button</p>
+        </section>
+        <section className="section-instructions">
+          <div className="title"><i className="fa fa-arrow-circle-right" /> Continue</div>
+          <p>To CONTINUE the quote process, you will need the following</p>
+          <ul>
+            <li>Mortgage information</li>
+            <li>Name and email address of additional owners</li>
+            <li>Name and address of any other additional insured to add to this policy</li>
+          </ul>
+          <p>When you are prepared to move forward, click the <strong>NEXT</strong> button</p>
+        </section>
+        <section className="section-instructions">
+          <div className="title"><i className="fa fa-quote-left" /> New Quote</div>
+          <p>Your current quote is saved and can be retrieved at any time. To begin a NEW QUOTE, click the <i className="fa fa-dashboard" /> <strong>DASHBOARD</strong> tab</p>
+        </section>
+        <div className="btn-group">
+          <button className="btn btn-secondary" type="button" data-test="share" onClick={() => setEmailPopup(true)}>share</button>
+          <button className="btn btn-primary" type="button" onClick={this.noShareSubmit} data-test="submit" disabled={isLoading}>next</button>
         </div>
+
 
         {showEmailPopup &&
           <EmailPopup
@@ -97,7 +92,7 @@ export class Share extends React.Component {
           />
         }
 
-      </div>
+      </React.Fragment>
     );
   }
 }
