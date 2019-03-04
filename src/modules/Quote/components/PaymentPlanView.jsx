1462720af5a327f6a1dafcf172eb101e861ba9e9
Joe Sutphin <jsutphin@exzeo.com>
2019-03-04 13:56:46 -0500

    update html of billing

--- src/modules/Quote/components/PaymentPlanView.jsx
+++ src/modules/Quote/components/PaymentPlanView.jsx
@@ -4,32 +4,30 @@ import { format, date } from '@exzeo/core-ui';
 // TODO come back and clean this up. For now this works with what CG is giving us, may need to change once workflows are implemented.
 const PaymentPlanView = ({ paymentPlan }) => {
   return (
-    <dl className="column-3">
-      <div>
-        {paymentPlan && paymentPlan.amount &&
-          <div>
-            <dt><span>Annual</span> Installment Plan</dt>
-            <dd>{format.toCurrency(paymentPlan.amount)} : {date.formatDate(paymentPlan.dueDate)}</dd>
-          </div>
-        }
-        {paymentPlan && paymentPlan.s1 && paymentPlan.s2 &&
-          <div>
-            <dt><span>Semi-Annual</span> Installment Plan</dt>
-            <dd>{format.toCurrency(paymentPlan.s1.amount)} : {date.formatDate(paymentPlan.s1.dueDate)}</dd>
-            <dd>{format.toCurrency(paymentPlan.s2.amount)} : {date.formatDate(paymentPlan.s2.dueDate)}</dd>
-          </div>
-        }
-        {paymentPlan && paymentPlan.q1 && paymentPlan.q2 && paymentPlan.q3 && paymentPlan.q4 &&
-          <div>
-            <dt><span>Quarterly</span> Installment Plan</dt>
-            <dd>{format.toCurrency(paymentPlan.q1.amount)} : {date.formatDate(paymentPlan.q1.dueDate)}</dd>
-            <dd>{format.toCurrency(paymentPlan.q2.amount)} : {date.formatDate(paymentPlan.q2.dueDate)}</dd>
-            <dd>{format.toCurrency(paymentPlan.q3.amount)} : {date.formatDate(paymentPlan.q3.dueDate)}</dd>
-            <dd>{format.toCurrency(paymentPlan.q4.amount)} : {date.formatDate(paymentPlan.q4.dueDate)}</dd>
-          </div>
-        }
-      </div>
-    </dl>
+    <React.Fragment>
+      {paymentPlan && paymentPlan.amount &&
+        <dl className="view-col-4">
+          <dt><span>Annual</span> Installment Plan</dt>
+          <dd>{format.toCurrency(paymentPlan.amount)} : {date.formatDate(paymentPlan.dueDate)}</dd>
+        </dl>
+      }
+      {paymentPlan && paymentPlan.s1 && paymentPlan.s2 &&
+        <dl className="view-col-4">
+          <dt><span>Semi-Annual</span> Installment Plan</dt>
+          <dd>{format.toCurrency(paymentPlan.s1.amount)} : {date.formatDate(paymentPlan.s1.dueDate)}</dd>
+          <dd>{format.toCurrency(paymentPlan.s2.amount)} : {date.formatDate(paymentPlan.s2.dueDate)}</dd>
+        </dl>
+      }
+      {paymentPlan && paymentPlan.q1 && paymentPlan.q2 && paymentPlan.q3 && paymentPlan.q4 &&
+        <dl className="view-col-4">
+          <dt><span>Quarterly</span> Installment Plan</dt>
+          <dd>{format.toCurrency(paymentPlan.q1.amount)} : {date.formatDate(paymentPlan.q1.dueDate)}</dd>
+          <dd>{format.toCurrency(paymentPlan.q2.amount)} : {date.formatDate(paymentPlan.q2.dueDate)}</dd>
+          <dd>{format.toCurrency(paymentPlan.q3.amount)} : {date.formatDate(paymentPlan.q3.dueDate)}</dd>
+          <dd>{format.toCurrency(paymentPlan.q4.amount)} : {date.formatDate(paymentPlan.q4.dueDate)}</dd>
+        </dl>
+      }
+    </React.Fragment>
   );
 };
 
