c291c1b3d02a0477fb9e88e4bc1fc6ec9082a53c
Colin Fendrick <colin.fendrick@gmail.com>
2019-03-21 08:21:40 -0400

    Refactor additional insured to use imported struct

--- cypress/integration/additionalInterests/additionalInsured.spec.js
+++ cypress/integration/additionalInterests/additionalInsured.spec.js
@@ -11,10 +11,9 @@ import {
 } from '../../helpers';
 import ains1 from '../../fixtures/stockData/ains1.json';
 import ains2 from '../../fixtures/stockData/ains2.json';
+import { ains1Fields, ains2Fields } from './additionalInsuredInputs.js';
 
 describe('Additional Insured Testing', () => {
-  const ains1fields = ['ains1Name1', 'ains1MailingAddress1', 'ains1City', 'ains1State', 'ains1Zip'];
-  const ains2fields = ['ains2Name1', 'ains2MailingAddress1', 'ains2City', 'ains2State', 'ains2Zip'];
   const toggleModalOn = () => cy.findDataTag('ains-add').click();
   const addAdditional = () => cy.findDataTag('isAdditional2').find('.switch-div').click();
 
@@ -35,28 +34,32 @@ describe('Additional Insured Testing', () => {
     cy.route('POST', '/cg/complete?addAdditionalAIs', 'fx:stubs/addAdditionalAIs/additionalInsured');
   });
 
+  const ains1Required = ains1Fields.filter(({ required }) => required !== false);
+  const ains2Required = ains2Fields.filter(({ required }) => required !== false);
+
   it('NEG:All Additional Insure 1 Inputs Empty Value', () =>
     goBack().then(() => {
       toggleModalOn();
-      cy.clearAllText(ains1fields).submitAndCheckValidation(ains1fields);
+      cy.clearAllText(ains1Fields).submitAndCheckValidation(ains1Required);
     })
   );
 
   it('NEG:Additional Insured 1 Empty Value', () =>
     goBack().then(() => {
       toggleModalOn();
-      cy.clearAllText(ains1fields);
-
-      ains1fields.forEach(leaveBlank => cy.verifyForm(ains1fields, [leaveBlank], ains1));
+      cy.clearAllText(ains1Fields)
+        .wrap(ains1Required).each(fieldToLeaveBlank => cy.verifyForm(ains1Required, [fieldToLeaveBlank], ains1));
     })
   );
 
   it('NEG:Additional Insured 1 Invalid Input Value', () =>
     goBack().then(() => {
       toggleModalOn();
-      cy.clearAllText(ains1fields)
-        .verifyForm(['ains1State'], undefined, { ains1State: 'foo' }, { errors: ['Only 2 letters allowed'] })
-        .verifyForm(['ains1Zip'], undefined, { ains1Zip: '123456789' }, { errors: ['Only 8 letters or numbers allowed'] });
+      const state = ains1Fields.find(({ name }) => name === 'ains1State');
+      const zip = ains1Fields.find(({ name }) => name === 'ains1Zip');
+      cy.clearAllText(ains1Fields)
+        .verifyForm([{ ...state, error: 'Only 2 letters allowed'}], undefined, { ains1State: 'foo' })
+        .verifyForm([{ ...zip, error: 'Only 8 letters or numbers allowed' }], undefined, { ains1Zip: '123456789' });
     })
   );
 
@@ -64,8 +67,7 @@ describe('Additional Insured Testing', () => {
     goBack().then(() => {
       toggleModalOn();
       addAdditional();
-      cy.clearAllText(ains2fields)
-        .submitAndCheckValidation(ains2fields);
+      cy.clearAllText(ains2Fields).submitAndCheckValidation(ains2Required);
     })
   );
 
@@ -73,9 +75,8 @@ describe('Additional Insured Testing', () => {
     goBack().then(() => {
       toggleModalOn();
       addAdditional();
-      cy.clearAllText([...ains1fields, ...ains2fields]);
-
-      ains1fields.forEach(leaveBlank => cy.verifyForm(ains2fields, [leaveBlank], ains2));
+      cy.clearAllText([...ains1Fields, ...ains2Fields])
+        .wrap(ains1Required).each(leaveBlank => cy.verifyForm(ains2Required, [leaveBlank], ains2));
     })
   );
 
@@ -83,56 +84,32 @@ describe('Additional Insured Testing', () => {
     goBack().then(() => {
       toggleModalOn();
       addAdditional();
-      cy.clearAllText([...ains1fields, ...ains2fields])
-        .verifyForm(['ains2State'], undefined, { ains2State: 'foo' }, { errors: ['Only 2 letters allowed'] })
-        .verifyForm(['ains2Zip'], undefined, { ains2Zip: '123456789' }, { errors: ['Only 8 letters or numbers allowed'] });
+      const state = ains2Fields.find(({ name }) => name === 'ains2State');
+      const zip = ains2Fields.find(({ name }) => name === 'ains2Zip');
+      cy.clearAllText([...ains1Fields, ...ains2Fields])
+        .verifyForm([{ ...state, error: 'Only 2 letters allowed' }], undefined, { ains2State: 'foo' })
+        .verifyForm([{ ...zip, error: 'Only 8 letters or numbers allowed' }], undefined, { ains2Zip: '123456789' });
     })
   );
 
   it('POS:Additional Insured', () =>
-    goBack().then(() => {
-      const ains1LabelText = [
-        ['ains1Name1', 'Name 1'],
-        ['ains1Name2', 'Name 2'],
-        ['ains1MailingAddress1', 'Mailing Address 1'],
-        ['ains1MailingAddress2', 'Mailing Address 2'],
-        ['ains1City', 'City'],
-        ['ains1State', 'State'],
-        ['ains1Zip', 'Zip'],
-        ['ains1ReferenceNumber', 'Reference Number']
-      ];
-
-      const ains2LabelText = [
-        ['ains2Name1', 'Name 1'],
-        ['ains2Name2', 'Name 2'],
-        ['ains2MailingAddress1', 'Mailing Address 1'],
-        ['ains2MailingAddress2', 'Mailing Address 2'],
-        ['ains2City', 'City'],
-        ['ains2State', 'State'],
-        ['ains2Zip', 'Zip'],
-        ['ains2ReferenceNumber', 'Reference Number']
-      ];
-
+    goBack().then(() =>
       //AI 1 Testing
       cy.findDataTag('ains-add').should('have.attr', 'class', 'btn btn-secondary').click()
         .get('#AdditionalInsured .survey-wrapper > h3.section-group-header').should('contain', 'Additional Insured').find('i').should('have.attr', 'class', 'fa fa-user-plus')
         .findDataTag('isAdditional').find('label').should('contain', 'Do you want to add an Additional Insured?')
         .find('input[name="isAdditional"]').should('have.attr', 'value', 'true')
         .next().click().findDataTag('ains1Name1').should('not.exist')
-        .findDataTag('isAdditional').find('label[for="isAdditional"] > .switch-div').click().findDataTag('ains1Name1').should('exist');
-
-      ains1LabelText.forEach(([tag, text]) => cy.checkLabel(tag, text));
-      ains1LabelText.forEach(([tag]) => cy.checkText(tag));
+        .findDataTag('isAdditional').find('label[for="isAdditional"] > .switch-div').click().findDataTag('ains1Name1').should('exist')
+        .wrap(ains1Fields).each(({name, label}) => cy.checkLabel(name, label).checkText(name))
 
       // AI2 Testing
-      cy.findDataTag('isAdditional2').find('label').should('contain', 'Do you want to add second Additional Insured?')
+        .findDataTag('isAdditional2').find('label').should('contain', 'Do you want to add second Additional Insured?')
         .find('input[name="isAdditional2"]').should('have.attr', 'value', '')
         .next().click().findDataTag('ains2Name1').should('exist')
         .findDataTag('isAdditional2').find('label[for="isAdditional2"] > .switch-div').click().findDataTag('ains2Name1').should('not.exist')
-        .findDataTag('isAdditional2').find('label[for="isAdditional2"] > .switch-div').click().findDataTag('ains2Name1').should('exist');
-
-      ains2LabelText.forEach(([tag, text]) => cy.checkLabel(tag, text));
-      ains2LabelText.forEach(([tag]) => cy.checkText(tag));
-    })
+        .findDataTag('isAdditional2').find('label[for="isAdditional2"] > .switch-div').click().findDataTag('ains2Name1').should('exist')
+        .wrap(ains2Fields).each(({ name, label }) => cy.checkLabel(name, label).checkText(name))
+    )
   );
 });
---
+++ cypress/integration/additionalInterests/additionalInsuredInputs.js
@@ -0,0 +1,95 @@
+export const ains1Fields =  [
+  {
+    name: 'ains1Name1',
+    error: 'Field Required',
+    label: 'Name 1',
+    required: true
+  },
+  {
+    name: 'ains1Name2',
+    label: 'Name 2',
+    required: false
+  },
+  {
+    name: 'ains1MailingAddress1',
+    error: 'Field Required',
+    label: 'Mailing Address 1',
+    required: true
+  },
+  {
+    name: 'ains1MailingAddress2',
+    label: 'Mailing Address 2',
+    required: false
+  },
+  {
+    name: 'ains1City',
+    error: 'Field Required',
+    label: 'City',
+    required: true
+  },
+  {
+    name: 'ains1State',
+    error: 'Field Required',
+    label: 'State',
+    required: true
+  },
+  {
+    name: 'ains1Zip',
+    error: 'Field Required',
+    label: 'Zip',
+    required: true
+  },
+  {
+    name: 'ains1ReferenceNumber',
+    label: 'Reference Number',
+    required: false
+  }
+];
+
+export const ains2Fields = [
+  {
+    name: 'ains2Name1',
+    error: 'Field Required',
+    label: 'Name 1',
+    required: true
+  },
+  {
+    name: 'ains2Name2',
+    label: 'Name 2',
+    required: false
+  },
+  {
+    name: 'ains2MailingAddress1',
+    error: 'Field Required',
+    label: 'Mailing Address 1',
+    required: true
+  },
+  {
+    name: 'ains2MailingAddress2',
+    label: 'Mailing Address 2',
+    required: false
+  },
+  {
+    name: 'ains2City',
+    error: 'Field Required',
+    label: 'City',
+    required: true
+  },
+  {
+    name: 'ains2State',
+    error: 'Field Required',
+    label: 'State',
+    required: true
+  },
+  {
+    name: 'ains2Zip',
+    error: 'Field Required',
+    label: 'Zip',
+    required: true
+  },
+  {
+    name: 'ains2ReferenceNumber',
+    label: 'Reference Number',
+    required: false
+  }
+];
