dd92531ad39f4c0b4bcd14abd8ddbd2871c02fc1
Colin Fendrick <colin.fendrick@gmail.com>
2019-04-01 14:33:30 -0400

    Update test to do all tests

--- src/modules/Quote/QuoteWorkflow.rtl.spec.js
+++ src/modules/Quote/QuoteWorkflow.rtl.spec.js
@@ -2,10 +2,9 @@ import React from 'react';
 import 'jest-dom/extend-expect';
 import configureStore from 'redux-mock-store';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { render, fireEvent, cleanup, configure } from 'react-testing-library';
-// import { configure } from 'dom-testing-library'
+import { ph1Fields, ph2Fields, policyDetailsFields, workflowSections, pageHeaders } from './policyholderFields';
 
 import QuoteWorkflowTest from './QuoteWorkflow';
 import quoteState from './quoteworkflowStore';
@@ -45,10 +44,38 @@ describe('Testing Quote Component with react-testing-library', () => {
     return { ...render(<Router><Provider store={store}>{ui}</Provider></Router>), store };
   };
 
-  it('Works?', () => {
+  const toggleSecondUser = (dir = 'on') => {
+    const toggle = document.querySelector('[data-test="additionalPolicyholder"]');
+    if ((!toggle.classList.contains('active') && dir === 'on') || (toggle.classList.contains('active') && dir === 'off')) {
+      fireEvent.click(toggle);
+    };
+  };
+
+
+  it('POS:Primary Policyholder Label / Text', () => {
     const { getByTestId } = renderWithReduxAndRouter(<QuoteWorkflowTest {...props} />, { initialState });
-    expect(getByTestId('effectiveDate_label')).toHaveTextContent('Effective Date');
+    ph1Fields.forEach(({ name, label, data }) => {
+      expect(getByTestId(`${name}_wrapper`)).toHaveTextContent(label);
+      fireEvent.change(getByTestId(name), { target: { value: data }});
+      expect(getByTestId(name).value).toBe(data);
+    });
   });
 
+  it('POS:Primary Policyholder Label / Text', () => {
+    const { getByTestId } = renderWithReduxAndRouter(<QuoteWorkflowTest {...props} />, { initialState });
+    toggleSecondUser();
+    ph2Fields.forEach(({ name, label, data }) => {
+      expect(getByTestId(`${name}_wrapper`)).toHaveTextContent(label);
+      fireEvent.change(getByTestId(name), { target: { value: data } });
+      expect(getByTestId(name).value).toBe(data);
+    });
+  });
+
+  it('POS:Policy Details Text', () => {
+    const { getByTestId } = renderWithReduxAndRouter(<QuoteWorkflowTest {...props} />, { initialState });
+    expect(getByTestId('effectiveDate_wrapper')).toHaveTextContent('Effective Date');
+    expect(getByTestId('effectiveDate')).toHaveAttribute('type', 'date');
+    expect(getByTestId('agentCode_wrapper')).toHaveTextContent('Agent');
+  });
 });
 
