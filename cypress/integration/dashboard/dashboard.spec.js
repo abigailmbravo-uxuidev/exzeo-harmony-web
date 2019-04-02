30e4803b20655edd0307863230e7ae777a333b47
Colin Fendrick <colin.fendrick@gmail.com>
2019-04-02 13:55:00 -0400

    Add login test

--- cypress/integration/dashboard/dashboard.spec.js
+++ cypress/integration/dashboard/dashboard.spec.js
@@ -6,38 +6,8 @@ describe('Dashboard Testing', () => {
     cy.login();
   });
 
-  it('POS:Dashboard Banner', () =>
-    cy.get('img[alt="TypTap Insurance"]').should('exist').click().wait('@fetchAgency')
-      .get('a[href="tel:844-289-7968"]').should('exist')
-  );
-
-  it('POS:Dashboard Detail Header', () => cy.findDataTag('user-info').children().should('have.length', 2));
-
-  it('POS:Dashboard Side Navigation', () =>
-    ['nav-home', 'nav-searchAddress', 'nav-policy', 'nav-contacts', 'nav-training']
-      .forEach(tag => cy.findDataTag(tag).find('a').children().should('have.length', 2))
-  );
-
-  it('POS:Dashboard Footer', () =>
-    cy.get('footer div[role="banner"] small.copyright').should('contain', 'TypTap Management Company. All rights reserved.')
-      .get('footer div[role="banner"]').find('small').should('have.length', 2)
-  );
-
-  it('POS:Dashboard Text', () =>
-    cy.get('h1.app-header').should('contain', 'Agency App')
-      .get('.dashboard-message h4').should('contain', 'Homeowners (HO3) insurance for Florida properties.')
-      .get('.dashboard-message p').should('contain', 'Getting a quote is always quick and simple with ')
-      .get('h4.product-name').should('contain', ' Homeowners Insurance')
-      .get('.product.card .card-block p').should('contain', 'TypTap currently offers')
-  );
-
-  it('POS:Dashboard Image', () =>
-    cy.get('.card-header-image-home').should('exist')
-      .get('div.exzeo').should('exist')
-  );
-
   it('POS:Dashboard Button', () =>
-    ['/search/address', '/search/retrieve', '/policy'].forEach(url =>
+    cy.wrap(['/search/address', '/search/retrieve', '/policy']).each(url =>
       cy.get(`.card-footer a[href="${url}"]`).click().url().should('contain', url).go('back')
     )
   );
--- src/containers/Splash.rtl.spec.js
+++ src/containers/Splash.rtl.spec.js
@@ -31,7 +31,34 @@ describe('Testing Splash component', () => {
 
   it('POS:Dashboard Banner', () => {
     const { getByAltText, getByText } = renderWithReduxAndRouter(<Splash {...props} />, { initialState });
-    expect(getByAltText('TypTap Insurance')).toBeVisible();
+    expect(getByAltText('TypTap Insurance').parentNode).toHaveAttribute('href', '/');
     expect(getByText(/844-289-7968/)).toHaveAttribute('href', 'tel:844-289-7968');
   });
-})
+
+  it('POS:Dashboard Detail Header', () => {
+    const { getByTestId } = renderWithReduxAndRouter(<Splash {...props} />, { initialState });
+    expect(getByTestId('user-info').children.length).toEqual(2);
+  });
+
+  it('POS:Dashboard Side Navigation', () => {
+    const { getByTestId } = renderWithReduxAndRouter(<Splash {...props} />, { initialState });
+    ['nav-home', 'nav-searchAddress', 'nav-policy', 'nav-contacts', 'nav-training'].forEach(tag => {
+      expect(getByTestId(tag).firstChild.children.length).toEqual(2);
+    });
+  });
+
+  it('POS:Dashboard Footer', () => {
+    const { getByText } = renderWithReduxAndRouter(<Splash {...props} />, { initialState });
+    expect(getByText(/TypTap Management Company/).className).toEqual('copyright');
+  });
+
+  it('POS:Dashboard Text', () => {
+    const { getByText, container } = renderWithReduxAndRouter(<Splash {...props} />, { initialState });
+    expect(getByText('Agency App').className).toEqual('app-header');
+    expect(getByText(/insurance for Florida properties/));
+    expect(getByText(/Getting a quote is always quick and simple with/));
+    expect(getByText(/Homeowners Insurance/).className).toEqual('product-name');
+    expect(getByText(/TypTap currently offers/));
+    expect(container.querySelector('.card-header.image.card-header-image-home')).not.toEqual(null);
+  });
+});
