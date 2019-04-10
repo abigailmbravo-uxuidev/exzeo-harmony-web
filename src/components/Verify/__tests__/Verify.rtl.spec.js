022a300e0b8d1d868d86f704df8fadee29f4a360
Colin Fendrick <colin.fendrick@gmail.com>
2019-04-10 13:23:49 -0400

    Update verify test

--- src/components/Verify/__tests__/Verify.rtl.spec.js
+++ src/components/Verify/__tests__/Verify.rtl.spec.js
@@ -12,6 +12,8 @@ import {
 
 import ConnectedVerify from '../Verify';
 
+const { checkButton } = testHelpers;
+
 // TODO: Return to this test once Verify has been updated to use reusable component structure
 describe('Verify Testing', () => {
   const props = {
@@ -32,5 +34,49 @@ describe('Verify Testing', () => {
   it('should render', () => {
     const { getByText } = renderWithReduxAndRouter(<ConnectedVerify {...props} />, { state });
     expect(getByText('Property Details'));
+  });
+
+  it('POS:Property Details Text', () => {
+    const { getByText } = renderWithReduxAndRouter(<ConnectedVerify {...props} />, { state });
+    ['Quote Number', 'Property Address', 'Year Built', 'Effective Date',
+      '12-5162296-01', /4131 TEST ADDRESS/, '1998', '05/05/2019'
+    ].forEach(text => expect(getByText(text)));
+  });
+
+  it('POS:Quote Details', () => {
+    const { getByText } = renderWithReduxAndRouter(<ConnectedVerify {...props} />, { state });
+    [/Yearly Premium/, /Dwelling/, /Other Structures/, /Personal Property/, /Loss Of Use/, /Personal Liability/,
+      /Medical Payments/, /Personal Property Replacement Cost/, /Mold Property/, /Mold Liability/, /Ordinance or Law/,
+      /All Other Perils Deductible/, /Hurricane Deductible/, /Sinkhole Deductible/
+    ].forEach(text => expect(getByText(text)));
+  });
+
+  it('POS:Policyholder Details Text', () => {
+    const { getByText } = renderWithReduxAndRouter(<ConnectedVerify {...props} />, { state });
+    expect(getByText(/Please be sure the information below/));
+  });
+
+  it('POS:Policyholder Details Primary / Secondary Policyholder', () => {
+    const { getByText } = renderWithReduxAndRouter(<ConnectedVerify {...props} />, { state });
+    [/Name/, /Phone Number/, /Email/].forEach(text => expect(getByText(text)));
+  });
+
+  it('POS:Mailing Address Text', () => {
+    const { getByText } = renderWithReduxAndRouter(<ConnectedVerify {...props} />, { state });
+    [/Street Address/, /City\/State\/Zip/, /Country/].forEach(text => expect(getByText(text)));
+  });
+
+  it('POS:Verify Toggle', () => {
+    const { getByTestId } = renderWithReduxAndRouter(<ConnectedVerify {...props} />, { state });
+    ['confirmProperyDetails', 'confirmQuoteDetails', 'confirmPolicyHolderDetails', 'confirmAdditionalInterestsDetails'
+    ].forEach(tag => {
+      expect(getByTestId(tag)).toHaveTextContent('Verified');
+      document.querySelector(`[data-test="${tag}"] div.switch-div`)
+    });
+  });
+
+  it('POS:Next Button', () => {
+    const { getByTestId } = renderWithReduxAndRouter(<ConnectedVerify {...props} />, { state });
+    expect(getByTestId('submit').getAttribute('type')).toEqual('submit');
   });
 });
