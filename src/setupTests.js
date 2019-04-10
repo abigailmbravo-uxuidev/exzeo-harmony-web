bfdfb1bd6a2dee0c7d00c8263a36e77bdeb84c1f
Colin Fendrick <colin.fendrick@gmail.com>
2019-04-10 11:33:01 -0400

    Delete all rtl info

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,10 +1,7 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import 'react-testing-library/cleanup-after-each';
-import { configure as reactTestingConfigure } from 'react-testing-library';
 
 configure({ adapter: new Adapter() });
-reactTestingConfigure({ testIdAttribute: 'data-test' });
 
 const localStorageMock = {
   setItem(key, value) {
@@ -23,4 +20,4 @@ const localStorageMock = {
 
 global.localStorage = localStorageMock;
 
-export default localStorageMock;
+export default localStorageMock
