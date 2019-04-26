5a390a2f97eefa9966b5929b7dd173565ce21c03
Joe Sutphin <jsutphin@exzeo.com>
2019-04-26 09:11:41 -0400

    update policyholder modal

--- src/modules/Quote/Verify/PolicyHolderPopup.jsx
+++ src/modules/Quote/Verify/PolicyHolderPopup.jsx
@@ -5,52 +5,50 @@ import {Button} from '@exzeo/core-ui';
 
 const PolicyHolderPopup = ({formValues, handleFormSubmit, handleCancel, submitting}) => {
   return (
-    <div className="edit-policyholder-modal modal active" role="article">
-      <div className="survey-wrapper">
+    <div className="modal modal-xl" role="article">
         <Form
           onSubmit={handleFormSubmit}
           initialValues={formValues}
           render={({values, handleSubmit}) => (
-            <form onSubmit={handleSubmit}>
-              <div className="contact-message">
-                <div className="card card-policyholder">
-
-                  <React.Fragment>
-                    <div className="card-block">
-                      <PolicyHolder
-                        isPrimary
-                        fieldPrefix="policyHolders[0]"
-                        formValues={values}
-                        title="Primary Policyholder"
-                      />
-                      <PolicyHolder
-                        fieldPrefix="policyHolders[1]"
-                        watchField="removeSecondary"
-                        formValues={values}
-                        title="Secondary Policyholder"
-                      />
-                    </div>
-                    <div className="card-footer">
-                      <Button
-                        className={Button.constants.classNames.secondary}
-                        onClick={handleCancel}
-                        data-test="modal-cancel"
-                      >Cancel</Button>
-                      <Button
-                        className={Button.constants.classNames.primary}
-                        type="submit"
-                        disabled={submitting}
-                        data-test="modal-submit"
-                      >Save</Button>
-                    </div>
-                  </React.Fragment>
-
+            <div className="card card-policyholder">
+              <form onSubmit={handleSubmit}>  
+                <div className="card-header">
+                  <h4>
+                    <i className="fa fa-pencil" />
+                    &nbsp;Edit Policyholder(s)
+                  </h4>
                 </div>
-              </div>
-            </form>
+                <div className="card-block">
+                  <PolicyHolder
+                    isPrimary
+                    fieldPrefix="policyHolders[0]"
+                    formValues={values}
+                    title="Primary Policyholder"
+                  />
+                  <PolicyHolder
+                    fieldPrefix="policyHolders[1]"
+                    watchField="removeSecondary"
+                    formValues={values}
+                    title="Secondary Policyholder"
+                  />
+                </div>
+                <div className="card-footer">
+                  <Button
+                    className={Button.constants.classNames.secondary}
+                    onClick={handleCancel}
+                    data-test="modal-cancel"
+                  >Cancel</Button>
+                  <Button
+                    className={Button.constants.classNames.primary}
+                    type="submit"
+                    disabled={submitting}
+                    data-test="modal-submit"
+                  >Save</Button>
+                </div>
+              </form>
+            </div>
           )}
         />
-      </div>
     </div>
   );
 };
