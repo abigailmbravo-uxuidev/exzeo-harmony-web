2478ef532f4859661b8b4bdae266ac49b7510c43
Colin Fendrick <colin.fendrick@gmail.com>
2019-04-29 15:25:01 -0400

    Add bill payer test to additionalInterest rl spec

--- src/modules/Quote/__tests__/AdditionalInterests.rtl.spec.js
+++ src/modules/Quote/__tests__/AdditionalInterests.rtl.spec.js
@@ -113,7 +113,7 @@ describe('Testing Additional Interests', () => {
     });
   });
   
-  it('POS:Confirm Additional Interests Show Up In Order and Disable Buttons', () => {
+  it('POS:Confirm Additional Interests Show Up In Order and Disable Buttons [Premium Finance]', () => {
     const newState = {
       ...state,
       quoteState: {
@@ -141,8 +141,47 @@ describe('Testing Additional Interests', () => {
       'Premium Finance 1'
     ];
 
-    const { getByText, getAllByText, getByTestId } = renderWithReduxAndRouter(<ConnectedQuoteWorkflow {...props} />, { state: newState });
+    const { getByText } = renderWithReduxAndRouter(<ConnectedQuoteWorkflow {...props} />, { state: newState });
     // ...so we know the UI will still organize and sort them correctly, in order
+    const labelTexts = document.querySelectorAll('.results.result-cards li.card .card-icon label');
+    labelTexts.forEach((label, i) => expect(label.textContent).toEqual(expectedLabels[i]));
+
+    expect(getByText('Mortgagee')).toBeDisabled();
+    expect(getByText('Additional Insured')).toBeDisabled();
+    expect(getByText('Additional Interest')).toBeDisabled();
+    expect(getByText('Premium Finance')).toBeDisabled();
+    expect(getByText('Bill Payer')).toBeDisabled();
+  });
+
+  // Identical test to the one above except with a Bill Payer
+  it('POS:Confirm Additional Interests Show Up In Order and Disable Buttons [Bill Payer]', () => {
+    const newState = {
+      ...state,
+      quoteState: {
+        quote: {
+          ...state.quoteState.quote,
+          additionalInterests: [
+            { ...additionalInterest, order: 1, type: 'Additional Interest' },
+            { ...additionalInterest, order: 2, type: 'Mortgagee' },
+            { ...additionalInterest, order: 1, type: 'Additional Insured' },
+            { ...additionalInterest, order: 0, type: 'Mortgagee' },
+            { ...additionalInterest, order: 1, type: 'Mortgagee' },
+            { ...additionalInterest, order: 0, type: 'Bill Payer' },
+            { ...additionalInterest, order: 0, type: 'Additional Interest' },
+            { ...additionalInterest, order: 0, type: 'Additional Insured' },
+          ]
+        }
+      }
+    };
+
+    const expectedLabels = [
+      'Mortgagee 1', 'Mortgagee 2', 'Mortgagee 3',
+      'Additional Insured 1', 'Additional Insured 2',
+      'Additional Interest 1', 'Additional Interest 2',
+      'Bill Payer 1'
+    ];
+
+    const { getByText } = renderWithReduxAndRouter(<ConnectedQuoteWorkflow {...props} />, { state: newState });
     const labelTexts = document.querySelectorAll('.results.result-cards li.card .card-icon label');
     labelTexts.forEach((label, i) => expect(label.textContent).toEqual(expectedLabels[i]));
 
