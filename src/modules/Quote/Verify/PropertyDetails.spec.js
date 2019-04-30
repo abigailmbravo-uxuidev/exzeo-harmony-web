ce57ef0ce538ba01ae40fe51bdc660b2116fd3ec
Jordan Rhodes <jsrhodes15@gmail.com>
2019-04-29 21:18:46 -0400

    Fix tests

--- src/modules/Quote/Verify/PropertyDetails.spec.js
+++ src/modules/Quote/Verify/PropertyDetails.spec.js
@@ -5,7 +5,7 @@ import PropertyDetails from './PropertyDetails';
 describe('Test PropertyDetails component', () => {
   it('should render PropertyDetails', () => {
     const props = {
-      quoteNumber: '12-1234567-01', effectiveDate: '2020-01-01', property: {}, selectedAgent: {}
+      quoteNumber: '12-1234567-01', effectiveDate: '2020-01-01', property: { physicalAddress: {}}, selectedAgent: {}
     };
     const wrapper = mount(<PropertyDetails {...props} />);
     expect(wrapper.exists()).toBeTruthy();
