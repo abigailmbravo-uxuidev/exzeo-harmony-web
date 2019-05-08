35e7ef1a6872af0d7189fdb9f54495872bec4469
Jordan Rhodes <jsrhodes15@gmail.com>
2019-05-08 10:09:38 -0400

    Use AdditionalInterest Card from core-ui

--- src/modules/Quote/Verify/AdditionalInterestDetails.jsx
+++ src/modules/Quote/Verify/AdditionalInterestDetails.jsx
@@ -1,42 +1,19 @@
 import React from 'react';
-import classNames from 'classnames';
 import { defaultMemoize } from 'reselect';
-import { getSortedAdditionalInterests } from '@exzeo/core-ui/src/@Harmony/AdditionalInterests/utilities';
+import { AdditionalInterestCard, getSortedAdditionalInterests } from '@exzeo/core-ui/src/@Harmony';
 
 const sortAdditionalInterests = defaultMemoize(getSortedAdditionalInterests);
-
-function formatTitle(type, order) {
-  return `${type} ${order + 1}`;
-}
 
 export const AdditionalInterestDetails = ({ additionalInterests }) => {
   const sortedAdditionalInterests = sortAdditionalInterests(additionalInterests);
 
   return (
     <section className="display-element additional-interests">
+      <ul>
       {sortedAdditionalInterests.map((additionalInterest) =>
-        <div className="card" key={additionalInterest._id}>
-          <div className="icon-wrapper">
-            <i className={classNames('fa', additionalInterest.type)}/>
-            <p>{formatTitle(additionalInterest.type, additionalInterest.order)}</p>
-          </div>
-          <section>
-            <h4>{additionalInterest.name1}</h4>
-            <h4>{additionalInterest.name2 || ''}</h4>
-            <p>
-              {additionalInterest.mailingAddress.address1}
-              {additionalInterest.mailingAddress.address2 ? `, ${additionalInterest.mailingAddress.address2}` : ''}
-            </p>
-            <p>
-              {`${additionalInterest.mailingAddress.city}, ${additionalInterest.mailingAddress.state} ${additionalInterest.mailingAddress.zip}`}
-            </p>
-          </section>
-          <div className="ref-number">
-            <label htmlFor="ref-number">Reference Number</label>
-            <span>{additionalInterest.referenceNumber || ''}</span>
-          </div>
-        </div>
+        <AdditionalInterestCard ai={additionalInterest} />
       )}
+      </ul>
     </section>
   );
 };
