976fe0cd043abaf83b73f7b558eaad883afef513
Colin Fendrick <colin.fendrick@gmail.com>
2019-05-14 16:48:58 -0400

    Add error test and uw exceptions to rtl

--- src/test-utils/fixtures/index.js
+++ src/test-utils/fixtures/index.js
@@ -4,4 +4,5 @@ export { default as latestPolicy } from './latestPolicy';
 export { customizeList, mailingBillingList, underwritingList } from './listData';
 export { default as rating } from './rating';
 export { default as policyholder } from './policyholder';
+export { default as underwritingException } from './underwritingException';
 export { default as quote } from './quote';
---
+++ src/test-utils/fixtures/underwritingException.js
@@ -0,0 +1,9 @@
+export default {
+  code: '108',
+  displayText: 'underwritingAnswers.rented = Yes',
+  category: 'Underwriting Answers',
+  action: 'Fatal Error',
+  agentMessage: 'Homes that are rented are not eligible for this program.',
+  internalMessage: 'Homes that are rented are not eligible for this program.',
+  active: true
+};
