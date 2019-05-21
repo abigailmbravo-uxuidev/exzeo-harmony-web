31c461bf54382758add665adb874c1286ff3fe0a
Colin Fendrick <colin.fendrick@gmail.com>
2019-05-21 10:14:32 -0400

    Document rtl fns -- modify queryParser

--- src/test-utils/helpers.js
+++ src/test-utils/helpers.js
@@ -1,3 +1,7 @@
+/**
+ * @param {Object} slider - The DOM node of the slider to alter.
+ * @param {number | string} value - The value to which to set the slider.
+ */
 export const setSliderValue = (slider, value) => {
   // React sliders overwrite the value setter, so we reach to get it from the window
   const nativeInputValueSetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, 'value').set;
--- src/test-utils/test-utils.js
+++ src/test-utils/test-utils.js
@@ -70,7 +70,12 @@ export const defaultInitialState = {
   }
 };
 
-export const renderWithReduxAndRouter = (ui, { state = defaultInitialState, store = mockStore(state) } = {}) =>
+export /**
+ * @param {Object} ui - React component to be Rendered
+ * @param {Object} [{ state = defaultInitialState, store = mockStore(state) }={}] - The state and store, both optional, to be used.
+ * If state is provided but store is not, store will be mocked from the given state.
+ */
+const renderWithReduxAndRouter = (ui, { state = defaultInitialState, store = mockStore(state) } = {}) =>
   ({
     ...render(<Router><Provider store={store}>{ui}</Provider></Router>),
     // Return our mock store, in case we want to do something with it in a test
@@ -89,43 +94,73 @@ export const defaultProps = {
   match: { params: {}}
 };
 
+/**
+ * A function to handle your query and your field and find the correct DOM element.
+ * This way the check functions below can handle any query type from react-testing-library.
+ * @param {Object} query - The function from react-testing-library to be used.
+ * @param {Object} field - The field object to find and test.
+ * @returns {Object} Node - The DOM node found via the query.
+ */
 const parseQueryType = (query, field) => {
   // We determine which field value to use based on query name
   const queryName = query.name.replace(/bound /g, '');
-  switch (queryName) {
-    case 'getByTestId':
-      return query(field.name);
-    case 'getByText':
-      return query(field.text);
-    case 'getByLabelText':
-      return query(field.label);
-    default:
-      return query(field.name);
-  };
+
+  if (queryName.includes('ByTestId')) return query(field.name);
+  if (queryName.includes('ByText')) return query(field.text);
+  if (queryName.includes('ByLabelText')) return query(field.label);
+  else return query(field.name);
 };
 
+/**
+ * @param {Object} query - The function from react-testing-library to be used.
+ * @param {regex} [button=/submit/] - The regex used to find the button.
+ */
 export const submitForm = (query, button = /submit/) => fireEvent.click(query(button));
 
+/**
+ * @param {Object} query - The function from react-testing-library to be used.
+ * @param {Object} field - The field object to find and test.
+ */
 export const clearText = (query, field) => fireEvent.change(parseQueryType(query, field), { target: { value: '' }});
 
+/**
+ * @param {Object} query - The function from react-testing-library to be used.
+ * @param {Object} field [{ name = '', text = '', label = '', error = 'Field Required' }={}] - The field object to find and test.
+ */
 export const checkError = (query, { name = '', text = '', label = '', error = 'Field Required' } = {}) =>
   expect(parseQueryType(query, { name: `${name}_error`, text, label, error })).toHaveTextContent(error);
 
+/**
+ * @param {Object} query - The function from react-testing-library to be used.
+ * @param {Object} field { name = '', text = '', label } - The field object to find and test.
+ */
 export const checkLabel = (query, { name = '', text = '', label }) => expect(parseQueryType(query, { name: `${name}_label`, text, label })).toHaveTextContent(label);
 
+/**
+ * @param {Object} query - The function from react-testing-library to be used.
+ * @param {Object} field - The field object to find and test.
+ */
 export const checkTextInput = (query, field) => {
   const input = parseQueryType(query, field);
   fireEvent.change(input, { target: { value: field.data } });
   expect(input.value).toBe(field.data);
 };
 
+/**
+ * @param {Object} query - The function from react-testing-library to be used.
+ * @param {Object} field - The field object to find and test.
+ */
 export const checkPhoneInput = (query, field) => {
   const input = parseQueryType(query, field);
   fireEvent.change(input, { target: { value: field.data }});
   expect(input.value).toMatch(new RegExp(field.data.slice(0, 2)));
 };
 
-export const checkRadio = (query, { name = '', text = '', label = '', values }) => {
+/**
+ * @param {Object} query - The function from react-testing-library to be used.
+ * @param {Object} field { name = '', text = '', label = '', values } - The field object to find and test.
+ */
+export const checkRadio = (query, { name = '', text = '', label = '', values }) =>
   values.forEach(value => {
     // Get the option to select and click it
     const selectedOption = parseQueryType(query, { name: `${name}_${value}`, text, label });
@@ -136,8 +171,11 @@ export const checkRadio = (query, { name = '', text = '', label = '', values }) 
     values.filter(uncheckedValue => value !== uncheckedValue)
       .forEach(uncheckedValue => expect(parseQueryType(query, { name: `${name}_${uncheckedValue}`, text, label }).parentNode.className).toEqual('label-segmented'));
   });
-};
 
+/**
+* @param {Object} query - The function from react-testing-library to be used.
+* @param {Object} field - The field object to find and test.
+*/
 export const checkSwitch = (query, field) => {
   const switchDiv = parseQueryType(query, field);
   // Toggle switch twice, check value each time
@@ -148,6 +186,10 @@ export const checkSwitch = (query, field) => {
   expect(switchDiv.getAttribute('data-value')).toEqual(`${!!field.defaultValue}`);
 };
 
+/**
+ * @param {Object} query - The function from react-testing-library to be used.
+ * @param {Object} field { name = '', text = '', label = '' } - The field object to find and test.
+ */
 export const checkSlider = (query, { name = '', text = '', label = '' }) => {
   // We check slider min and max value
   const slider = parseQueryType(query, { name: `${name}-slider`, text, label });
@@ -160,6 +202,10 @@ export const checkSlider = (query, { name = '', text = '', label = '' }) => {
   expect(slider.value).toEqual(max);
 };
 
+/**
+ * @param {Object} query - The function from react-testing-library to be used.
+ * @param {Object} field { name = '', text, label = '', icon = false } - The field object to find and test.
+ */
 export const checkHeader = (query, { name = '', text, label = '', icon = false }) => {
   // const header = query(name || text);
   const header = parseQueryType(query, { name, text, label });
@@ -171,18 +217,32 @@ export const checkHeader = (query, { name = '', text, label = '', icon = false }
   }
 };
 
-export const checkSelect = (query, field, queryOptions) => {
-  const select = parseQueryType(query, field, queryOptions);
+/**
+ * @param {Object} query - The function from react-testing-library to be used.
+ * @param {Object} field - The field object to find and test.
+ */
+export const checkSelect = (query, field) => {
+  const select = parseQueryType(query, field);
   field.values && field.values.forEach(value => {
     fireEvent.change(select, { target: { value }});
     expect(select.getAttribute('data-selected')).toEqual(value);
   });
 };
 
+/**
+ * @param {Object} query - The function from react-testing-library to be used.
+ * @param {Object} field [field={ name: 'submit' }] - The field object to find and test.
+ */
 export const checkButton = (query, field = { name: 'submit' }) =>
   expect(parseQueryType(query, field).getAttribute('type')).toEqual('button');
 
-// This function is used to verify specific submit errors for one field as well
+/**
+ * This function is used to verify specific submit errors for one field as well
+ * @param {Object} query - The function from react-testing-library to be used.
+ * @param {Array} [baseFields=[]] - Array of field objects to fill out.
+ * @param {Array} [fieldsLeftBlank=[]] - Array of field objects to leave blank.
+ * @param {regex} button - The regex used to find the button.
+ */
 export const verifyForm = (query, baseFields = [], fieldsLeftBlank = [], button) => {
   // Clears all text
   [...baseFields, ...fieldsLeftBlank].forEach(field => clearText(query, field));
