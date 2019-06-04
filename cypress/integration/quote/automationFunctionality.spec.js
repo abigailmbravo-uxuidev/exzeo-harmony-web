6c300568a725a6ce5c7d25b7e21ec9554e14141f
Colin Fendrick <colin.fendrick@gmail.com>
2019-06-04 13:08:42 -0400

    Break up back button test into multiple its

--- cypress/integration/quote/automationFunctionality.spec.js
+++ cypress/integration/quote/automationFunctionality.spec.js
@@ -1,101 +1,105 @@
-// import {
-//   setRouteAliases,
-//   navigateThroughLanding,
-//   navigateThroughSearchAddress,
-//   navigateThroughPolicyholder,
-//   navigateThroughUnderwriting,
-//   navigateThroughCustomize,
-//   navigateThroughShare,
-//   navigateThroughAssumptions,
-//   navigateThroughAdditionalInterests,
-//   navigateThroughMailingBilling,
-//   navigateThroughScheduleDate,
-//   navigateThroughVerify
-// } from '../../helpers';
+import {
+  setRouteAliases,
+  navigateThroughLanding,
+  navigateThroughSearchAddress,
+  navigateThroughPolicyholder,
+  navigateThroughUnderwriting,
+  navigateThroughCustomize,
+  navigateThroughShare,
+  navigateThroughAssumptions,
+  navigateThroughAdditionalInterests,
+  navigateThroughMailingBilling,
+  navigateThroughScheduleDate,
+  navigateThroughVerify
+} from '../../helpers';
 
-// describe('Back Button Testing', () => {
-//   // Grab the quote number off the ui and retrieve it to confirm our quote is searchable with the quote number
-//   const getQuoteNumberAndRetrieve = status =>
-//     cy.findDataTag('quoteNumberDetail').find('> dl > div > dd').then($quote =>
-//       cy.go('back')
-//         .get('div.dashboard-message').should('exist')
-//         .get('.btn[href="/search/retrieve"]').click()
-//         .findDataTag('quoteNumber').type($quote.text())
-//         .clickSubmit('#SearchBar')
-//         .findDataTag('quote-list').should('not.be.empty')
-//         .find('li.card .quote-state').should('contain', status)
-//         .go('back')
-//     );
+describe('Back Button Testing', () => {
+  // Grab the quote number off the ui and retrieve it to confirm our quote is searchable with the quote number
+  const getQuoteNumberAndRetrieve = status =>
+    cy.findDataTag('quoteNumberDetail').find('> dl > div > dd').then($quote =>
+      cy.go('back')
+        .get('div.dashboard-message').should('exist')
+        .get('.btn[href="/search/retrieve"]').click()
+        .findDataTag('quoteNumber').type($quote.text())
+        .clickSubmit('#SearchBar')
+        .findDataTag('quote-list').should('not.be.empty')
+        .find('li.card .quote-state').should('contain', status)
+        .go('back')
+    );
 
-//   before('Login and go to search', () => {
-//     cy.login();
-//     navigateThroughLanding();
-//   });
-//   beforeEach('Set Route Aliases', () => setRouteAliases());
+  before('Login and go to search', () => {
+    cy.login();
+    navigateThroughLanding();
+  });
+  beforeEach('Set Route Aliases', () => setRouteAliases());
 
-//   it('Browser Back Button', () => {
-//     cy.go('back')
-//       .get('div.dashboard-message').should('exist');
+  it('Browser Back Button', () => {
+    cy.go('back')
+      .get('div.dashboard-message').should('exist');
 
-//     navigateThroughLanding();
-//     navigateThroughSearchAddress();
-//     cy.wait('@getZipcodeSettings').go('back')
-//       .get('div.dashboard-message').should('exist');
+    navigateThroughLanding();
+    navigateThroughSearchAddress();
+    cy.wait('@getZipcodeSettings').go('back')
+      .get('div.dashboard-message').should('exist');
+  });
+  
+  it('Browser Back Button pt 2', () => {
+    navigateThroughLanding();
+    navigateThroughSearchAddress();
+    navigateThroughPolicyholder();
+    cy.go('back')
+      .get('div.dashboard-message').should('exist');
 
-//     navigateThroughLanding();
-//     navigateThroughSearchAddress();
-//     navigateThroughPolicyholder();
-//     cy.go('back')
-//       .get('div.dashboard-message').should('exist');
+    navigateThroughLanding();
+    navigateThroughSearchAddress();
+    navigateThroughPolicyholder();
+    navigateThroughUnderwriting();
+    getQuoteNumberAndRetrieve('Quote Qualified');
 
-//     navigateThroughLanding();
-//     navigateThroughSearchAddress();
-//     navigateThroughPolicyholder();
-//     navigateThroughUnderwriting();
-//     getQuoteNumberAndRetrieve('Quote Qualified');
+    navigateThroughLanding();
+    navigateThroughSearchAddress();
+    navigateThroughPolicyholder();
+    navigateThroughUnderwriting();
+    navigateThroughCustomize();
+    getQuoteNumberAndRetrieve('Quote Qualified');
 
-//     navigateThroughLanding();
-//     navigateThroughSearchAddress();
-//     navigateThroughPolicyholder();
-//     navigateThroughUnderwriting();
-//     navigateThroughCustomize();
-//     getQuoteNumberAndRetrieve('Quote Qualified');
+    navigateThroughLanding();
+    navigateThroughSearchAddress();
+    navigateThroughPolicyholder();
+    navigateThroughUnderwriting();
+    navigateThroughCustomize();
+    navigateThroughShare();
+    navigateThroughAssumptions();
+    navigateThroughAdditionalInterests();
+    navigateThroughMailingBilling();
+    getQuoteNumberAndRetrieve('Application Ready');
+  })
 
-//     navigateThroughLanding();
-//     navigateThroughSearchAddress();
-//     navigateThroughPolicyholder();
-//     navigateThroughUnderwriting();
-//     navigateThroughCustomize();
-//     navigateThroughShare();
-//     navigateThroughAssumptions();
-//     navigateThroughAdditionalInterests();
-//     navigateThroughMailingBilling();
-//     getQuoteNumberAndRetrieve('Application Ready');
+  it('Browser back button pt 3', () => {
+    navigateThroughLanding();
+    navigateThroughSearchAddress();
+    navigateThroughPolicyholder();
+    navigateThroughUnderwriting();
+    navigateThroughCustomize();
+    navigateThroughShare();
+    navigateThroughAssumptions();
+    navigateThroughAdditionalInterests();
+    navigateThroughMailingBilling();
+    navigateThroughVerify();
+    getQuoteNumberAndRetrieve('Application Ready');
 
-//     navigateThroughLanding();
-//     navigateThroughSearchAddress();
-//     navigateThroughPolicyholder();
-//     navigateThroughUnderwriting();
-//     navigateThroughCustomize();
-//     navigateThroughShare();
-//     navigateThroughAssumptions();
-//     navigateThroughAdditionalInterests();
-//     navigateThroughMailingBilling();
-//     navigateThroughVerify();
-//     getQuoteNumberAndRetrieve('Application Ready');
-
-//     navigateThroughLanding();
-//     navigateThroughSearchAddress();
-//     navigateThroughPolicyholder();
-//     navigateThroughUnderwriting();
-//     navigateThroughCustomize();
-//     navigateThroughShare();
-//     navigateThroughAssumptions();
-//     navigateThroughAdditionalInterests();
-//     navigateThroughMailingBilling();
-//     navigateThroughVerify();
-//     navigateThroughScheduleDate();
-//     cy.wait('@sendApplication').go('back')
-//       .get('div.dashboard-message').should('exist');
-//   });
-// });
+    navigateThroughLanding();
+    navigateThroughSearchAddress();
+    navigateThroughPolicyholder();
+    navigateThroughUnderwriting();
+    navigateThroughCustomize();
+    navigateThroughShare();
+    navigateThroughAssumptions();
+    navigateThroughAdditionalInterests();
+    navigateThroughMailingBilling();
+    navigateThroughVerify();
+    navigateThroughScheduleDate();
+    cy.wait('@sendApplication').go('back')
+      .get('div.dashboard-message').should('exist');
+  })
+});
