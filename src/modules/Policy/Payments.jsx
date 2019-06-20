0b1958a46e0043b9f2d44f4144c149191ab54b12
Mark Eads <meads@hcpci.com>
2019-06-20 11:40:30 -0400

    add payments received

--- src/modules/Policy/Payments.jsx
+++ src/modules/Policy/Payments.jsx
@@ -5,17 +5,22 @@ const amountFormatter = amt => (amt ? `$ ${amt.toLocaleString('en', { minimumFra
 const dateFormatter = cell => `${cell.substring(0, 10)}`;
 
 const Payments = ({ initialValues }) => {
-  const { billing: { payments } } = initialValues;
+  const { billing: { payments, cashReceived } } = initialValues;
   const flattenedPaymentHistory = payments.map(({ amount, ...rest }) => ({ amount: Number(amount.$numberDecimal), ...rest }));
 
-  return <div className="table-view">
-    <BootstrapTable className="" data={flattenedPaymentHistory} striped hover>
-      <TableHeaderColumn isKey dataField="date" dataFormat={dateFormatter} className="date" columnClassName="date" width="150" dataSort>Date</TableHeaderColumn>
-      <TableHeaderColumn dataField="type" className="type" columnClassName="type" dataSort width="150">Type</TableHeaderColumn>
-      <TableHeaderColumn dataField="description" className="description" columnClassName="description" dataSort>Description</TableHeaderColumn>
-      <TableHeaderColumn dataField="amount" dataFormat={amountFormatter} className="amount" columnClassName="amount" width="150" dataSort dataAlign="right">Amount</TableHeaderColumn>
-    </BootstrapTable>
-  </div>
+  return (<React.Fragment>
+    <div className="table-view">
+      <BootstrapTable className="" data={flattenedPaymentHistory} striped hover>
+        <TableHeaderColumn isKey dataField="date" dataFormat={dateFormatter} className="date" columnClassName="date" width="150" dataSort>Date</TableHeaderColumn>
+        <TableHeaderColumn dataField="type" className="type" columnClassName="type" dataSort width="150">Type</TableHeaderColumn>
+        <TableHeaderColumn dataField="description" className="description" columnClassName="description" dataSort>Description</TableHeaderColumn>
+        <TableHeaderColumn dataField="amount" dataFormat={amountFormatter} className="amount" columnClassName="amount" width="150" dataSort dataAlign="right">Amount</TableHeaderColumn>
+      </BootstrapTable>
+    </div>
+    <div className="payments-received">
+      <h4>Payments Received: $ {cashReceived.$numberDecimal}</h4>
+    </div>
+ </React.Fragment>);
 };
 
 export default Payments;
