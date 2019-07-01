7e5ee8c8dd8f855f6c64de15f331dc75da29c22d
Colin Fendrick <colin.fendrick@gmail.com>
2019-07-01 12:40:31 -0400

    Update scheduler modal test

--- src/modules/Quote/Verify/__tests__/ScheduleDate.rtl.spec.js
+++ src/modules/Quote/Verify/__tests__/ScheduleDate.rtl.spec.js
@@ -10,7 +10,8 @@ describe('Congragulations Schedule Date Modal testing', () => {
       submitting: false,
       entity: { policyHolders: [policyholder]},
       companyName: 'Mom Co.',
-      productDescription: 'Robot'
+      productDescription: 'Robot',
+      selectedAgent: { emailAddress: 'test@robot.co' }
     };
 
     const { getByText } = render(<ScheduleDate {...props} />);
@@ -18,7 +19,7 @@ describe('Congragulations Schedule Date Modal testing', () => {
     expect(getByText('You have successfully completed a Mom Co. Robot Quote.'));
     expect(getByText('With this information, we will generate the Robot Application, and e-mail it to:'));
     expect(getByText('Bruce Wayne (Batman@gmail.com)'));
-    expect(getByText('A copy will also be sent to you ()'));
+    expect(getByText('A copy will also be sent to you (test@robot.co)'));
     expect(getByText('Once all electronic signatures have been received, the policy will automatically be bound and the policy documents will be emailed to you and to the policyholder.'));
     expect(getByText('NOTE: All signatures must be completed within 10 days, or the application will expire.'));
     expect(getByText('Once you send, no changes can be made to this quote.'));
@@ -35,7 +36,10 @@ describe('Congragulations Schedule Date Modal testing', () => {
       productDescription: 'Robot'
     };
 
-    const { getAllByTestId } = render(<ScheduleDate {...props} />);
+    const { getAllByTestId, getByTestId } = render(<ScheduleDate {...props} />);
     expect(getAllByTestId('modal-submit').length).toEqual(1);
+    expect(getByTestId('modal-edit').textContent).toEqual('Edit Quote');
+    expect(getByTestId('modal-save').textContent).toEqual('Save Quote, Continue Later');
+    expect(getByTestId('modal-submit').textContent).toEqual('Send Application For Signature');
   });
 });
