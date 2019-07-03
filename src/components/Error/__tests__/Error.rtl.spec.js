9dd9e5c73722ec907b12fb0fe8beb4561be1614b
Colin Fendrick <colin.fendrick@gmail.com>
2019-07-03 08:14:32 -0400

    Add error popup unit test

--- src/components/Error/__tests__/Error.rtl.spec.js
+++ src/components/Error/__tests__/Error.rtl.spec.js
@@ -17,12 +17,12 @@ describe('Error testing', () => {
     };
 
     const { getByText, queryByText } = renderWithReduxAndRouter(<ConnectedError />, { state });
+    expect(getByText('Property does not qualify for automated quote').querySelector('i').className).toEqual('fa fa-exclamation-triangle');
     expect(getByText('The following errors have occurred for this property:'));
-    expect(getByText('Property does not qualify for automated quote'));
-    expect(queryByText('Please contact one of our representatives so they may further assist you in obtaining a HO3 insurance quote for this property.')).not.toBeInTheDocument();
     expect(getByText('Homes that are rented are not eligible for this program.'));
-    expect(getByText('email us'));
-    expect(getByText('(844) 289-7968'));
+    expect(queryByText('Please contact one of our representatives so they may further assist you in obtaining a HO3 insurance quote for this property.')).not.toBeInTheDocument();
+    expect(getByText('email us').parentNode).toHaveAttribute('href', 'mailto:customerservice@typtap.com');
+    expect(getByText('(844) 289-7968').parentNode).toHaveAttribute('href', 'tel:8442897968');
   });
 
   it('Has an underwriting review error', () => {
