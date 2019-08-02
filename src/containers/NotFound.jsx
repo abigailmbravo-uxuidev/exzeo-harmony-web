7b778f834924032941ac6f596f2317fa64c8e12d
Joe Sutphin <jsutphin@exzeo.com>
2019-08-02 14:12:36 -0400

    update 404 page

--- src/containers/NotFound.jsx
+++ src/containers/NotFound.jsx
@@ -3,57 +3,73 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 const NotFound = function NotFound() {
   return (
-    <div className="app-wrapper">
+    <div className="app-wrapper not-found">
       <Header toggleSideNav={x => x} />
-      <main role="document">
-        <div className="route-content">
-          <div className="error-content" role="article">
-            <div className="error-wrapper">
-              <section>
-                <div className="" id="Error">
-                  <div className="detail-wrapper">
-                    <h3 className="section-group-header error">
-                      <i className="fa fa-exclamation-triangle" /> 404 Page not
-                      found
-                    </h3>
-                    <h4 className="error-message">
-                      We apologize, but the page you requested was not found.
-                    </h4>
-                    <p>
-                      Please check the URL for proper spelling. If you are
-                      having trouble locating your destination, you can return
-                      to the dashboard or contact one of our representatives so
-                      they may further assist you
-                    </p>
-                    <Link className="btn btn-secondary" to="/">
-                      Return to Dashboard
-                    </Link>
+      <div role="main">
+        <div className="content-wrapper">
+          <div className="workflow" role="article">
+            <div className="route">
+              <div className="route-content">
+                <div className="error-content" role="article">
+                  <header>
+                    <div className="error-header">
+                      <i className="fa fa-exclamation-triangle" />
+                      <h4 className="section-group-header error">
+                        Whoopsies! We're sorry, something seems to have gone
+                        wrong!
+                      </h4>
+                    </div>
+                  </header>
+                  <div className="error-wrapper">
+                    <section>
+                      <div id="Error">
+                        <h4 className="error-intro">
+                          Sorry for the inconvenience, either the page you're
+                          looking for does not exist or we're experiencing an
+                          issue with our application at the moment.
+                        </h4>
+                        <div className="static-error-message">
+                          <p>
+                            Please try refreshing the page. If you continue
+                            having trouble, please return to the Dashboard or
+                            contact one of our representatives so they may
+                            further assist you
+                          </p>
+                        </div>
+                        <div className="error-footer">
+                          <Link className="btn btn-primary" to="/">
+                            Return to Dashboard
+                          </Link>
+                        </div>
+                      </div>
+                    </section>
+                    <aside>
+                      <div className="image" />
+                      <div className="contact-info">
+                        <a
+                          className="link-email"
+                          href="mailto:customerservice@typtap.com"
+                        >
+                          <i className="fa fa-envelope" /> <span>email us</span>
+                        </a>
+                        <a className="link-phone" href="tel:8442897968">
+                          <i className="fa fa-phone" />{' '}
+                          <span>(844) 289-7968</span>
+                        </a>
+                      </div>
+                    </aside>
                   </div>
+                  <Footer />
                 </div>
-              </section>
-              <aside>
-                <div className="image" />
-                <div className="contact-info">
-                  <a
-                    className="link-email"
-                    href="mailto:customerservice@typtap.com"
-                  >
-                    <i className="fa fa-envelope" />
-                    <span>email us</span>
-                  </a>
-                  <a className="link-phone" href="tel:8442897968">
-                    <i className="fa fa-phone" />
-                    <span>(844) 289-7968</span>
-                  </a>
-                </div>
-              </aside>
+              </div>
             </div>
           </div>
         </div>
-      </main>
+      </div>
     </div>
   );
 };
