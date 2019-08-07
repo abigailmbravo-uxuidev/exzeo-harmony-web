f802b476ae7ad213ab000525d2a9c39e8965ea41
elamison <elamison@exzeo>
2019-08-07 11:31:43 -0400

    Fix underwriting error test.

--- src/state/actions/quoteState.actions.js
+++ src/state/actions/quoteState.actions.js
@@ -122,6 +122,7 @@ export function verifyQuote({ quoteNumber, quoteId }) {
         'quoteManager.verifyQuote'
       );
       const quote = response.data.result;
+      console.log('verify: ', quote);
       dispatch(setQuote(quote));
       return quote;
     } catch (error) {
