83248556064dc92e457ad4e4cc56b1a558a151f7
Jordan Rhodes <jsrhodes15@gmail.com>
2019-08-12 09:01:59 -0400

    Comment out HO3 for testing in CI

--- cypress/integration/quote/base.spec.js
+++ cypress/integration/quote/base.spec.js
@@ -24,79 +24,36 @@ import {
   verifyTest
 } from '../../pageTests';
 
-describe('Agency Happy Path', () => {
-  before('Login', () => cy.login());
-  beforeEach('Set Route Aliases', () => setRouteAliases());
-
-  it('Navigates through the HO3 quote workflow', () => {
-    navigateThroughLanding();
-    navigateThroughSearchAddress();
-    policyholderTest();
-    navigateThroughPolicyholder();
-
-    underwritingTest();
-    navigateThroughUnderwriting();
-    customizeTest();
-    navigateThroughCustomize();
-
-    shareTest();
-    navigateThroughShare();
-    navigateThroughAssumptions();
-    additionalInterestTest();
-    navigateThroughAdditionalInterests();
-
-    mailingBillingTest();
-    navigateThroughMailingBilling();
-    verifyTest();
-    navigateThroughVerify();
-    navigateThroughScheduleDate();
-
-    // TODO until we can figure out why 'Application Sent DocuSign' never happens in CI, skipping.
-    if (Cypress.env('CI') === true) {
-      cy.task('log', 'CI is set to true - not retrieving HO3 quote to check \'quoteState\' === \'Application Sent DocuSign\'')
-    } else {
-      cy.findDataTag('quoteNumberDetail').find('> dl > div > dd').then($quote => {
-        navigateThroughThankYou();
-        cy.wait(15000)
-          .get('.btn[href="/search/retrieve"]').click()
-          .findDataTag('quoteNumber').type($quote.text())
-          .clickSubmit('#SearchBar')
-          .findDataTag('quote-list').should('not.be.empty')
-          .find('.card .card-detail-wrapper .quote-state').should('contain', 'Application Sent DocuSign')
-          .go('back');
-      });
-    }
-  });
-});
-
-// describe('AF3 Happy Path', () => {
-//   before('Login', () => cy.login(loginAF3));
+// describe('Agency Happy Path', () => {
+//   before('Login', () => cy.login());
 //   beforeEach('Set Route Aliases', () => setRouteAliases());
 //
-//   it('Navigates through the AF3 quote workflow', () => {
+//   it('Navigates through the HO3 quote workflow', () => {
 //     navigateThroughLanding();
-//     cy.findDataTag('product').select('AF3');
-//     navigateThroughSearchAddress(userAF3);
-//     policyholderTest('AF3');
-//     navigateThroughPolicyholder(userAF3);
-//     underwritingTest('AF3');
-//     navigateThroughUnderwriting(underwritingAF3);
-//     // customizeTest('AF3');
+//     navigateThroughSearchAddress();
+//     policyholderTest();
+//     navigateThroughPolicyholder();
+//
+//     underwritingTest();
+//     navigateThroughUnderwriting();
+//     customizeTest();
 //     navigateThroughCustomize();
-//     shareTest('AF3');
+//
+//     shareTest();
 //     navigateThroughShare();
 //     navigateThroughAssumptions();
-//     additionalInterestTest('AF3');
+//     additionalInterestTest();
 //     navigateThroughAdditionalInterests();
-//     mailingBillingTest('AF3');
+//
+//     mailingBillingTest();
 //     navigateThroughMailingBilling();
-//     verifyTest('AF3');
+//     verifyTest();
 //     navigateThroughVerify();
 //     navigateThroughScheduleDate();
 //
 //     // TODO until we can figure out why 'Application Sent DocuSign' never happens in CI, skipping.
 //     if (Cypress.env('CI') === true) {
-//       cy.task('log', 'CI is set to true - not retrieving AF3 quote to check \'quoteState\' === \'Application Sent DocuSign\'')
+//       cy.task('log', 'CI is set to true - not retrieving HO3 quote to check \'quoteState\' === \'Application Sent DocuSign\'')
 //     } else {
 //       cy.findDataTag('quoteNumberDetail').find('> dl > div > dd').then($quote => {
 //         navigateThroughThankYou();
@@ -105,8 +62,51 @@ describe('Agency Happy Path', () => {
 //           .findDataTag('quoteNumber').type($quote.text())
 //           .clickSubmit('#SearchBar')
 //           .findDataTag('quote-list').should('not.be.empty')
+//           .find('.card .card-detail-wrapper .quote-state').should('contain', 'Application Sent DocuSign')
 //           .go('back');
 //       });
 //     }
 //   });
 // });
+
+describe('AF3 Happy Path', () => {
+  before('Login', () => cy.login(loginAF3));
+  beforeEach('Set Route Aliases', () => setRouteAliases());
+
+  it('Navigates through the AF3 quote workflow', () => {
+    navigateThroughLanding();
+    cy.findDataTag('product').select('AF3');
+    navigateThroughSearchAddress(userAF3);
+    policyholderTest('AF3');
+    navigateThroughPolicyholder(userAF3);
+    underwritingTest('AF3');
+    navigateThroughUnderwriting(underwritingAF3);
+    // customizeTest('AF3');
+    navigateThroughCustomize();
+    shareTest('AF3');
+    navigateThroughShare();
+    navigateThroughAssumptions();
+    additionalInterestTest('AF3');
+    navigateThroughAdditionalInterests();
+    mailingBillingTest('AF3');
+    navigateThroughMailingBilling();
+    verifyTest('AF3');
+    navigateThroughVerify();
+    navigateThroughScheduleDate();
+
+    // TODO until we can figure out why 'Application Sent DocuSign' never happens in CI, skipping.
+    if (Cypress.env('CI') === true) {
+      cy.task('log', 'CI is set to true - not retrieving AF3 quote to check \'quoteState\' === \'Application Sent DocuSign\'')
+    } else {
+      cy.findDataTag('quoteNumberDetail').find('> dl > div > dd').then($quote => {
+        navigateThroughThankYou();
+        cy.wait(15000)
+          .get('.btn[href="/search/retrieve"]').click()
+          .findDataTag('quoteNumber').type($quote.text())
+          .clickSubmit('#SearchBar')
+          .findDataTag('quote-list').should('not.be.empty')
+          .go('back');
+      });
+    }
+  });
+});
