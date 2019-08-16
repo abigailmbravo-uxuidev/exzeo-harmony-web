4d47bf06eb752d1911b56c11c91cf448c1904610
elamison <elamison@exzeo>
2019-08-16 16:55:37 -0400

    Fix Cypress UW Error test.

--- cypress/integration/underwritingExceptions/underwritingExceptions.spec.js
+++ cypress/integration/underwritingExceptions/underwritingExceptions.spec.js
@@ -25,15 +25,21 @@ describe('Underwriting Error Testing', () => {
     // Give underwriting bad data.
     navigateThroughUnderwriting({
       ...underwritingHO3,
-      'previousClaims': '3-5 Years'
+      previousClaims: '3-5 Years'
     });
     navigateThroughCustomize();
     // We go to the error page.
-    cy.get('ul.error').should('contain', 'Due to previous claims history, underwriting review is required prior to binding.')
-      .findDataTag('edit').should('contain', 'Edit')
+    cy.get('ul.error')
+      .should(
+        'contain',
+        'Due to previous claims history, underwriting review is required prior to binding.'
+      )
+      .findDataTag('edit')
+      .should('contain', 'Edit')
       .click()
       // Policyholder should be able to be navigated through without re-filling out the form.
-      .clickSubmit('#QuoteWorkflow').wait('@updateQuote');
+      .clickSubmit('#QuoteWorkflow')
+      .wait('@updateQuote');
     // Go back through with good data.
     navigateThroughUnderwriting();
     navigateThroughCustomize();
@@ -41,26 +47,32 @@ describe('Underwriting Error Testing', () => {
     navigateThroughAssumptions();
     navigateThroughAdditionalInterests();
     // On mailing billing, we stub the post request.
-    cy.route('POST', '/svc?quoteManager.reviewQuote', {
+    cy.route('POST', '/svc?quoteManager.updateQuote', {
       ...updateQuote,
       result: {
         ...updateQuote.result,
-        quoteState: 'Quote Stopped',
-        underwritingExceptions: [{
-          'code': '003',
-          'displayText': 'Missing Info - Mailing/Billing Info',
-          'category': 'Coverages & Deductibles',
-          'action': 'Missing Info',
-          'agentMessage': 'Missing required information to complete quote -  Mailing/Billing Info',
-          'internalMessage': 'Missing required information to complete quote -  Mailing/Billing Info',
-          'active': true,
-          'canOverride': false,
-          'overridden': false,
-        }]
+        underwritingExceptions: [
+          {
+            code: '003',
+            displayText: 'Missing Info - Mailing/Billing Info',
+            category: 'Coverages & Deductibles',
+            action: 'Missing Info',
+            agentMessage:
+              'Missing required information to complete quote -  Mailing/Billing Info',
+            internalMessage:
+              'Missing required information to complete quote -  Mailing/Billing Info',
+            active: true,
+            canOverride: false,
+            overridden: false
+          }
+        ]
       }
-    }).as('reviewQuote');
+    }).as('updateQuote');
     navigateThroughMailingBilling();
     cy.get('button.btn-primary').click();
-    cy.get('div#Error').should('contain', "Sorry for the inconvenience, either the page you're looking for does not exist or we're experiencing an issue with our application at the moment.");
+    cy.get('div#Error').should(
+      'contain',
+      "Sorry for the inconvenience, either the page you're looking for does not exist or we're experiencing an issue with our application at the moment."
+    );
   });
 });
