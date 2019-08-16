560671c0f443d6f90581dcb5868d3ab2f1448250
Joe Sutphin <jsutphin@exzeo.com>
2019-08-16 12:08:24 -0400

    update link to commision addendum

--- src/containers/Training.jsx
+++ src/containers/Training.jsx
@@ -8,7 +8,7 @@ export const externalLinks = [
   {
     key: 1,
     productIcon: 'generic',
-    url: `${process.env.REACT_APP_DOCUMENT_URL}marketing/generic/TypTap-Commision-Schedule.pdf`,
+    url: `${process.env.REACT_APP_DOCUMENT_URL}/marketing/generic/TypTap-Commision-Schedule.pdf`,
     title: 'Commission Addendum',
     description: 'Homeowners and Flood Insurance commission schedule.',
     linkIcon: 'pdf'
