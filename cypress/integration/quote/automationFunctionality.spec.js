520ab347f12a949dc082a22308c4c84dfe09baf5
elamison <elamison@exzeo>
2019-08-19 13:05:05 -0400

    Skip Cypress tests with retrieve.

--- cypress/integration/quote/automationFunctionality.spec.js
+++ cypress/integration/quote/automationFunctionality.spec.js
@@ -16,16 +16,25 @@ import {
 describe('Back Button Testing', () => {
   // Grab the quote number off the ui and retrieve it to confirm our quote is searchable with the quote number
   const getQuoteNumberAndRetrieve = status =>
-    cy.findDataTag('quoteNumberDetail').find('> dl > div > dd').then($quote =>
-      cy.go('back')
-        .get('div.dashboard-message').should('exist')
-        .get('.btn[href="/search/retrieve"]').click()
-        .findDataTag('quoteNumber').type($quote.text())
-        .clickSubmit('#SearchBar')
-        .findDataTag('quote-list').should('not.be.empty')
-        .find('li.card .quote-state').should('contain', status)
-        .go('back')
-    );
+    cy
+      .findDataTag('quoteNumberDetail')
+      .find('> dl > div > dd')
+      .then($quote =>
+        cy
+          .go('back')
+          .get('div.dashboard-message')
+          .should('exist')
+          .get('.btn[href="/search/retrieve"]')
+          .click()
+          .findDataTag('quoteNumber')
+          .type($quote.text())
+          .clickSubmit('#SearchBar')
+          .findDataTag('quote-list')
+          .should('not.be.empty')
+          .find('li.card .quote-state')
+          .should('contain', status)
+          .go('back')
+      );
 
   before('Login and go to search', () => {
     cy.login();
@@ -35,71 +44,83 @@ describe('Back Button Testing', () => {
 
   it('Browser Back Button', () => {
     cy.go('back')
-      .get('div.dashboard-message').should('exist');
+      .get('div.dashboard-message')
+      .should('exist');
 
     navigateThroughLanding();
     navigateThroughSearchAddress();
-    cy.wait('@getZipcodeSettings').go('back')
-      .get('div.dashboard-message').should('exist');
+    cy.wait('@getZipcodeSettings')
+      .go('back')
+      .get('div.dashboard-message')
+      .should('exist');
   });
 
-  it('Browser Back Button pt 2', () => {
-    navigateThroughLanding();
-    navigateThroughSearchAddress();
-    navigateThroughPolicyholder();
-    cy.go('back')
-      .get('div.dashboard-message').should('exist');
+  if (Cypress.env('CI')) {
+    it('Skip test due to environment', () => {
+      cy.task('log', 'CI === true: not running Quote/retrieve specs');
+    });
+  } else {
+    it('Browser Back Button pt 2', () => {
+      navigateThroughLanding();
+      navigateThroughSearchAddress();
+      navigateThroughPolicyholder();
+      cy.go('back')
+        .get('div.dashboard-message')
+        .should('exist');
 
-    navigateThroughLanding();
-    navigateThroughSearchAddress();
-    navigateThroughPolicyholder();
-    navigateThroughUnderwriting();
-    getQuoteNumberAndRetrieve('Quote Qualified');
+      navigateThroughLanding();
+      navigateThroughSearchAddress();
+      navigateThroughPolicyholder();
+      navigateThroughUnderwriting();
+      getQuoteNumberAndRetrieve('Quote Qualified');
 
-    navigateThroughLanding();
-    navigateThroughSearchAddress();
-    navigateThroughPolicyholder();
-    navigateThroughUnderwriting();
-    navigateThroughCustomize();
-    getQuoteNumberAndRetrieve('Quote Qualified');
+      navigateThroughLanding();
+      navigateThroughSearchAddress();
+      navigateThroughPolicyholder();
+      navigateThroughUnderwriting();
+      navigateThroughCustomize();
+      getQuoteNumberAndRetrieve('Quote Qualified');
 
-    navigateThroughLanding();
-    navigateThroughSearchAddress();
-    navigateThroughPolicyholder();
-    navigateThroughUnderwriting();
-    navigateThroughCustomize();
-    navigateThroughShare();
-    navigateThroughAssumptions();
-    navigateThroughAdditionalInterests();
-    navigateThroughMailingBilling();
-    getQuoteNumberAndRetrieve('Application Ready');
-  });
+      navigateThroughLanding();
+      navigateThroughSearchAddress();
+      navigateThroughPolicyholder();
+      navigateThroughUnderwriting();
+      navigateThroughCustomize();
+      navigateThroughShare();
+      navigateThroughAssumptions();
+      navigateThroughAdditionalInterests();
+      navigateThroughMailingBilling();
+      getQuoteNumberAndRetrieve('Application Ready');
+    });
 
-  it('Browser back button pt 3', () => {
-    navigateThroughLanding();
-    navigateThroughSearchAddress();
-    navigateThroughPolicyholder();
-    navigateThroughUnderwriting();
-    navigateThroughCustomize();
-    navigateThroughShare();
-    navigateThroughAssumptions();
-    navigateThroughAdditionalInterests();
-    navigateThroughMailingBilling();
-    navigateThroughVerify();
-    getQuoteNumberAndRetrieve('Application Ready');
+    it('Browser back button pt 3', () => {
+      navigateThroughLanding();
+      navigateThroughSearchAddress();
+      navigateThroughPolicyholder();
+      navigateThroughUnderwriting();
+      navigateThroughCustomize();
+      navigateThroughShare();
+      navigateThroughAssumptions();
+      navigateThroughAdditionalInterests();
+      navigateThroughMailingBilling();
+      navigateThroughVerify();
+      getQuoteNumberAndRetrieve('Application Ready');
 
-    navigateThroughLanding();
-    navigateThroughSearchAddress();
-    navigateThroughPolicyholder();
-    navigateThroughUnderwriting();
-    navigateThroughCustomize();
-    navigateThroughShare();
-    navigateThroughAssumptions();
-    navigateThroughAdditionalInterests();
-    navigateThroughMailingBilling();
-    navigateThroughVerify();
-    navigateThroughScheduleDate();
-    cy.wait('@sendApplication').go('back')
-      .get('div.dashboard-message').should('exist');
-  });
+      navigateThroughLanding();
+      navigateThroughSearchAddress();
+      navigateThroughPolicyholder();
+      navigateThroughUnderwriting();
+      navigateThroughCustomize();
+      navigateThroughShare();
+      navigateThroughAssumptions();
+      navigateThroughAdditionalInterests();
+      navigateThroughMailingBilling();
+      navigateThroughVerify();
+      navigateThroughScheduleDate();
+      cy.wait('@sendApplication')
+        .go('back')
+        .get('div.dashboard-message')
+        .should('exist');
+    });
+  }
 });
