a5c495fce57401d61bb94dbc20d436c19cd3bb15
elamison <elamison@exzeo>
2019-09-18 13:11:42 -0400

    Add quote-detail class to navigation.

--- src/utilities/navigation.js
+++ src/utilities/navigation.js
@@ -1,53 +1,60 @@
 import classNames from 'classnames';
 
-export const getNavLinks = (params, status) => [
-  {
-    key: 'home',
-    to: '/',
-    label: 'DASHBOARD',
-    styleName: 'agent-dashboard',
-    hasIcon: true,
-    exact: true
-  },
-  {
-    key: 'searchAddress',
-    to: status === 'Active' ? '/search/address' : '#',
-    label: 'NEW QUOTE',
-    styleName:
-      status === 'Active' ? 'new-quote label' : 'new-quote label disabled',
-    hasIcon: true
-  },
-  {
-    key: 'searchQuotes',
-    to: status === 'Active' || status === 'Pending' ? '/search/retrieve' : '#',
-    label: 'QUOTES',
-    styleName:
-      status === 'Active' || status === 'Pending'
-        ? 'quote label'
-        : 'quote label disabled',
-    hasIcon: true
-  },
-  {
-    key: 'policy',
-    to: '/policy',
-    label: 'POLICIES',
-    styleName: classNames('policy label', {
-      'policy-detail': params.policyNumber
-    }),
-    hasIcon: true
-  },
-  {
-    key: 'contacts',
-    to: '/contacts',
-    label: 'CONTACTS',
-    styleName: 'contacts label',
-    hasIcon: true
-  },
-  {
-    key: 'training',
-    to: '/training',
-    label: 'HELPFUL INFO',
-    styleName: 'training label',
-    hasIcon: true
-  }
-];
+export const getNavLinks = (params, status) => {
+  const quoteClasses =
+    status === 'Active' || status === 'Pending'
+      ? 'quote label'
+      : 'quote label disabled';
+
+  return [
+    {
+      key: 'home',
+      to: '/',
+      label: 'DASHBOARD',
+      styleName: 'agent-dashboard',
+      hasIcon: true,
+      exact: true
+    },
+    {
+      key: 'searchAddress',
+      to: status === 'Active' ? '/search/address' : '#',
+      label: 'NEW QUOTE',
+      styleName:
+        status === 'Active' ? 'new-quote label' : 'new-quote label disabled',
+      hasIcon: true
+    },
+    {
+      key: 'searchQuotes',
+      to:
+        status === 'Active' || status === 'Pending' ? '/search/retrieve' : '#',
+      label: 'QUOTES',
+      styleName: classNames(quoteClasses, {
+        'quote-detail': params.quoteNumber
+      }),
+      hasIcon: true
+    },
+    {
+      key: 'policy',
+      to: '/policy',
+      label: 'POLICIES',
+      styleName: classNames('policy label', {
+        'policy-detail': params.policyNumber
+      }),
+      hasIcon: true
+    },
+    {
+      key: 'contacts',
+      to: '/contacts',
+      label: 'CONTACTS',
+      styleName: 'contacts label',
+      hasIcon: true
+    },
+    {
+      key: 'training',
+      to: '/training',
+      label: 'HELPFUL INFO',
+      styleName: 'training label',
+      hasIcon: true
+    }
+  ];
+};
