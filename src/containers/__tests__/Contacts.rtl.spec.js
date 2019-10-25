65b8485a1f4a3ead6a372ef20a0fa28483999a77
elamison <elamison@exzeo>
2019-10-25 08:27:41 -0400

    Fix unit tests.

--- src/containers/__tests__/Contacts.rtl.spec.js
+++ src/containers/__tests__/Contacts.rtl.spec.js
@@ -42,7 +42,7 @@ describe('Testing the Contacts Page', () => {
         document.querySelector(`div.${manager.icon.split(' ')[0]}`)
       ).toBeInTheDocument();
       expect(getByText(manager.name));
-      expect(getByText(`| ${manager.title}`));
+      expect(getByText(manager.title));
       expect(getByText(normalize.phone(manager.phone)));
       manager.cell && expect(getByText(normalize.phone(manager.cell)));
       expect(getByText(manager.email));
@@ -55,13 +55,12 @@ describe('Testing the Contacts Page', () => {
     );
 
     supportContacts.forEach(support => {
+      const extension = support.extension ? ` ${support.extension}` : '';
       expect(
         document.querySelector(`div.${support.icon.split(' ')[0]}`)
       ).toBeInTheDocument();
       expect(getByText(support.name));
-      expect(
-        getByText(`${normalize.phone(support.phone)} ${support.extension}`)
-      );
+      expect(getByText(`${normalize.phone(support.phone)}${extension}`));
       expect(getByText(support.email));
       support.message && expect(getByText(support.message));
       support.disclaimer && expect(getByText(support.disclaimer));
