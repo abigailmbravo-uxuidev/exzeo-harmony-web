859a1cbb0dfd1aad467e4ca392a4d0f8a961b454
jsutphin <>
2019-12-19 13:40:06 -0500

    update lists

--- src/containers/Training.jsx
+++ src/containers/Training.jsx
@@ -4,7 +4,147 @@ import FancyExternalLink from '../components/FancyExternalLink';
 import Footer from '../components/Footer';
 import AppWrapper from '../components/AppWrapper';
 
-export const externalLinks = [
+export const externalLinksGeneric = [
+  {
+    key: 1,
+    productIcon: 'generic',
+    url: `${process.env.REACT_APP_DOCUMENT_URL}/marketing/generic/TypTap-Commision-Schedule.pdf`,
+    title: 'Commission Addendum',
+    description: 'Homeowners and Flood Insurance commission schedule.',
+    linkIcon: 'pdf'
+  },
+  {
+    key: 2,
+    productIcon: 'generic',
+    url: `${process.env.REACT_APP_DOCUMENT_URL}/marketing/generic/Who-Is-TypTap.pdf`,
+    title: 'Who Is TypTap',
+    description: 'Brief summary of information about TypTap Insurance.',
+    linkIcon: 'pdf'
+  },
+  {
+    key: 3,
+    productIcon: 'home',
+    url: `${process.env.REACT_APP_DOCUMENT_URL}/marketing/ho3/TT-Pilot-Agents-one-pager.pdf`,
+    title: 'TypTap Homeowners Agent Program Guide',
+    description:
+      'Reference guide for payments, how to login, open counties, year built and agency support.',
+    linkIcon: 'pdf'
+  },
+  {
+    key: 4,
+    productIcon: 'home',
+    url: `${process.env.REACT_APP_DOCUMENT_URL}/marketing/ho3/TT-HO3-Quick-Ref-Guide.pdf`,
+    title: 'TypTap Homeowners Quick Reference Guide',
+    description: 'Detailed list of the TypTap HO3 underwriting guidelines.',
+    linkIcon: 'pdf'
+  },
+  {
+    key: 5,
+    url: `${process.env.REACT_APP_DOCUMENT_URL}/marketing/ho3/TT-HO3-County-Map.pdf`,
+    productIcon: 'home',
+    title: 'TypTap Homeowners County Map',
+    description: 'Where TypTap HO3 product is currently available.',
+    linkIcon: 'pdf'
+  },
+  {
+    key: 6,
+    url: `${process.env.REACT_APP_DOCUMENT_URL}/marketing/ho3/TypTap-Underwriting-Exception-Req.pdf`,
+    productIcon: 'home',
+    title: 'TypTap Underwriting Exception Requirements Document',
+    description: 'Information needed for underwriting exception review.',
+    linkIcon: 'pdf'
+  },
+  {
+    key: 7,
+    url: `${process.env.REACT_APP_DOCUMENT_URL}/marketing/ho3/TT-HO3-Opening-Protection.pdf`,
+    productIcon: 'home',
+    title: 'Wind Mitigation Opening Protection Assistance',
+    description:
+      'Assistance in determining the proper Opening Protection selection in Harmony based on the insured’s Wind Mitigation.',
+    linkIcon: 'pdf'
+  },
+  {
+    key: 8,
+    url: `${process.env.REACT_APP_DOCUMENT_URL}/marketing/ho3/TT-HO3-Coverage-C-Exclusion-Doc.pdf`,
+    productIcon: 'home',
+    title: 'Coverage C Rejection Form',
+    description:
+      'Form required to reject Coverage C (0% Contents Coverage). All insured’s signatures required.',
+    linkIcon: 'pdf'
+  }
+];
+
+export const externalLinksHome = [
+  {
+    key: 1,
+    productIcon: 'generic',
+    url: `${process.env.REACT_APP_DOCUMENT_URL}/marketing/generic/TypTap-Commision-Schedule.pdf`,
+    title: 'Commission Addendum',
+    description: 'Homeowners and Flood Insurance commission schedule.',
+    linkIcon: 'pdf'
+  },
+  {
+    key: 2,
+    productIcon: 'generic',
+    url: `${process.env.REACT_APP_DOCUMENT_URL}/marketing/generic/Who-Is-TypTap.pdf`,
+    title: 'Who Is TypTap',
+    description: 'Brief summary of information about TypTap Insurance.',
+    linkIcon: 'pdf'
+  },
+  {
+    key: 3,
+    productIcon: 'home',
+    url: `${process.env.REACT_APP_DOCUMENT_URL}/marketing/ho3/TT-Pilot-Agents-one-pager.pdf`,
+    title: 'TypTap Homeowners Agent Program Guide',
+    description:
+      'Reference guide for payments, how to login, open counties, year built and agency support.',
+    linkIcon: 'pdf'
+  },
+  {
+    key: 4,
+    productIcon: 'home',
+    url: `${process.env.REACT_APP_DOCUMENT_URL}/marketing/ho3/TT-HO3-Quick-Ref-Guide.pdf`,
+    title: 'TypTap Homeowners Quick Reference Guide',
+    description: 'Detailed list of the TypTap HO3 underwriting guidelines.',
+    linkIcon: 'pdf'
+  },
+  {
+    key: 5,
+    url: `${process.env.REACT_APP_DOCUMENT_URL}/marketing/ho3/TT-HO3-County-Map.pdf`,
+    productIcon: 'home',
+    title: 'TypTap Homeowners County Map',
+    description: 'Where TypTap HO3 product is currently available.',
+    linkIcon: 'pdf'
+  },
+  {
+    key: 6,
+    url: `${process.env.REACT_APP_DOCUMENT_URL}/marketing/ho3/TypTap-Underwriting-Exception-Req.pdf`,
+    productIcon: 'home',
+    title: 'TypTap Underwriting Exception Requirements Document',
+    description: 'Information needed for underwriting exception review.',
+    linkIcon: 'pdf'
+  },
+  {
+    key: 7,
+    url: `${process.env.REACT_APP_DOCUMENT_URL}/marketing/ho3/TT-HO3-Opening-Protection.pdf`,
+    productIcon: 'home',
+    title: 'Wind Mitigation Opening Protection Assistance',
+    description:
+      'Assistance in determining the proper Opening Protection selection in Harmony based on the insured’s Wind Mitigation.',
+    linkIcon: 'pdf'
+  },
+  {
+    key: 8,
+    url: `${process.env.REACT_APP_DOCUMENT_URL}/marketing/ho3/TT-HO3-Coverage-C-Exclusion-Doc.pdf`,
+    productIcon: 'home',
+    title: 'Coverage C Rejection Form',
+    description:
+      'Form required to reject Coverage C (0% Contents Coverage). All insured’s signatures required.',
+    linkIcon: 'pdf'
+  }
+];
+
+export const externalLinksFlood = [
   {
     key: 1,
     productIcon: 'generic',
@@ -84,7 +224,17 @@ const Training = ({ auth, match }) => (
             &nbsp;Reference
           </h2>
           <ul className="link-list reference-links">
-            {externalLinks.map(link => (
+            {externalLinksGeneric.map(link => (
+              <FancyExternalLink key={link.key} {...link} />
+            ))}
+          </ul>
+          <ul className="link-list reference-links">
+            {externalLinksHome.map(link => (
+              <FancyExternalLink key={link.key} {...link} />
+            ))}
+          </ul>
+          <ul className="link-list reference-links">
+            {externalLinksFlood.map(link => (
               <FancyExternalLink key={link.key} {...link} />
             ))}
           </ul>
