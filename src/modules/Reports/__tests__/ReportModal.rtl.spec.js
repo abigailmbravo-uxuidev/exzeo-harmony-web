12cb87acca2b8f4ae978cf6c67403f65ca091180
Mark Eads <meads@hcpci.com>
2020-01-08 15:41:38 -0500

    update report modal tests

--- src/modules/Reports/__tests__/ReportModal.rtl.spec.js
+++ src/modules/Reports/__tests__/ReportModal.rtl.spec.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { mockServiceRunner, renderWithForm } from '../../../test-utils';
 
 import ReportModal from '../ReportModal';
-import { agencyActivityColumns } from '../utilities';
+import { agencyActivityColumns, bookOfBusinessColumns } from '../utilities';
 
 mockServiceRunner([]);
 
@@ -12,8 +12,10 @@ const defaultProps = {
   handleCancel() {},
   handleDownload() {},
   report: {
-    title: 'Book Of Business',
-    columns: agencyActivityColumns
+    title: 'Agency Activity Report',
+    columns: agencyActivityColumns,
+    minDate: '01/01/2019',
+    maxDate: '01/01/2020'
   },
   auth: {},
   match: {
@@ -33,19 +35,36 @@ describe('Testing the Reports Modal', () => {
     const modalIcon = getByTestId('modal-icon');
     expect(modalIcon.className).toEqual('fa fa-calendar');
 
+    const title = getByTestId('modal-title');
+    expect(title).toHaveTextContent('Agency Activity Report');
+
     const fromLabel = getByTestId('from_label');
     expect(fromLabel).toHaveTextContent('From');
 
     const toLabel = getByTestId('to_label');
     expect(toLabel).toHaveTextContent('To');
 
-    const title = getByTestId('modal-title');
-    expect(title).toHaveTextContent('Book Of Business');
-
     const minDate = getByTestId('from');
     expect(minDate.type).toEqual('date');
 
     const maxDate = getByTestId('to');
     expect(maxDate.type).toEqual('date');
+  });
+
+  it('Reports Modal Testing with min and max dates', async () => {
+    const newProps = {
+      ...defaultProps,
+      report: {
+        title: 'Book Of Business',
+        columns: bookOfBusinessColumns
+      }
+    };
+
+    const { getByTestId } = renderWithForm(<ReportModal {...newProps} />);
+    const modalIcon = getByTestId('modal-icon');
+    expect(modalIcon.className).toEqual('fa fa-calendar');
+
+    const title = getByTestId('modal-title');
+    expect(title).toHaveTextContent('Book Of Business');
   });
 });
