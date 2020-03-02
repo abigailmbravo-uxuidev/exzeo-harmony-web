148786d5ab3717f13fbe0c53867d8a1fc6767716
Mark Eads <meads@hcpci.com>
2020-03-02 11:36:16 -0500

    update test

--- cypress/pageTests/policyDetailsTest.js
+++ cypress/pageTests/policyDetailsTest.js
@@ -1,4 +1,5 @@
 import { user } from '../fixtures';
+import moment from 'moment';
 
 const headers = [
   { name: 'quoteNumberDetail', label: 'Quote Number', value: '-' },
@@ -34,35 +35,28 @@ export default () =>
     )
     // Change effective date
     .findDataTag('effectiveDate')
-    .then($date => {
-      const effDate = $date.val().split('-');
-      let day = parseInt(effDate[2] > 2)
-        ? parseInt(effDate[2]) + 1
-        : parseInt(effDate[2]) - 1;
-      day = day < 9 ? '0' + day : day;
-      cy.findDataTag('effectiveDate')
-        .type(effDate[0] + '-' + effDate[1] + '-' + day)
-        // Select an agent
-        .findDataTag('agentCode')
-        .select(user.agentCode)
-        // check detail header before submit (this allows time for the 'premium' animation to finish)
-        .wrap(headers)
-        .each(header => cy.checkDetailHeader(header))
-        .clickSubmit('#QuoteWorkflow')
-        // Expect that there is only one policyholder submitted
-        .wait('@updateQuote')
-        .then(({ request, response }) => {
-          expect(
-            request.body.data.quote.policyHolders.length,
-            'Policyholders in request'
-          ).to.equal(1);
-          expect(
-            response.body.result.quoteInputState,
-            'Quote Input State'
-          ).to.equal('Underwriting');
-          //Verify that effective date has changed
-          expect(response.body.result.effectiveDate.split('T')[0]).not.to.equal(
-            effDate[0] + '-' + effDate[1] + '-' + effDate[2]
-          );
-        });
+    .type(
+      moment
+        .utc()
+        .add('7', 'days')
+        .format('YYYY-MM-DD')
+    )
+    // Select an agent
+    .findDataTag('agentCode')
+    .select(user.agentCode)
+    // check detail header before submit (this allows time for the 'premium' animation to finish)
+    .wrap(headers)
+    .each(header => cy.checkDetailHeader(header))
+    .clickSubmit('#QuoteWorkflow')
+    // Expect that there is only one policyholder submitted
+    .wait('@updateQuote')
+    .then(({ request, response }) => {
+      expect(
+        request.body.data.quote.policyHolders.length,
+        'Policyholders in request'
+      ).to.equal(1);
+      expect(
+        response.body.result.quoteInputState,
+        'Quote Input State'
+      ).to.equal('Underwriting');
     });
